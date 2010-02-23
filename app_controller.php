15cdb71b5834fe6964f5f4afc62b924dc3c223f6
Dibyasakti Das <dibyasaktid@mindfiresolutions.com>
2010-02-23 20:42:35 +0530

    modified for the auth component

--- app_controller.php
+++ app_controller.php
@@ -1,8 +1,21 @@
 <?php
 	class AppController extends Controller {
 		
-		var $components = array('Session', 'DebugKit.Toolbar');
+		var $components = array('Session', 'DebugKit.Toolbar','Auth');
 		
-		var $helpers = array('Javascript', 'Html', 'Form');
+		 var $helpers = array('Session','Html','Ajax','Javascript','Form');
+		
+		
+		/* var $components = array('Session','Auth');
+		 var $helpers = array('Session','Html','Ajax','Javascript','Form');*/
+    
+		 function beforeFilter()
+			{
+				$this->Auth->userModel = 'Admin';
+				$this->Auth->loginAction = array('controller' => 'admins', 'action' => 'login');
+				$this->Auth->loginRedirect = array('controller' => 'admin_homes', 'action' => 'index');
+				$this->Auth->logoutRedirect = '/admins/login';
+
+			}
 		
 	}
