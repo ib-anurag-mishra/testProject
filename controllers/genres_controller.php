8613ac19895734355677b1dd5f0561e5462b9d6f
Rob Richmond <rob@maycreate.com>
2010-03-18 10:12:46 -0400

    changed from UpdateOn to CreatedOn

--- controllers/genres_controller.php
+++ controllers/genres_controller.php
@@ -28,7 +28,7 @@ Class GenresController extends AppController
 							array('Genre.Genre' => $genreName),							
 							array("Physicalproduct.ReferenceID <> Physicalproduct.ProdID"),
 							array('Physicalproduct.TrackBundleCount' => 0),
-							array("Physicalproduct.UpdateOn >" => date('Y-m-d', strtotime("-1 week")))
+							array("Physicalproduct.CreatedOn >" => date('Y-m-d', strtotime("-1 week")))
 						      )
 						),
 					  'fields' => array(
