080d9c5183b544a2e8ab4c69c60a173d139e34d6
Jyotiprakash Dhal <jyotiprakashd@mindfiresolutions.com>
2010-03-19 22:15:20 +0530

    Added new components

---
+++ controllers/components/downloads.php
@@ -0,0 +1,36 @@
+<?php
+Class DownloadsComponent extends Component
+{    
+    var $components = array('Session');
+
+    function checkLibraryDownload($libId)
+    {
+        $libraryInstance = ClassRegistry::init('Library');
+        $results = $libraryInstance->find('count',array('conditions' => array('library_download_limit >' => 'library_current_downloads'),'Library.id' => $libId));       
+        if($results > 0)
+        {
+            return 1;
+        }
+        else{
+            return 0;
+        }
+    }
+    
+    function checkPatronDownload($patId,$libId)
+    {
+        $downloadInstance = ClassRegistry::init('Download');
+        $libraryInstance = ClassRegistry::init('Library');
+        $libraryResults = $libraryInstance->find('all',array('conditions' => array('Library.id' => $libId)));        
+        $patronLimit = $libraryResults['0']['Library']['library_user_download_limit'];        
+        $results = $downloadInstance->find('count',array('conditions' => array('library_id >' => $libId),'patron_id' => $patId));
+        if($results < $patronLimit)
+        {
+            return 1;
+        }
+        else{
+            return 0;
+        }
+    }
+}
+    
+    
