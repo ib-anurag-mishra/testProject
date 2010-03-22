6ee0fcf1c9f9138a2de10f2828efb4f4c65b5360
Jyotiprakash Dhal <jyotiprakashd@mindfiresolutions.com>
2010-03-22 21:12:50 +0530

    Fixed the issue for patron download control

--- controllers/components/downloads.php
+++ controllers/components/downloads.php
@@ -6,7 +6,7 @@ Class DownloadsComponent extends Component
     function checkLibraryDownload($libId)
     {
         $libraryInstance = ClassRegistry::init('Library');
-        $results = $libraryInstance->find('count',array('conditions' => array('library_download_limit >' => 'library_current_downloads'),'Library.id' => $libId));       
+        $results = $libraryInstance->find('count',array('conditions' => array('library_download_limit >' => 'library_current_downloads',),'Library.id' => $libId));       
         if($results > 0)
         {
             return 1;
@@ -22,7 +22,7 @@ Class DownloadsComponent extends Component
         $libraryInstance = ClassRegistry::init('Library');
         $libraryResults = $libraryInstance->find('all',array('conditions' => array('Library.id' => $libId)));        
         $patronLimit = $libraryResults['0']['Library']['library_user_download_limit'];        
-        $results = $downloadInstance->find('count',array('conditions' => array('library_id >' => $libId),'patron_id' => $patId));
+        $results = $downloadInstance->find('count',array('conditions' => array('library_id' => $libId),'patron_id' => $patId));        
         if($results < $patronLimit)
         {
             return 1;
