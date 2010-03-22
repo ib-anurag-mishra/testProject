85f172ed92ecd99d88e2b3f5ab8d357a5d246cc4
rob@maycreate.com <rob@maycreate.com>
2010-03-22 14:32:25 -0400

    fixed the path issue for download process

--- webroot/js/freegal.js
+++ webroot/js/freegal.js
@@ -30,10 +30,10 @@ function userDownload(prodId,downloadUrl1,downloadUrl2,downloadUrl3)
 	var finalURL = downloadUrl1;
 	finalURL += downloadUrl2;
 	finalURL += downloadUrl3;
-	var data = "prodId="+prodId;
+	var data = "prodId="+prodId;	
 	jQuery.ajax({
 	type: "post",  // Request method: post, get
-	url: webroot+"/homes/userDownload", // URL to request
+	url: webroot+"homes/userDownload", // URL to request
 	data: data,  // post data
 	success: function(response) {
 	location.href = unescape(finalURL);
