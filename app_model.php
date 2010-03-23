bf7b6a59d0603278c7e00e1045107b68acc95f3f
Jyotiprakash Dhal <jyotiprakashd@mindfiresolutions.com>
2010-03-23 21:13:14 +0530

    Modified for adding memcache

--- app_model.php
+++ app_model.php
@@ -1,28 +1,67 @@
 <?php
 class AppModel extends Model {
  
-    function find($conditions = null, $fields = array(), $order = null, $recursive = null) {
- $doQuery = true;
-     // check if we want the cache
-     if (!empty($fields['cache'])) {
-  $cacheConfig = null;
-  // check if we have specified a custom config, e.g. different expiry time
-  if (!empty($fields['cacheConfig']))
-      $cacheConfig = $fields['cacheConfig'];
- 
-  $cacheName = $this->name . '-' . $fields['cache'];
-     
-      // if so, check if the cache exists
-      if (($data = Cache::read($cacheName, $cacheConfig)) === false) {
-       $data = parent::find($conditions, $fields, $order, $recursive);
-       Cache::write($cacheName, $data, $cacheConfig);
-      }
-      $doQuery = false;
-     }
- if ($doQuery)
-     $data = parent::find($conditions, $fields, $order, $recursive);
- return $data;
+    function find($conditions = null, $fields = array(), $order = null, $recursive = null)
+    {
+        $doQuery = true;
+        // check if we want the cache
+        if (!empty($fields['cache'])) {
+         $cacheConfig = null;
+         // check if we have specified a custom config, e.g. different expiry time
+         if (!empty($fields['cacheConfig']))
+             $cacheConfig = $fields['cacheConfig'];
+        
+         $cacheName = $this->name . '-' . $fields['cache'];
+            
+             // if so, check if the cache exists
+             if (($data = Cache::read($cacheName, $cacheConfig)) === false) {
+              $data = parent::find($conditions, $fields, $order, $recursive);
+              Cache::write($cacheName, $data, $cacheConfig);
+             }
+             $doQuery = false;
+            }
+        if ($doQuery)
+            $data = parent::find($conditions, $fields, $order, $recursive);
+        return $data;
     }
- 
+    
+    function paginate ($conditions, $fields, $order, $limit, $page = 1, $recursive = null, $extra = array())
+    {
+        $args = func_get_args();
+        $uniqueCacheId = '';
+        foreach ($args as $arg) {
+                $uniqueCacheId .= serialize($arg);
+        }
+        if (!empty($extra['contain'])) {
+                $contain = $extra['contain'];	
+        }
+        $uniqueCacheId = md5($uniqueCacheId);
+        $pagination = Cache::read('pagination-'.$this->alias.'-'.$uniqueCacheId, 'paginate_cache');
+        if (empty($pagination)) {
+                $pagination = $this->find('all', compact('conditions', 'fields', 'order', 'limit', 'page', 'recursive', 'group', 'contain'));
+                Cache::write('pagination-'.$this->alias.'-'.$uniqueCacheId, $pagination, 'paginate_cache');
+        }
+        return $pagination;
+    }
+
+    function paginateCount ($conditions = null, $recursive = 0, $extra = array())
+    {
+        $args = func_get_args();
+        $uniqueCacheId = '';
+        foreach ($args as $arg) {
+                $uniqueCacheId .= serialize($arg);
+        }
+        $uniqueCacheId = md5($uniqueCacheId);
+        if (!empty($extra['contain'])) {
+                $contain = $extra['contain'];	
+        }
+    
+        $paginationcount = Cache::read('paginationcount-'.$this->alias.'-'.$uniqueCacheId, 'paginate_cache');
+        if (empty($paginationcount)) {
+                $paginationcount = $this->find('count', compact('conditions', 'contain', 'recursive'));
+                Cache::write('paginationcount-'.$this->alias.'-'.$uniqueCacheId, $paginationcount, 'paginate_cache');
+        }
+        return $paginationcount;
+    } 
 }
 ?>
