d837e2a18b3e56c1e5afaa99e0e75c8a3ffda3ca
Jyotiprakash Dhal <jyotiprakashd@mindfiresolutions.com>
2010-03-24 14:33:51 +0530

    Modified to fetch results from libraries table only

--- controllers/components/downloads.php
+++ controllers/components/downloads.php
@@ -6,6 +6,7 @@ Class DownloadsComponent extends Component
     function checkLibraryDownload($libId)
     {
         $libraryInstance = ClassRegistry::init('Library');
+        $libraryInstance->recursive = -1;
         $results = $libraryInstance->find('count',array('conditions' => array('library_download_limit >' => 'library_current_downloads',),'Library.id' => $libId));       
         if($results > 0)
         {
@@ -20,6 +21,7 @@ Class DownloadsComponent extends Component
     {
         $downloadInstance = ClassRegistry::init('Download');
         $libraryInstance = ClassRegistry::init('Library');
+        $libraryInstance->recursive = -1;
         $libraryResults = $libraryInstance->find('all',array('conditions' => array('Library.id' => $libId)));        
         $patronLimit = $libraryResults['0']['Library']['library_user_download_limit'];        
         $results = $downloadInstance->find('count',array('conditions' => array('library_id' => $libId),'patron_id' => $patId));        
