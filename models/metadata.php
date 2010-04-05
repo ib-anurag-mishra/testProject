a3de2bc31ed3c4b299dd4f9e40e93013ed15f4a0
Jyotiprakash Dhal <jyotiprakashd@mindfiresolutions.com>
2010-04-05 18:47:25 +0530

    Added pagination for genre view page

--- models/metadata.php
+++ models/metadata.php
@@ -31,6 +31,13 @@ class Metadata extends AppModel {
 	{
 		$getTrackData = $this->find('first', array('conditions' => array('ProdID' => $id),'fields' => array('Title','Artist')));
 		return $getTrackData;
-	} 
+	}
+	function paginateCount($conditions = null, $recursive = 0, $extra = array())
+	{	    
+	    $genre = $conditions['Genre.Genre'];
+	    $sql = "SELECT distinct Artist AS `count` FROM `METADATA` AS `Metadata` LEFT JOIN `Genre` AS `Genre` ON (`Genre`.`ProdID` = `Metadata`.`ProdId`) LEFT JOIN `PhysicalProduct` AS `Physicalproduct` ON (`Physicalproduct`.`ProdID` = `Metadata`.`ProdId`) WHERE `Genre`.`Genre` = '$genre'";	   
+	    $results = $this->query($sql);
+	    return count($results);
+	}
 	
 }
