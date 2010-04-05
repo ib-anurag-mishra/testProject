6b60c1a6ba1cf03a45b1950d5818f489a2866a5c
Jyotiprakash Dhal <jyotiprakashd@mindfiresolutions.com>
2010-04-05 18:49:05 +0530

    Added pagination

--- views/helpers/metadata.php
+++ views/helpers/metadata.php
@@ -4,9 +4,21 @@ class MetadataHelper extends AppHelper {
     var $uses = array('Metadata');
     
     function getArtistDetails($artist) {
-        $metadataInstance = ClassRegistry::init('Metadata');                
-        $metadataInstance->recursive = 1;
-        $metadataDetails = $metadataInstance->find('first', array('conditions' => array('Artist' => $artist)));
+        $metadataInstance = ClassRegistry::init('Metadata');
+        $metadataInstance->Behaviors->attach('Containable');
+        $metadataInstance->recursive = 2;
+        $metadataDetails = $metadataInstance->find('first', array('conditions' =>
+                                                                    array('Artist' => $artist),                                                       
+                                                                  'contain' => array(
+                                                                        'Genre' => array(
+							'fields' => array(
+								'Genre.Genre'								
+								)),
+                                                         'Physicalproduct' => array(
+							'fields' => array(
+								'Physicalproduct.ArtistText'								
+								)
+							))));        
         return $metadataDetails;
     }
 }
