865a3b37c87bbdea0367d243098c2a95dc6846cc
Jyotiprakash Dhal <jyotiprakashd@mindfiresolutions.com>
2010-04-15 19:27:13 +0530

    removed unused function

--- models/home.php
+++ models/home.php
@@ -2,12 +2,6 @@
 class Home extends AppModel
 {
   var $name = 'Home';
-  var $useTable = 'METADATA';
-  
-  //Function to get random songs
-  function getSongs() {
-     $randomSongs = $this->find('all', array('limit' => 8));
-     return $randomSongs;
-  }
+  var $useTable = 'METADATA';  
 }  
 ?>
