aaa3742216f6658feb84f1ca82745e183f6d4f58
Jyotiprakash Dhal <jyotiprakashd@mindfiresolutions.com>
2010-04-15 19:26:20 +0530

    modified for patron account process

--- controllers/users_controller.php
+++ controllers/users_controller.php
@@ -10,18 +10,23 @@ Class UsersController extends AppController
    var $helpers = array('Html','Ajax','Javascript','Form', 'User', 'Library');
    var $layout = 'admin';
    var $components = array('Session','Auth','Acl','PasswordHelper','Email');
-   var $uses = array('User','Group', 'Library');
+   var $uses = array('User','Group', 'Library', 'Currentpatron', 'Download');
    
    function before_filter() {
      $this->Auth->userModel = 'User';
      $this->Auth->loginAction = array('controller' => 'users', 'action' => 'admin_login');
      $this->Auth->loginRedirect = array('controller' => 'users', 'action' => 'admin_index');
      $this->Auth->logoutRedirect = '/users/admin_login';
-     $this->Auth->allow('admin_login','admin_logout');
+     $this->Auth->allow('admin_login','admin_logout','forgot_password');
      $this->set('username', $this->Session->read('Auth.User.username'));
    }
  
    function admin_index() {
+         $userType = $this->Session->read('Auth.User.type_id');
+         if($userType == '5'){
+            $this->redirect('/homes/index');
+            $this->Auth->autoRedirect = false;     
+         }
         //takes to the default admin home page
          $this->set('username', $this->Session->read('Auth.User.username'));  //setting the username to display on the header 
    }
@@ -35,10 +40,14 @@ Class UsersController extends AppController
       if (empty($this->data)) {
          $this->Session->delete('Message.auth');
       }
-      if ($this->Session->read('Auth.User'))
-      {
-       $this->redirect('/users/admin_index');
-       $this->Auth->autoRedirect = false;
+      if ($this->Session->read('Auth.User')) {
+         $userType = $this->Session->read('Auth.User.type_id');
+         if($userType == '5'){
+            $this->redirect('/homes/index');
+            $this->Auth->autoRedirect = false;     
+         }
+         $this->redirect('/users/admin_index');
+         $this->Auth->autoRedirect = false;
       }
    }
     
@@ -49,7 +58,113 @@ Class UsersController extends AppController
    function admin_logout() {
      $this->redirect($this->Auth->logout()); 
    }
-    
+   
+   function login(){     
+      if ($this->Session->read('Auth.User')){
+         $userType = $this->Session->read('Auth.User.type_id');
+         if($userType == '5'){
+            $this->redirect('/homes/index');
+            $this->Auth->autoRedirect = false;     
+         }
+      }
+   }
+   
+   function index(){      
+      /*echo "<PRE>";
+      print_r($_SESSION);exit;*/
+      $patronId = $this->Session->read('Auth.User.id');      
+      $typeId = $this->Session->read('Auth.User.type_id');
+      if($typeId == '5'){
+         $libraryId = $this->Session->read('Auth.User.library_id');
+         $this->Library->recursive = -1;
+         $libraryArr = $this->Library->find('first',array(                                                
+                                                'conditions' => array('Library.id' => $libraryId)
+                                                )
+                                            );         
+         $authMethod = $libraryArr['Library']['library_authentication_method'];        
+         if($authMethod == 'user_account'){
+            $currentPatron = $this->Currentpatron->find('all', array('conditions' => array('libid' => $libraryId, 'patronid' => $patronId)));           
+            if(count($currentPatron) > 0)
+            {
+                $modifiedTime = strtotime($currentPatron[0]['Currentpatron']['modified']);                           
+                $date = strtotime(date('Y-m-d H:i:s'));              
+                if(!(isset($_SESSION['patron'])))
+                {               
+                    if(($date-$modifiedTime) > 60)
+                    {
+                        $updateArr = array();
+                        $updateArr['id'] = $currentPatron[0]['Currentpatron']['id'];                
+                        $updateArr['created'] = date('Y-m-d H:i:s');
+                        $updateArr['session_id'] = session_id();
+                        $this->Currentpatron->save($updateArr);
+                    }
+                    else
+                    {                
+                        $this -> Session -> setFlash("This account is already active.");
+                        $this->redirect(array('controller' => 'homes', 'action' => 'aboutus'));
+                    }
+                }
+                else
+                {
+                    $sessionId = session_id();                    
+                    if($currentPatron[0]['Currentpatron']['session_id'] != $sessionId)
+                    {                        
+                        if(($date-$modifiedTime) > 60)
+                        {                            
+                            $updateArr = array();
+                            $updateArr['id'] = $currentPatron[0]['Currentpatron']['id'];                
+                            $updateArr['created'] = date('Y-m-d H:i:s');
+                            $updateArr['session_id'] = session_id();
+                            $this->Currentpatron->save($updateArr);
+                        }
+                        else
+                        {                            
+                            $this -> Session -> setFlash("This account is already active.");
+                            $this->redirect(array('controller' => 'homes', 'action' => 'aboutus'));
+                        }                  
+                    }                    
+                }
+            }
+            else
+            {                
+                $insertArr['libid'] = $libraryId;
+                $insertArr['patronid'] = $patronId;
+                $insertArr['session_id'] = session_id();                 
+                $this->Currentpatron->save($insertArr);
+            }
+            $this->Session->write("library", $libraryId);
+            $this->Session->write("patron", $patronId);
+            $isApproved = $this->Currentpatron->find('first',array('conditions' => array('libid' => $libraryId,'patronid' => $patronId)));            
+            $this->Session->write("approved", $isApproved['Currentpatron']['is_approved']);
+            $startDate = date('Y-m-d', mktime(0, 0, 0, date('m'), date('d')-(date('w', mktime(0, 0, 0, date('m'), date('d'), date('Y')))-1), date('Y')))." 00:00:00";
+            $endDate = date('Y-m-d', mktime(0, 0, 0, date('m'), date('d')+(7-date('w', mktime(0, 0, 0, date('m'), date('d'), date('Y')))), date('Y')))." 23:59:59";            
+            $this->Session->write("downloadsAllotted", $libraryArr['Library']['library_user_download_limit']);
+            $results =  $this->Download->find('count',array('conditions' => array('library_id' => $libraryId,'patron_id' => $patronId,'created BETWEEN ? AND ?' => array($startDate, $endDate))));
+            $this ->Session->write("downloadsUsed", $results);            
+            if($libraryArr['Library']['library_block_explicit_content'] == '1')
+            {
+                $this ->Session->write("block", 'yes');
+            }
+            else{
+                $this ->Session->write("block", 'no');
+            }
+            $this->redirect(array('controller' => 'homes', 'action' => 'index'));
+         }
+         else{
+            $this->redirect($this->Auth->logout());
+         }
+      }
+      else{
+         $this->redirect($this->Auth->logout());
+      }
+   }
+   
+   function logout() {
+      session_destroy(); 
+      $this->redirect($this->Auth->logout()); 
+   }
+   
+   
    /*
     Function Name : listuser
     Desc : action for listing all the admin users
@@ -291,6 +406,27 @@ Class UsersController extends AppController
     $this->Email->to = $Patron['User']['email'];
     $this->Email->subject = 'FreegalMusic - Patron account password changed!!';
     $result = $this->Email->send(); 
+   }
+   
+   function my_account(){
+      $this->layout = 'home';
+      $patronId = $this->Session->read('patron');
+      $this->set('getData', $this->User->getuserdata($patronId));
+      if(isset($this->data)){
+         $this->data['User']['type_id'] = 5;
+         $getData['User'] = $this->data['User'];
+         $getData['type_id'] = 5;        
+         $this->set('getData', $getData);        
+         if(trim($this->data['User']['password']) == "48d63321789626f8844afe7fdd21174eeacb5ee5"){            
+            // do not update the password
+            $this->data['User']= $this->User->arrayremovekey($this->data['User'],'password');
+         }         
+         $this->User->set($this->data['User']);        
+         if($this->User->save()){         
+            $this->Session->setFlash('Data has been saved successfully!');
+            $this->redirect($this->webroot.'users/my_account');
+         }
+      }
    }
 }
 ?>
