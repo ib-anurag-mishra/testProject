01f3881c6d1154481f85a63f52971c62c8fb6370
Rob Richmond <rob@maycreate.com>
2010-04-16 11:25:44 -0400

    missing Action and Controller

--- app_error.php
+++ app_error.php
@@ -1,4 +1,24 @@
 <?php
 class AppError extends ErrorHandler {
 	var $layout = 'home';
+	
+	function missingAction($messages = array()) {
+		// return parent::missingAction($messages);
+		
+		$this->controller->redirect(array_merge(
+			array('controller' => 'homes', 'action' => 'index'),
+			$this->controller->params['pass']
+		));
+		
+	}
+	
+	function missingController($messages = array()) {
+		// return parent::missingController($messages);
+		
+		$this->controller->redirect(array_merge(
+			array('controller' => 'homes', 'action' => 'index'),
+			$this->controller->params['pass']
+		));
+		
+	}
 }
