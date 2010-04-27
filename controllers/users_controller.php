541201882c724d2085c46647a593d50767160b58
Rob Richmond <rob@maycreate.com>
2010-04-27 09:06:56 -0400

    corrected error is spelling

--- controllers/users_controller.php
+++ controllers/users_controller.php
@@ -624,7 +624,7 @@ Class UsersController extends AppController
                                              );            
             if(count($existingLibraries) == 0)
             {
-                $this -> Session -> setFlash("This is not a valid creadential.");
+                $this -> Session -> setFlash("This is not a valid credential.");
                 $this->redirect(array('controller' => 'users', 'action' => 'inlogin'));
             }        
             else{
