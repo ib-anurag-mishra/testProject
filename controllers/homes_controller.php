f512b755ac996b985a060d1fd4364e6fa6f82d70
rob@maycreate.com <rob@maycreate.com>
2010-05-04 10:19:37 -0400

    addded link for returning to previous page

--- controllers/homes_controller.php
+++ controllers/homes_controller.php
@@ -477,20 +477,20 @@ class HomesController extends AppController
 
     function aboutus(){
 	if(isset($this->params['pass'][0]) && $this->params['pass'][0] == "js_err") {
-	    $this->Session->destroy('User');
 	    if(isset($_SESSION['referral_url']) && ($_SESSION['referral_url'] != '')){
 		$url = $_SESSION['referral_url'];
 	    }
 	    elseif(isset($_SESSION['innovative']) && ($_SESSION['innovative'] != '')){
-		$url = 'users/ilogin';
+		$url = $this->webroot.'users/ilogin';
 	    }
 	    elseif(isset($_SESSION['innovative_wo_pin']) && ($_SESSION['innovative_wo_pin'] != '')){
-		$url = 'users/inlogin';
+		$url = $this->webroot.'users/inlogin';
 	    }
-	    else{            
-	       $url = 'users/login';
+	    else{
+	       $url = $this->webroot.'users/login';
 	    }
-            $this -> Session -> setFlash("Javascript is required to use this website. For the best experience, please enable javascript and <a href='".$this->webroot.$url."'>Click Here</a> to try again. <a href='https://www.google.com/adsense/support/bin/answer.py?hl=en&answer=12654' target='_blank'>Click Here</a> for the steps to enable javascript in different type of browsers.");
+	    $this->Session->destroy('User');
+           $this -> Session -> setFlash("Javascript is required to use this website. For the best experience, please enable javascript and <a href='".$url."'>Click Here</a> to try again. <a href='https://www.google.com/adsense/support/bin/answer.py?hl=en&answer=12654' target='_blank'>Click Here</a> for the steps to enable javascript in different type of browsers.");
 	}
 	$this->layout = 'home';
     }
