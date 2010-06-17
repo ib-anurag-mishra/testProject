234994ebbabfc3527e2e2118e509694d1bd83f5c
Abhijit Pattanaik <abhijit.pattanaik@mindfiresolutions.com>
2010-06-17 18:34:11 +0530

    added changes for cake session

--- views/helpers/wishlist.php
+++ views/helpers/wishlist.php
@@ -5,8 +5,8 @@ class WishlistHelper extends AppHelper {
     
     function getWishlistData($id) {
         $wishlistInstance = ClassRegistry::init('Wishlist');
-        $libraryId = $_SESSION['library'];
-        $patronId = $_SESSION['patron'];        
+        $libraryId = $this->Session->read('library');
+        $patronId = $this->Session->read('patron');        
         $wishlistDetails = $wishlistInstance->find('all', array('conditions' => array('library_id' => $libraryId,'patron_id' => $patronId, 'ProdID' => $id)));
         if(count($wishlistDetails) != 0) {
             return "Added to Wishlist";
@@ -18,8 +18,8 @@ class WishlistHelper extends AppHelper {
     
     function getWishlistCount() {
         $wishlistInstance = ClassRegistry::init('Wishlist');
-        $libraryId = $_SESSION['library'];
-        $patronId = $_SESSION['patron'];        
+        $libraryId = $this->Session->read('library');
+        $patronId = $this->Session->read('patron');        
         $wishlistCount = $wishlistInstance->find('count', array('conditions' => array('library_id' => $libraryId,'patron_id' => $patronId)));
         return $wishlistCount;
     }
