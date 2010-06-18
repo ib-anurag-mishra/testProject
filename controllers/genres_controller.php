b400dee12edd3b226c57922c975d4d0b884ee2e4
Abhijit Pattanaik <abhijit.pattanaik@mindfiresolutions.com>
2010-06-18 16:31:07 +0530

    commented out session destroy

--- controllers/genres_controller.php
+++ controllers/genres_controller.php
@@ -23,12 +23,12 @@ Class GenresController extends AppController
 		if(in_array($this->action,$libraryCheckArr)) {
 		  $validPatron = $this->ValidatePatron->validatepatron();
 			if($validPatron == '0') {
-				$this->Session->destroy();
+				//$this->Session->destroy();
 				$this -> Session -> setFlash("Sorry! Your session has expired.  Please log back in again if you would like to continue using the site.");
 				$this->redirect(array('controller' => 'homes', 'action' => 'aboutus'));
 			}
 			else if($validPatron == '2') {
-				$this->Session->destroy();
+				//$this->Session->destroy();
 				$this -> Session -> setFlash("Sorry! Your Library or Patron information is missing. Please log back in again if you would like to continue using the site.");
 				$this->redirect(array('controller' => 'homes', 'action' => 'aboutus'));			
 			}
