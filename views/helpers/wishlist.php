203b8ab8534c1960161e02d9e86a76c104471a63
Abhijit Pattanaik <abhijit.pattanaik@mindfiresolutions.com>
2010-06-25 18:38:56 +0530

    added session helper

--- views/helpers/wishlist.php
+++ views/helpers/wishlist.php
@@ -1,12 +1,13 @@
 <?php
 
 class WishlistHelper extends AppHelper {
+    var $helpers = array('Session');	
     var $uses = array('Wishlist');
     
     function getWishlistData($id) {
         $wishlistInstance = ClassRegistry::init('Wishlist');
         $libraryId = $this->Session->read('library');
-        $patronId = $this->Session->read('patron');        
+        $patronId = $this->Session->read('patron');  
         $wishlistDetails = $wishlistInstance->find('all', array('conditions' => array('library_id' => $libraryId,'patron_id' => $patronId, 'ProdID' => $id)));
         if(count($wishlistDetails) != 0) {
             return "Added to Wishlist";
@@ -19,7 +20,7 @@ class WishlistHelper extends AppHelper {
     function getWishlistCount() {
         $wishlistInstance = ClassRegistry::init('Wishlist');
         $libraryId = $this->Session->read('library');
-        $patronId = $this->Session->read('patron');        
+		$patronId = $this->Session->read('patron');
         $wishlistCount = $wishlistInstance->find('count', array('conditions' => array('library_id' => $libraryId,'patron_id' => $patronId)));
         return $wishlistCount;
     }
