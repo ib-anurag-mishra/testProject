ed0590c3895f8713672ad9e4da487ee3473bff5d
Abhijit Pattanaik <abhijit.pattanaik@mindfiresolutions.com>
2010-08-12 21:46:16 +0530

    modified sql queries to update variables table

--- config/schema/library_schema_12th_Aug.sql
+++ config/schema/library_schema_12th_Aug.sql
@@ -1,2 +1,3 @@
-ALTER TABLE `downloads` ADD `history` TINYINT( 1 ) NOT NULL DEFAULT '0' AFTER `ip`;
+INSERT INTO variables(library_id,authentication_variable,authentication_response) SELECT id,library_authentication_variable,library_authentication_response FROM libraries WHERE library_authentication_method IN ('innovative_var_wo_pin','sip2_var');# 4 row(s) affected.
+
 UPDATE variables SET created = NOW(),modified = NOW();
