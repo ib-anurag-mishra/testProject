0f4b0838575f30b00830b14567b8476db1cfe5c1
Abhijit Pattanaik <abhijit.pattanaik@mindfiresolutions.com>
2010-08-12 14:04:06 +0530

    created model for variables table

---
+++ models/variable.php
@@ -0,0 +1,14 @@
+<?php
+/*
+ File Name : wishlist.php
+ File Description : Models page for the  Wishlist functionality.
+ Author : maycreate
+*/
+
+class Variable extends AppModel
+{
+  var $name = 'Variable';
+  var $usetable = 'Variables';
+  
+}
+?>
