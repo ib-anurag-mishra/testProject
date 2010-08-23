a7781a770305e9057fddd421dce5c52cb3998993
Abhijit Pattanaik <abhijit.pattanaik@mindfiresolutions.com>
2010-08-23 15:03:16 +0530

    added model for url table

---
+++ models/libraryurl.php
@@ -0,0 +1,13 @@
+<?php
+/*
+ File Name : url.php
+ File Description : Models page for the  Multiple Referral Url.
+ Author : maycreate
+*/
+
+class Url extends AppModel
+{
+  var $name = 'Url';
+  var $usetable = 'urls';
+}
+?>
