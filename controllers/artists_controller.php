6a848e9a80423591efc64cc371e3061c552eaa74
Abhijit Pattanaik <abhijit.pattanaik@mindfiresolutions.com>
2010-09-14 17:08:54 +0530

    modified query on artist view page

--- controllers/artists_controller.php
+++ controllers/artists_controller.php
@@ -419,7 +419,7 @@ Class ArtistsController extends AppController
 		$this -> paginate =  array('conditions' =>
 					array('and' =>
 						array(
-						    array('Album.ArtistText' => base64_decode($id),'Album.DownloadStatus' => 1,'Country.Territory' => $country),
+						    array('Album.ArtistText' => base64_decode($id),'Album.DownloadStatus' => 0,'Country.Territory' => $country),
 						    $condition
 						)
 					),
@@ -532,7 +532,7 @@ Class ArtistsController extends AppController
 					$array[$k1] = $v1; 
 				}
 			}
-			$res[$val] = $array;
+			$res[$key] = $array;
 		}
 	    $this->set('albumSongs',$res);
 	}
