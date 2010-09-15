966d9b17b665c4de0c3bfaab8b3ed66dd29eb1d6
Abhijit Pattanaik <abhijit.pattanaik@mindfiresolutions.com>
2010-09-15 21:11:32 +0530

    increased the time to fix the with See All link not working

--- controllers/genres_controller.php
+++ controllers/genres_controller.php
@@ -74,7 +74,7 @@ Class GenresController extends AppController
 													array("Physicalproduct.ReferenceID <> Physicalproduct.ProdID"),
 													array('Physicalproduct.DownloadStatus' => 1),
 													array('Physicalproduct.TrackBundleCount' => 0),
-													array("Physicalproduct.UpdateOn >" => date('Y-m-d', strtotime("-3 week"))),$cond
+													array("Physicalproduct.UpdateOn >" => date('Y-m-d', strtotime("-7 week"))),$cond
 												)
 											),
 											'fields' => array(
