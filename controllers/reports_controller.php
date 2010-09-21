87bb6c4a970f06e5bd7c884e7f0160447941d854
Abhijit Pattanaik <abhijit.pattanaik@mindfiresolutions.com>
2010-09-21 20:15:44 +0530

    added validation on librarydownload  report page

--- controllers/reports_controller.php
+++ controllers/reports_controller.php
@@ -48,18 +48,20 @@ Class ReportsController extends AppController
             else {
                 $this->Report->setValidation('reports_manual');
             }
-			if($library_id == 'all'){
-				$sql = "SELECT id from libraries where library_territory = '".$territory."'";
-				$result = mysql_query($sql);
-				while ($row = mysql_fetch_assoc($result)) {
-					$all_Ids = $all_Ids.$row["id"].",";
-				}		  
-				$lib_condition = "and library_id IN (".rtrim($all_Ids,",'").")";
-				$this->set('libraries_download', $this->Library->find('list', array('fields' => array('Library.library_name','Library.library_available_downloads'),'conditions' => array('Library.id IN ('.rtrim($all_Ids,",").')'), 'order' => 'Library.library_name ASC', 'recursive' => -1)));
+			if($territory != ''){
+				if($library_id == 'all'){
+					$sql = "SELECT id from libraries where library_territory = '".$territory."'";
+					$result = mysql_query($sql);
+					while ($row = mysql_fetch_assoc($result)) {
+						$all_Ids = $all_Ids.$row["id"].",";
+					}		  
+					$lib_condition = "and library_id IN (".rtrim($all_Ids,",'").")";
+					$this->set('libraries_download', $this->Library->find('list', array('fields' => array('Library.library_name','Library.library_available_downloads'),'conditions' => array('Library.id IN ('.rtrim($all_Ids,",").')'), 'order' => 'Library.library_name ASC', 'recursive' => -1)));
+				}
+				else{
+					$this->set('libraries_download', $this->Library->find('list', array('fields' => array('Library.library_name','Library.library_available_downloads'),'conditions' => array('Library.id = '.$library_id,'Library.library_territory= "'.$territory.'"'),'order' => 'Library.library_name ASC', 'recursive' => -1)));			
+				}
 			}
-			else{
-				$this->set('libraries_download', $this->Library->find('list', array('fields' => array('Library.library_name','Library.library_available_downloads'),'conditions' => array('Library.id = '.$library_id,'Library.library_territory= "'.$territory.'"'),'order' => 'Library.library_name ASC', 'recursive' => -1)));			
-			}			
             if($this->Report->validates()) {
                 if($this->data['Report']['reports_daterange'] == 'day') {
                     list($downloads, $patronDownloads, $genreDownloads) = $this->Download->getDaysDownloadInformation($library_id, $this->data['Report']['date'], $this->data['Report']['Territory']);
@@ -125,17 +127,19 @@ Class ReportsController extends AppController
             else {
                 $this->Report->setValidation('reports_manual');
             }
-			if($library_id == 'all'){
-				$sql = "SELECT id from libraries where library_territory = '".$territory."'";
-				$result = mysql_query($sql);
-				while ($row = mysql_fetch_assoc($result)) {
-					$all_Ids = $all_Ids.$row["id"].",";
-				}		  
-				$lib_condition = "and library_id IN (".rtrim($all_Ids,",").")";
-				$this->set('libraries_download', $this->Library->find('list', array('fields' => array('Library.library_name','Library.library_available_downloads'),'conditions' => array('Library.id IN ('.rtrim($all_Ids,",'").')'), 'order' => 'Library.library_name ASC', 'recursive' => -1)));
-			}
-			else{
-				$this->set('libraries_download', $this->Library->find('list', array('fields' => array('Library.library_name','Library.library_available_downloads'),'conditions' => array('Library.id = '.$library_id,'Library.library_territory= "'.$territory.'"'),'order' => 'Library.library_name ASC', 'recursive' => -1)));			
+			if($territory != ''){
+				if($library_id == 'all'){
+					$sql = "SELECT id from libraries where library_territory = '".$territory."'";
+					$result = mysql_query($sql);
+					while ($row = mysql_fetch_assoc($result)) {
+						$all_Ids = $all_Ids.$row["id"].",";
+					}		  
+					$lib_condition = "and library_id IN (".rtrim($all_Ids,",'").")";
+					$this->set('libraries_download', $this->Library->find('list', array('fields' => array('Library.library_name','Library.library_available_downloads'),'conditions' => array('Library.id IN ('.rtrim($all_Ids,",").')'), 'order' => 'Library.library_name ASC', 'recursive' => -1)));
+				}
+				else{
+					$this->set('libraries_download', $this->Library->find('list', array('fields' => array('Library.library_name','Library.library_available_downloads'),'conditions' => array('Library.id = '.$library_id,'Library.library_territory= "'.$territory.'"'),'order' => 'Library.library_name ASC', 'recursive' => -1)));			
+				}
 			}			
             if($this->Report->validates()) {
                 if($this->data['Report']['reports_daterange'] == 'day') {
@@ -190,18 +194,20 @@ Class ReportsController extends AppController
             else {
                 $this->Report->setValidation('reports_manual');
             }
-			if($library_id == 'all'){
-				$sql = "SELECT id from libraries where library_territory = '".$territory."'";
-				$result = mysql_query($sql);
-				while ($row = mysql_fetch_assoc($result)) {
-					$all_Ids = $all_Ids.$row["id"].",";
-				}		  
-				$lib_condition = "and library_id IN (".rtrim($all_Ids,",").")";
-				$this->set('libraries_download', $this->Library->find('list', array('fields' => array('Library.library_name','Library.library_available_downloads'),'conditions' => array('Library.id IN ('.rtrim($all_Ids,",'").')'), 'order' => 'Library.library_name ASC', 'recursive' => -1)));
-			}
-			else{
-				$this->set('libraries_download', $this->Library->find('list', array('fields' => array('Library.library_name','Library.library_available_downloads'),'conditions' => array('Library.id = '.$library_id,'Library.library_territory= "'.$territory.'"'),'order' => 'Library.library_name ASC', 'recursive' => -1)));			
-			}			
+			if($territory != ''){
+				if($library_id == 'all'){
+					$sql = "SELECT id from libraries where library_territory = '".$territory."'";
+					$result = mysql_query($sql);
+					while ($row = mysql_fetch_assoc($result)) {
+						$all_Ids = $all_Ids.$row["id"].",";
+					}		  
+					$lib_condition = "and library_id IN (".rtrim($all_Ids,",'").")";
+					$this->set('libraries_download', $this->Library->find('list', array('fields' => array('Library.library_name','Library.library_available_downloads'),'conditions' => array('Library.id IN ('.rtrim($all_Ids,",").')'), 'order' => 'Library.library_name ASC', 'recursive' => -1)));
+				}
+				else{
+					$this->set('libraries_download', $this->Library->find('list', array('fields' => array('Library.library_name','Library.library_available_downloads'),'conditions' => array('Library.id = '.$library_id,'Library.library_territory= "'.$territory.'"'),'order' => 'Library.library_name ASC', 'recursive' => -1)));			
+				}
+			}		
             if($this->Report->validates()) {
                 if($this->data['Report']['reports_daterange'] == 'day') {
                     list($downloads, $patronDownloads, $genreDownloads) = $this->Download->getDaysDownloadInformation($library_id, $this->data['Report']['date'], $this->data['Report']['Territory']);
