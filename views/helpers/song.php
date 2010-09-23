45e11fa1af7b6fdebee13f65bd02a35548e5c23d
Abhijit Pattanaik <abhijit.pattanaik@mindfiresolutions.com>
2010-09-23 18:18:24 +0530

    added helpers for Song

---
+++ views/helpers/song.php
@@ -0,0 +1,30 @@
+<?php
+
+class SongHelper extends AppHelper {
+    var $uses = array('Song');
+    
+    function getDownloadData($id) {
+        $songInstance = ClassRegistry::init('Song');
+        $details = $songInstance->find('all', array(
+		'conditions'=>array('Song.ProdID' => $id),
+		'fields' => array(
+			'Song.ProdID',
+			'Song.ProductID',
+			'Song.Title',
+			'Song.SongTitle',
+			'Song.Artist',
+			'Song.ISRC'
+		),
+		'contain' => array(										
+			'Full_Files' => array(
+				'fields' => array(
+					'Full_Files.CdnPath',
+					'Full_Files.SaveAsName'
+					),                             
+			)
+	)));
+        return  $details;
+    }
+}
+
+?>
