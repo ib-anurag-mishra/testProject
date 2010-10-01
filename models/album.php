063da7b8c8d1de8bd6bce7ea6b0cdd33f52b853f
Abhijit Pattanaik <abhijit.pattanaik@mindfiresolutions.com>
2010-10-01 13:54:57 +0530

    added model for Albums table

---
+++ models/album.php
@@ -0,0 +1,44 @@
+<?
+/*
+ File Name : song.php
+ File Description : Models page for the  Songs table.
+ Author : maycreate
+ */
+
+class Album extends AppModel
+{
+	var $name = 'Albums';
+	var $useDbConfig = 'freegal';
+	var $useTable = 'Albums';
+	var $primaryKey = 'ProdID';
+	var $actsAs = array('Containable');
+	var $uses = array('Featuredartist');
+	var $hasOne = array(
+		'Genre' => array(
+			'className' => 'Genre',
+			'foreignKey' => 'ProdID'
+		),
+		'Country' => array(
+					'className' => 'Country',
+					'foreignKey' => 'ProdID'
+		),							
+	);
+	var $hasMany = array(
+			'Song' => array(
+				'className' => 'Song',
+				'foreignKey' => 'ReferenceID'
+			),
+	);	
+	var $belongsTo = array(
+		'Files' => array(
+			'className' => 'Files',
+			'foreignKey' => 'FileID'
+		)
+	);
+	function paginateCount($conditions = null, $recursive = 0, $extra = array()) {
+		$group = array('Album.ProdID');
+	    $results = $this->find('count', compact('conditions','recursive', 'group'));
+	    return $results;
+	}	
+}
+?>
