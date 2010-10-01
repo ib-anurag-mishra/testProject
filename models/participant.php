dc34368c2728487fee796c5b529bc61e19f37709
Abhijit Pattanaik <abhijit.pattanaik@mindfiresolutions.com>
2010-10-01 13:57:16 +0530

    modified for multiple db references

--- models/participant.php
+++ models/participant.php
@@ -8,6 +8,7 @@
 class Participant extends AppModel
 {
   var $name = 'Participant';
+  var $useDbConfig = 'freegal';  
   var $useTable = 'Participant';
   var $primaryKey = 'ProdID';
   var $actsAs = array('Containable');
