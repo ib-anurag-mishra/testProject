bbcfaa0d424e85453f43619a7ce1777a2bf05503
Abhijit Pattanaik <abhijit.pattanaik@mindfiresolutions.com>
2010-10-02 17:21:46 +0530

    modified to use DISTINCT

--- controllers/artists_controller.php
+++ controllers/artists_controller.php
@@ -502,7 +502,7 @@ Class ArtistsController extends AppController
 						)
 					),
 					'fields' => array(
-						'Album.ProdID',
+						'DISTINCT Album.ProdID',
 						'Album.Title',
 						'Album.ArtistText',
 						'Album.AlbumTitle',
