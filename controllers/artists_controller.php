c7c788a4fbfe464848d4b38e08c75043d1de845e
Abhijit Pattanaik <abhijit.pattanaik@mindfiresolutions.com>
2010-10-11 12:29:34 +0530

    changes downloadstatus value o to 1

--- controllers/artists_controller.php
+++ controllers/artists_controller.php
@@ -505,7 +505,7 @@ Class ArtistsController extends AppController
 		$this->paginate =  array('conditions' =>
 					array('and' =>
 						array(
-						    array('Album.ArtistText' => base64_decode($id),'Album.DownloadStatus' => 0,'Country.Territory' => $country),
+						    array('Album.ArtistText' => base64_decode($id),'Album.DownloadStatus' => 1,'Country.Territory' => $country),
 						    $condition
 						), "1 = 1 GROUP BY Album.ProdID"
 					),
