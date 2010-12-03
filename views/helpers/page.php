3147c214c9d7aa6100070cd5311cf0ff11717249
Abhijit Pattanaik <abhijit.pattanaik@mindfiresolutions.com>
2010-12-03 11:25:37 +0530

    made changes to show corrent contents for fregalmusic based on language

--- views/helpers/page.php
+++ views/helpers/page.php
@@ -4,8 +4,9 @@ class PageHelper extends AppHelper {
     var $uses = array('Page');
     
     function getPageContent($type) {
+		$language = 'en';
         $pageInstance = ClassRegistry::init('Page');
-        $pageDetails = $pageInstance->find('all', array('conditions' => array('page_name' => $type)));
+        $pageDetails = $pageInstance->find('all', array('conditions' => array('page_name' => $type,'language' => $language)));
         if(count($pageDetails) != 0) {
             return $pageDetails[0]['Page']['page_content'];
         }
