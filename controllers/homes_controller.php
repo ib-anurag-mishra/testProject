f08c27aa6eccffeafd007303f2f0981c2e025335
Deepak Biswal <deepakb@mindfiresolutions.com>
2010-12-04 17:20:10 +0530

    Changed the sphinx port.

--- controllers/homes_controller.php
+++ controllers/homes_controller.php
@@ -310,7 +310,7 @@ class HomesController extends AppController
 				
 				App::import('vendor', 'sphinxapi', array('file' => 'sphinxapi.php'));
 				$sphinx = array('matchMode' => SPH_MATCH_ALL);
-				$results = $this->Song->find('all', array('search' =>  'test', 'limit' =>10, 'sphinx' => $sphinx));
+				$results = $this->Song->find('all', array('search' =>  'test', 'limit' =>10, 'recursive' => 0, 'sphinx' => $sphinx));
 				$this->set('searchResults', $results);
 				print_r($results);
 				exit;
