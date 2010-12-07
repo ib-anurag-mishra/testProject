d8fe091af73ef0143e89a4b28cbef118673a761d
Deepak Biswal <deepakb@mindfiresolutions.com>
2010-12-07 13:16:40 +0530

    sphinx search.

--- controllers/homes_controller.php
+++ controllers/homes_controller.php
@@ -315,7 +315,6 @@ class HomesController extends AppController
 				print_r($data);
 				exit();
 				
-				
 				/*$this->set('searchKey','match=All&artist='.urlencode($artist).'&composer='.urlencode($composer).'&song='.urlencode($song).'&album='.$album.'&genre_id='.$genre);
 				if($composer == '') {
 					$this->Song->unbindModel(array('hasOne' => array('Participant')));
