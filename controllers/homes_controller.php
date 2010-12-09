f34fb1a5aa8a8f7f152fd6469cc17dfb848ef594
Deepak Biswal <deepakb@mindfiresolutions.com>
2010-12-09 14:30:26 +0530

    Sphinx testing.

--- controllers/homes_controller.php
+++ controllers/homes_controller.php
@@ -334,7 +334,6 @@ class HomesController extends AppController
 				}
 				
 				$searchResults = $this->paginate('Song');
-				print_r($searchResults);exit;
 				$this->set('searchResults', $searchResults);
 			}
 			else {
