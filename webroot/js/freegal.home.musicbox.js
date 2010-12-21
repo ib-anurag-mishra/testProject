292091eefc9a7ff5f0f97552219707805b601bc1
Deepak Biswal <deepakb@mindfiresolutions.com>
2010-12-21 21:08:48 +0530

    Worked on to tab design.

--- webroot/js/freegal.home.musicbox.js
+++ webroot/js/freegal.home.musicbox.js
@@ -16,15 +16,15 @@ function getMusicBox(type) {
 }
 
 function filterTD(div) {
-	$('#tb1').css('font-weight', 'normal');
-	$('#tb2').css('font-weight', 'normal');
 	if (div == 'tab1') {
 		$('#tab2').hide();
 		$('#tab1').fadeIn();
-		$('#tb1').css('font-weight', 'bold');
+		$('#t2').removeClass('active');
+		$('#t1').addClass('active');
 	} else {
 		$('#tab1').hide();
 		$('#tab2').fadeIn();
-		$('#tb2').css('font-weight', 'bold');
+		$('#t1').removeClass('active');
+		$('#t2').addClass('active');
 	}
 }
