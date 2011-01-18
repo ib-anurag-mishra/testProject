80203bafbde366dd34b250ad6fecdb90ec935b61
Abhijit Pattanaik <abhijit.pattanaik@mindfiresolutions.com>
2011-01-18 14:35:58 +0530

    added model for genreids table

---
+++ models/genreid.php
@@ -0,0 +1,14 @@
+<?php
+/*
+ File Name : wishlist.php
+ File Description : Models page for the  Wishlist functionality.
+ Author : maycreate
+*/
+
+class GenreId extends AppModel
+{
+  var $name = 'GenreId';
+  var $usetable = 'genreids';
+  
+}
+?>
