bb3f5789cf568f043c5f48f563549a8a2598646e
Abhijit Pattanaik <abhijit.pattanaik@mindfiresolutions.com>
2011-01-20 21:17:45 +0530

    made the changes for country check

--- controllers/homes_controller.php
+++ controllers/homes_controller.php
@@ -589,7 +589,7 @@ class HomesController extends AppController
 				}		
 				App::import('vendor', 'sphinxapi', array('file' => 'sphinxapi.php'));
 				//$sphinxFinalCondition = $searchParam." & "."@TrackBundleCount 0 & @DownloadStatus 1 & @Territory !".$nonMatchCountry." & @Territory ".$country." & ".$condSphinx;
-				$sphinxFinalCondition = $searchParam." & "."@TrackBundleCount 0 & @DownloadStatus 1 & ".$condSphinx;
+				$sphinxFinalCondition = $searchParam." & "."@Territory ".$country." & @TrackBundleCount 0 & @DownloadStatus 1 & ".$condSphinx;
 				if ($condSphinx == "") {
 					$sphinxFinalCondition = substr($sphinxFinalCondition, 0, -2);
 				}
