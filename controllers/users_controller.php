8b4327a6847e877cf47d2e6525a6af547f5ef7b5
Abhijit Pattanaik <abhijit.pattanaik@mindfiresolutions.com>
2011-02-08 15:25:47 +0530

    made changes on logout method for ezproxy

--- controllers/users_controller.php
+++ controllers/users_controller.php
@@ -307,7 +307,6 @@ Class UsersController extends AppController
 			elseif($this->Session->read('ezproxy') && ($this->Session->read('ezproxy') != '')){
 				if($this->Session->read('ezproxy_logout') != ''){
 					$redirectUrl = $this->Session->read('ezproxy_logout');
-					$redirectUrl = str_replace('login', 'logout',$redirectUrl);
 					$this->Session->destroy();
 					$this->redirect($redirectUrl, null, true);				
 				}
