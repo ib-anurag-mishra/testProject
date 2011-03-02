687029dc746743b0de955f615cb4e77a727bea93
Abhijit Pattanaik <abhijit.pattanaik@mindfiresolutions.com>
2011-03-02 12:30:42 +0530

    created a controller for different cache methods

---
+++ controllers/cache_controller.php
@@ -0,0 +1,27 @@
+<?php
+class CacheController extends AppController {
+    var $name = 'Cache';
+    var $autoLayout = false;
+    var $uses = array();
+
+    function cacheLogin() {
+			$libid = $_REQUEST['libid'];       
+			$patronid = $_REQUEST['patronid'];
+			$date = time();
+			$values = array(0 => $date, 1 => session_id());			
+			Cache::write("login_".$libid.$patronid, $values);
+			print "success";exit;
+    }
+    function cacheUpdate() {
+			$libid = $_REQUEST['libid'];       
+			$patronid = $_REQUEST['patronid'];
+			$date = time();
+			$values = array(0 => $date, 1 => session_id());			
+			Cache::write("login_".$libid.$patronid, $values);
+    }
+    function cacheDelete() {
+			$libid = $_REQUEST['libid'];       
+			$patronid = $_REQUEST['patronid'];	
+			Cache::delete("login_".$libid.$patronid);
+    }	
+}
