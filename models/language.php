f0ea14159132429970119251e5295e4adbe7472b
Abhijit Pattanaik <abhijit.pattanaik@mindfiresolutions.com>
2011-04-21 17:10:56 +0530

    addded new model for languages table

---
+++ models/language.php
@@ -0,0 +1,14 @@
+<?php
+/*
+ File Name : wishlist.php
+ File Description : Models page for the  Wishlist functionality.
+ Author : maycreate
+*/
+
+class Language extends AppModel
+{
+  var $name = 'Language';
+  var $usetable = 'languages';
+  
+}
+?>
