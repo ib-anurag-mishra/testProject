5ac4585b2b88d5182a0df7107de058f15a6aa6e2
Abhijit Pattanaik <abhijit.pattanaik@mindfiresolutions.com>
2011-04-28 12:56:50 +0530

    added caching to queries to get languages

--- views/helpers/language.php
+++ views/helpers/language.php
@@ -6,7 +6,7 @@ class LanguageHelper extends AppHelper {
     
     function getLanguage() {
         $languageInstance = ClassRegistry::init('Language');
-        $languageDetails =  $languageInstance->find('list', array('conditions' => array('status' => 'active'), 'fields' => array('id', 'full_name')));
+        $languageDetails =  $languageInstance->find('list', array('conditions' => array('status' => 'active'), 'fields' => array('id', 'full_name'), 'cache' => 'yes'));
         return $languageDetails;
     }
 }
