d7f6899f3cb887f755068d3a3d6aa5e8a2bf4ce6
Sandeep Mohanty <sandeep.mohanty@mindfiresolutions.com>
2011-06-07 10:13:08 +0530

    fixing the undefined offset issue for ihdlogin

--- controllers/users_controller.php
+++ controllers/users_controller.php
@@ -3018,6 +3018,8 @@ Class UsersController extends AppController
 					$data['url'] = $authUrl."/PATRONAPI/".$card."/".$pin."/pintest";
 					$authUrl = "https://auth.libraryideas.com/ihdlogin_validation";
 					$result = $this->AuthRequest->getAuthResponse($data,$authUrl);
+					echo $result;
+					exit;
 					$resultAnalysis = explode("|",$result);
 					$resultAnalysis[0] = preg_replace('/[\x00-\x1F\x80-\xFF]/', '', $resultAnalysis[0]);
 					$resultAnalysis[1] = preg_replace('/[\x00-\x1F\x80-\xFF]/', '', $resultAnalysis[1]);
