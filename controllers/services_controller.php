61dde9dda8c5e37defc1212026f2850ac93bf41b
Abhijit Pattanaik <abhijit.pattanaik@mindfiresolutions.com>
2011-07-27 18:29:03 +0530

    added changes for italian webservices

--- controllers/services_controller.php
+++ controllers/services_controller.php
@@ -187,6 +187,7 @@ class ServicesController extends AppController {
 	}
 	
 	function genre(){
+		if($this->params['pass'][3] == ''){
 		$consortium = $this->Consortium->find('all',array(
                                                 'conditions' => 
 												array('consortium_key' => $this->params['pass'][0])
@@ -248,7 +249,120 @@ class ServicesController extends AppController {
 			$result = array('status' => 0 , 'message' => 'Access Denied');
 			$this->set('result', $result);
 			return;		
-		}		
+		}
+	}
+	else
+	{
+		$consortium = $this->Consortium->find('all',array(
+                                                'conditions' => 
+												array('consortium_key' => $this->params['pass'][0])
+												)
+                                            );
+		if(count($consortium) > 0){
+			$this->Library->recursive = -1;
+			$existingLibraries = $this->Library->find('all',array(
+													'conditions' => 
+													array('library_apikey' => $consortium[0]['Consortium']['consortium_name'],
+														  'id' => $this->params['pass'][1],
+														  'library_status' => 'active')
+													)
+												);
+			if(count($existingLibraries) == 0){
+				$result = array('status' => 0 , 'message' => 'Access Denied');
+				$this->set('result', $result);
+				return;
+			}
+			else{			
+				$country = $existingLibraries['0']['Library']['library_territory'];
+				if($existingLibraries['0']['Library']['library_block_explicit_content'] == '1'){
+					$condSphinx = "@Advisory F & @Territory '".addslashes($country)."'";
+				}
+				else {
+					$condSphinx = "@Territory '".addslashes($country)."'";
+				}
+				$searchString =  base64_decode($this->params['pass'][3]);	
+				$searchString = str_replace("^", " ", $searchString);					
+				$searchString = str_replace("$", " ", $searchString);
+				$sphinxCheckCondition = "&";
+				if($this->params['pass'][3] != '') {
+					$sphinxGenreSearch = '@Genre "'.addslashes($searchString).'" '.$sphinxCheckCondition.' ';	
+				}
+				else {
+					$sphinxGenreSearch = '';
+				}			
+				$sphinxTempCondition = $sphinxGenreSearch;
+				$sphinxFinalCondition = substr($sphinxTempCondition, 0, -2);
+				$sphinxFinalCondition = $sphinxFinalCondition.' & @DownloadStatus 1 & '.$condSphinx;
+				if ($condSphinx == "") {
+					$sphinxFinalCondition = substr($sphinxFinalCondition, 0, -2);
+				}
+			
+				App::import('vendor', 'sphinxapi', array('file' => 'sphinxapi.php'));
+				if (isset($this->passedArgs['sort'])){
+					$sphinxSort = $this->passedArgs['sort'];
+				} else {
+					$sphinxSort = "";
+				}
+				if (isset($this->passedArgs['direction'])){
+					$sphinxDirection = $this->passedArgs['direction'];
+				} else {
+					$sphinxDirection = "";
+				}
+				
+				$this->paginate = array('Song' => array(
+							'sphinx' => 'yes', 'sphinxcheck' => $sphinxFinalCondition, 'sphinxsort' => $sphinxSort, 'sphinxdirection' => $sphinxDirection, 'webservice' => 1
+						));
+				
+				$searchResults = $this->paginate('Song');
+				$reference = '';
+				foreach($searchResults as $k=>$v){
+					$result[$k]['Song']['ProdID'] = $v['Song']['ProdID'];
+					$result[$k]['Song']['ProductID'] = $v['Song']['ProductID'];
+					$result[$k]['Song']['ReferenceID'] = $v['Song']['ReferenceID'];
+					$result[$k]['Song']['Title'] = $v['Song']['Title'];
+					$result[$k]['Song']['SongTitle'] = $v['Song']['SongTitle'];
+					$result[$k]['Song']['ArtistText'] = $v['Song']['ArtistText'];
+					$result[$k]['Song']['Artist'] = $v['Song']['Artist'];
+					$result[$k]['Song']['Advisory'] = $v['Song']['Advisory'];
+					$result[$k]['Song']['Composer'] = str_replace('"','',$v['Song']['Composer']);
+					$result[$k]['Song']['Genre'] = str_replace('"','',$v['Song']['Genre']);
+					$result[$k]['Song']['freegal_url'] = "http://".$_SERVER['HTTP_HOST']."/services/login/".$this->params['pass'][0]."/".$this->params['pass'][1]."/".$this->params['pass'][2]."/".$v['Song']['ReferenceID']."/".$v['Song']['ArtistText'];
+					if($reference != $v['Song']['ReferenceID']){ 
+						$albumData = $this->Album->find('all', array(
+							'conditions'=>array('Album.ProdID' => $v['Song']['ReferenceID']),
+							'fields' => array(
+								'Album.ProdID',
+							),
+							'contain' => array(										
+								'Files' => array(
+									'fields' => array(
+										'Files.CdnPath',
+										'Files.SaveAsName',
+										'Files.SourceURL',
+										),                             
+								)
+						)));
+						$reference = $v['Song']['ReferenceID'];
+						$albumArtWork = Configure::read('App.Music_Path').shell_exec('perl files/tokengen ' . $albumData[0]['Files']['CdnPath']."/".$albumData[0]['Files']['SourceURL']);
+					//	print_r($result);exit;
+					}
+					$result[$k]['Song']['Album_Artwork'] = $albumArtWork;				
+				}
+				if(count($result) > 0){
+					$result = $result;
+				}
+				else{
+					$result = array('message' => 'No Records');
+				}
+				$this->set('result', $result);
+			}
+		}
+		else{
+			$result = array('status' => 0 , 'message' => 'Access Denied');
+			$this->set('result', $result);
+			return;		
+		}	
+	}
 	}
 	
 	function genreSong(){
@@ -343,6 +457,7 @@ class ServicesController extends AppController {
 						)));
 						$reference = $v['Song']['ReferenceID'];
 						$albumArtWork = Configure::read('App.Music_Path').shell_exec('perl files/tokengen ' . $albumData[0]['Files']['CdnPath']."/".$albumData[0]['Files']['SourceURL']);
+					//	print_r($result);exit;
 					}
 					$result[$k]['Song']['Album_Artwork'] = $albumArtWork;				
 				}
@@ -352,6 +467,7 @@ class ServicesController extends AppController {
 				else{
 					$result = array('message' => 'No Records');
 				}
+				$this->set('result', $result);
 			}
 		}
 		else{
@@ -502,6 +618,7 @@ class ServicesController extends AppController {
 			}
 			else{
 					$available = $existingLibraries['0']['Library']['library_user_download_limit'];
+					$this->Download->recursive = -1;
 					$downloadsUsed =  $this->Download->find('count',array('conditions' => array('library_id' => $existingLibraries['0']['Library']['id'],'patron_id' => $this->params['pass'][2],'created BETWEEN ? AND ?' => array(Configure::read('App.curWeekStartDate'), Configure::read('App.curWeekEndDate')))));
 					$result = array('remaining_downloads' => $available-$downloadsUsed);
 					$this->set('result', $result);
