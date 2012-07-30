f9b3de7d832951b743b24bfa65687f8a1feb6812
TaranSC <taran2010jeet@gmail.com>
2012-07-30 15:33:39 +0530

    Updated for Solr Search

--- webroot/js/advanced_search.js
+++ webroot/js/advanced_search.js
@@ -1,29 +1,43 @@
 
 
 show_blocksdiv = document.getElementById('show_blocks');
-if(show_blocksdiv){	
+if(show_blocksdiv){
 	show_blocksdiv.style.display="none";
 }
 
-function advanced_search_show_hide(id){	
+function advanced_search_show_hide(id){
 
 	leftColblockdiv = document.getElementById('leftColblock');
 	rightColblockdiv = document.getElementById('rightCol');
 	hide_blocksdiv = document.getElementById('hide_blocks');
 	show_blocksdiv = document.getElementById('show_blocks');
-	if(id == 'show_div'){		
+	if(id == 'show_div'){
 		//To show
 		leftColblockdiv.style.display="block";
 		rightColblockdiv.style.display="block";
 		hide_blocksdiv.style.display="block";
 		show_blocksdiv.style.display="none";
 	}
-	else {		
+	else {
 		//To hide
 		leftColblockdiv.style.display="none";
 		rightColblockdiv.style.display="none";
 		hide_blocksdiv.style.display="none";
 		show_blocksdiv.style.display="block";
-		
+
 	}
 }
+
+$(document).ready(function() {
+  $("#search_query").autocomplete(webroot+"search/autocomplete",
+  {
+		minChars: 1,
+		cacheLength: 10,
+		autoFill: false,
+    extraParams: {
+      type:$('#search_type').val()
+    }
+	}).result(function(e, item) {
+    $('#auto').attr('value', 1);
+  });
+});
