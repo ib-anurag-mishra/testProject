8fb82c6e44afc7a76efe5b6c67cff45c5301e9c8
Sandeep Gupta <gupta09sandeep@gmail.com>
2012-08-28 13:22:17 +0530

    Added code for featured artist.

--- controllers/homes_controller.php
+++ controllers/homes_controller.php
@@ -55,56 +55,62 @@ class HomesController extends AppController
 		if (($artists = Cache::read("featured".$country)) === false) {
 			$featured = $this->Featuredartist->find('all', array('conditions' => array('Featuredartist.territory' => $this->Session->read('territory'),'Featuredartist.language' => Configure::read('App.LANGUAGE')), 'recursive' => -1));
 		//	print "<pre>";print_r($featured);exit;
+			$ids_provider_type = '';
 			foreach($featured as $k => $v){
-				 if($v['Featuredartist']['album'] != 0){
-					$ids .= $v['Featuredartist']['album'].",";
-				 }
+				if(empty($ids_provider_type)){
+				  $ids .= $v['Featuredartist']['album'];
+				  $ids_provider_type .= "(" . $v['Featuredartist']['album'] .",'" . $v['Featuredartist']['provider_type'] ."')";
+				} else {
+				  $ids .= ','.$v['Featuredartist']['album'];
+				   $ids_provider_type .= ','. "(" . $v['Featuredartist']['album'] .",'" . $v['Featuredartist']['provider_type'] ."')";
+				}				 
+				 
 			}
 			if($ids != ''){
-				$this->Album->recursive = 2;
-				$featured =  $this->Album->find('all',array('conditions' =>
-							array('and' =>
-								array(
-									array("Country.Territory" => $territory, "Album.ProdID IN (".rtrim($ids,",'").")" ,"Album.provider_type = Country.provider_type"),
-								), "1 = 1 GROUP BY Album.ProdID"
-							),
+				$this->Album->recursive = 2;	
+				
+				  $featured_query =<<<STR
+				SELECT 
+					Album.ProdID,
+					Album.Title,
+					Album.ArtistText,
+					Album.AlbumTitle,
+					Album.Artist,
+					Album.ArtistURL,
+					Album.Label,
+					Album.Copyright,
+					Album.provider_type,
+					Genre.Genre,
+					Country.Territory,
+					Files.CdnPath,
+					Files.SaveAsName,
+					Files.SourceURL,
+					Files.FileID,
+					PRODUCT.pid
+				FROM
+					Albums AS Album
+						LEFT JOIN
+					File AS Files ON (Album.FileID = Files.FileID)
+						LEFT JOIN
+					Genre AS Genre ON (Genre.ProdID = Album.ProdID)
+						LEFT JOIN
+					countries AS Country ON (Country.ProdID = Album.ProdID)
+						LEFT JOIN
+					 PRODUCT ON (PRODUCT.ProdID = Album.ProdID) 
+				WHERE
+					(Country.Territory = '$territory') AND ((Album.ProdID,Album.provider_type) IN ($ids_provider_type) AND (Album.provider_type = Country.provider_type)) AND 1 = 1 AND (PRODUCT.provider_type = Album.provider_type)
+				GROUP BY Album.ProdID
+				ORDER BY Country.SalesDate desc  
+STR;
 
-							'fields' => array(
-								'Album.ProdID',
-								'Album.Title',
-								'Album.ArtistText',
-								'Album.AlbumTitle',
-								'Album.Artist',
-								'Album.ArtistURL',
-								'Album.Label',
-								'Album.Copyright',
-								'Album.provider_type'
-
-								),
-							'contain' => array(
-								'Genre' => array(
-									'fields' => array(
-										'Genre.Genre'
-										)
-									),
-								'Country' => array(
-									'fields' => array(
-										'Country.Territory'
-										)
-									),
-								'Files' => array(
-									'fields' => array(
-										'Files.CdnPath' ,
-										'Files.SaveAsName',
-										'Files.SourceURL'
-								),
-							)
-						), 'order' => array('Country.SalesDate' => 'desc')
-					)
-				);
+				  $featured =   $this->Album->query($featured_query);				
+				
+				
+				
 			} else {
 				$featured = array();
 			}
+			
 			Cache::write("featured".$territory, $featured);
 		}
 		$featured = Cache::read("featured".$country);
