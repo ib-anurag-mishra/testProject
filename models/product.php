023151a79b786721d41fb9632e8e748c1b02e6c3
TaranSC <taran2010jeet@gmail.com>
2012-08-28 14:20:12 +0530

    added for product table

---
+++ models/product.php
@@ -0,0 +1,13 @@
+<?php
+/*
+ File Name : siteconfig.php
+ File Description : Models page for the  site setting functionality.
+ Author : m68interactive
+*/
+
+class Product extends AppModel
+{
+    var $name = 'Product';
+    var $useTable = 'PRODUCT';
+}
+?>
