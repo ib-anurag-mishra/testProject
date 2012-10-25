6d103de00e2eee7d297cca13fc607e7419acedf3
Taranjeet Singh <taran2010jeet@gmail.com>
2012-10-25 13:35:45 +0530

    Added for Latest Downloads

---
+++ models/latest_download.php
@@ -0,0 +1,20 @@
+<?php
+/*
+ File Name : download.php
+ File Description : Models page for the  downloads table.
+ Author : m68interactive
+*/
+
+class LatestDownload extends AppModel
+{
+  var $name = 'LatestDownload';
+  //var $usetable = 'downloads';
+
+  var $belongsTo = array(
+    'Genre' => array(
+    'className' => 'Genre',
+    'foreignKey' => 'ProdID'
+    )
+  );
+}
+?>
