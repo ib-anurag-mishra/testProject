c89768baf504b026c9518edf3682d2e6059272aa
Taranjeet Singh <taran2010jeet@gmail.com>
2012-11-05 16:22:21 +0530

    added cron to delete one month old records from latest downloads table

---
+++ webroot/cron/del_old_latestdownloads.php
@@ -0,0 +1,27 @@
+<?php
+/**
+File Name : del_old_latestdownloads.php
+File Description : Cron file to delete one month old data from latestdownloads table
+**/
+
+include 'config.php';
+include 'dbconnect.php';
+
+$logFile = "delete_latest_download.txt";
+
+$logFileWriter=fopen($logFile,'w') or die("Can't Open the file!");
+
+$deleteQuery = "DELETE FROM latest_downloads WHERE created < TIMESTAMP(DATE_SUB(CURDATE(), INTERVAL 1 MONTH))";
+
+if(!mysql_query($deleteQuery)){
+   fwrite($logFileWriter, date('Y-m-d h:i:s', time())." : Query for delete not executed : ".$deleteQuery); 
+} else {
+   fwrite($logFileWriter, date('Y-m-d h:i:s', time())." : Query for delete executed succesfully : ".$deleteQuery);  
+}
+
+fclose($logFileWriter);
+
+echo "Script execution completed.....";
+exit();
+
+?>
