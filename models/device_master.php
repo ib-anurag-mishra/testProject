4b4818ceb58db32c5bac5a54c760c954128262b3
Nayan More <nayan.more@infobeans.com>
2012-12-03 15:35:59 +0530

    working on push notification: register deviceID web service

---
+++ models/device_master.php
@@ -0,0 +1,14 @@
+<?php
+/*
+ File Name : device_master.php
+ File Description : Models page for the push notification functionality.
+ Author : m68interactive
+*/
+
+class DeviceMaster extends AppModel {
+  
+  var $name = 'DeviceMaster';
+  var $useTable = 'device_masters';
+  
+ 
+}
