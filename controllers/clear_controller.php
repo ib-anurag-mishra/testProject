ad6f30ad5e3e1773a72af9230820f10ff3557385
Nayan More <nayan.more@infobeans.com>
2012-12-03 16:56:12 +0530

    working on push notification: register deviceID web service

--- controllers/clear_controller.php
+++ controllers/clear_controller.php
@@ -7,7 +7,7 @@
 class ClearController extends AppController {
   var $name = 'Clear';
   var $autoLayout = false;
-  var $uses = array('Album','Download','Song','Genre', 'Library','Artist', 'LatestDownload', 'DeviceMaster');
+  var $uses = array('Album','Download','Song','Genre', 'Library','Artist', 'LatestDownload');
 
   
   
