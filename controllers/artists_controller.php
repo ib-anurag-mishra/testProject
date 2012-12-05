7c87be0ab9b5e8687b56c2efa6b3fc88a5be7fa0
Narendra Nagesh <nagesh4group@gmail.com>
2012-12-05 15:11:06 +0530

    Feature artist images remove all functionality in the admin section

--- controllers/artists_controller.php
+++ controllers/artists_controller.php
@@ -380,7 +380,9 @@ Class ArtistsController extends AppController
 	 Desc : manages new artists with images
         */
 	function admin_manageartist() {
+            
 		$artists = $this->paginate('Artist',array('language' => Configure::read('App.LANGUAGE')));
+               
 		$this -> set( 'artists', $artists );
 	}
 
@@ -388,24 +390,49 @@ Class ArtistsController extends AppController
 	 Function Name : admin_deleteartists
 	 Desc : For deleting a new artist
         */
-	function admin_deleteartists() {
-		$deleteArtistUserId = $this -> params[ 'named' ][ 'id' ];
-		$deleteObj = new Artist();
-		$data = $this->Artist->find('all', array('conditions' => array('id' => $deleteArtistUserId)));
-		$fileName = $data[0]['Artist']['artist_image'];
-		$error = $this->CdnUpload->deleteFile(Configure::read('App.CDN_PATH').'artistimg/'.$fileName);
-		if( $deleteObj -> del( $deleteArtistUserId ) ) {
-			$this -> Session -> setFlash( 'Data deleted successfully!', 'modal', array( 'class' => 'modal success' ) );
-			$this -> redirect( 'manageartist' );
-		}
-		else {
-			$this -> Session -> setFlash( 'Error occured while deleteting the record', 'modal', array( 'class' => 'modal problem' ) );
-			$this -> redirect( 'manageartist' );
-		}
-		$memcache = new Memcache;
-		$memcache->addServer(Configure::read('App.memcache_ip'), 11211);
-		memcache_delete($memcache, Configure::read('App.memcache_key')."_artists");
-		memcache_close($memcache);
+	function admin_deleteartists() {                
+            
+            ob_start();
+            $deleteArtistIdArray = $this -> data[ 'Info' ];          
+            $deleteOption = $this -> data[ 'artist' ]['selectedOpt']; 
+            
+            //if admin want to remove selected records then
+            if($deleteOption == 1){
+                if( count($deleteArtistIdArray) > 0 ){                    
+                    for( $i=0; $i < count($deleteArtistIdArray); $i++ ){                  
+                        $deleteArtistId = $deleteArtistIdArray[$i];                      
+                        $deleteObj = new Artist();
+                        $data = $this->Artist->find('all', array('conditions' => array('id' => $deleteArtistId)));
+                        $fileName = $data[0]['Artist']['artist_image'];
+                        $error = $this->CdnUpload->deleteFile(Configure::read('App.CDN_PATH').'artistimg/'.$fileName);
+                        $deleteObj -> del( $deleteArtistId );
+                    }
+                    
+                    $this -> Session -> setFlash( 'Data deleted successfully!', 'modal', array( 'class' => 'modal success' ) );
+                    $this -> redirect( 'manageartist' );
+                }                
+            }
+            
+            //if admin want to remove all records then
+            if($deleteOption == 2){               
+                $deleteObj = new Artist();
+                $data = $this->Artist->find('all',array('conditions' => array('language' => Configure::read('App.LANGUAGE'))));
+                for( $i=0; $i < count($data); $i++ ){
+                    $fileName = $data[$i]['Artist']['artist_image'];
+                    $deleteArtistId = $data[$i]['Artist']['id'];
+                    $error = $this->CdnUpload->deleteFile(Configure::read('App.CDN_PATH').'artistimg/'.$fileName);  
+                    $deleteObj -> del( $deleteArtistId );
+                }                
+                $this -> Session -> setFlash( 'Data deleted successfully!', 'modal', array( 'class' => 'modal success' ) );
+                $this -> redirect( 'manageartist' );
+            }
+            $memcache = new Memcache;
+            $memcache->addServer(Configure::read('App.memcache_ip'), 11211);
+            memcache_delete($memcache, Configure::read('App.memcache_key')."_artists");
+            memcache_close($memcache);           
+            
+            ob_flush();
+            $this -> redirect( 'manageartist' ); 
 	}
 
 	/*
@@ -821,7 +848,7 @@ Class ArtistsController extends AppController
 			$this->Song->Behaviors->attach('Containable');
 			$songs = $this->Song->find('all', array(
 				'fields' => array('DISTINCT Song.ReferenceID', 'Song.provider_type'),
-				'conditions' => array('Song.ArtistText' => base64_decode($id) ,'Song.DownloadStatus' => 1,"Song.Sample_FileID != ''","Song.FullLength_FIleID != ''" ,'Country.Territory' => $country, $cond, 'Song.provider_type = Country.provider_type'),'contain' => array('Country' => array('fields' => array('Country.Territory'))), 'recursive' => 0));
+				'conditions' => array('Song.ArtistText' => base64_decode($id) ,'Song.DownloadStatus' => 1,"Song.Sample_FileID != ''","Song.FullLength_FIleID != ''" ,'Country.Territory' => $country, $cond, 'Song.provider_type = Country.provider_type'),'contain' => array('Country' => array('fields' => array('Country.Territory'))), 'recursive' => 0, 'order'=>array('Song.provider_type DESC')));
 
 			$val = '';
 			$val_provider_type = '';
@@ -888,7 +915,7 @@ Class ArtistsController extends AppController
 								'Files.SourceURL'
 							),
 						)
-					), 'order' => array('Country.SalesDate' => 'desc'), 'limit' => '15','cache' => 'yes', 'chk' => 2
+					), 'order' => array('Album.provider_type'=>'desc','Country.SalesDate' => 'desc'), 'limit' => '15','cache' => 'yes', 'chk' => 2
 				);
 		if($this->Session->read('block') == 'yes') {
 			$cond = array('Song.Advisory' => 'F');
--- views/artists/admin_manageartist.ctp
+++ views/artists/admin_manageartist.ctp
@@ -6,18 +6,104 @@
  */
 ?>
 <?php $this->pageTitle = 'Content'; ?>
-<form>
-<fieldset>
+<script language="javascript">
+function validateForm() {
+    
+       
+        document.artistAdminManageartistForm.submit();
+   
+}	
+
+function m_delete(flagVar) // Delete the contact
+{
+	
+	var k2=0;
+        if(flagVar == 1){
+            
+            for (var i=0;i<document.artistAdminManageartistForm.elements.length;i++)
+            {
+                    var e1 = document.artistAdminManageartistForm.elements[i];
+
+                    if((e1.type=="checkbox")&&(e1.name=='data[Info][ ]'))
+                    {
+                            if(e1.checked==true)
+                                    {
+                                            k2++;
+                                    }
+                    }
+            }
+            
+        }else{
+            
+            k2 = 1;
+        }
+	
+       
+	if(k2==0)
+	{
+		alert('Please select at least one recode for remove.');
+		return false;
+	}
+	else
+	{
+		if(flagVar == 1){
+                    var x=confirm('Are you sure you want to remove all selected records ?');
+                }else if(flagVar == 2){
+                    var x=confirm('Are you sure you want to remove all records ?');
+                }
+		
+		if(x==false)
+		{
+			return false;
+		}
+		else(x==true)
+		{
+			
+                        document.getElementById('artistSelectedOpt').value = flagVar;                        
+                        return true;
+		
+		}
+	}
+}
+
+
+function CheckAllChk(theForm,maincheckname)
+{
+	for(z=0; z<theForm.length;z++)
+	{
+		if(theForm[z].type =='checkbox')
+		{
+			if(maincheckname.checked == true)
+			{
+				theForm[z].checked=true;
+			}		   
+			else
+			{
+				theForm[z].checked=false;
+			}
+		}
+	}	
+}
+</script>
+<?php echo $this->Form->create('artist', array('type' => 'post','name' => 'artistAdminManageartistForm','url' => array('controller' => 'artists', 'action' => 'admin_deleteartists'))); ?>
+<fieldset style="border: 0 solid #E0E0E0;">
 <legend>Artist Listing</legend>
-	<table id="list">
+
+<table cellpadding="3" cellspacing="3" >
+    <tr>
+        <td>
+            
+            <table id="list"   >
 		<tr>            
 			<th class="left">Artist Name</th>
 			<th class="left">Territory</th>
 			<th>Artist image</th>
 			<th>Edit</th>
-			<th>Delete</th>
+			<th><input type="checkbox" name="maincheckbox" id="maincheckbox" value="1" onClick="CheckAllChk(form,this);">&nbsp;Delete</th>
 		</tr>
-		<?php
+                
+                <?php if(count($artists)) { ?>  
+		<?php                
 		foreach($artists as $artist)
 		{
 			$artistImage = $artist['Artist']['artist_image'];	   
@@ -27,19 +113,48 @@
 			<td class="left"><?php echo $artist['Artist']['territory'];?></td>
 			<td><a href="<?php echo $cdnPath.'artistimg/'.$artist['Artist']['artist_image'];?>" rel="image" onclick="javascript: show_uploaded_images('<?php echo $cdnPath.'artistimg/'.$artist['Artist']['artist_image'];?>')"><?php echo $artistImage;?></a></td>                
 			<td><?php echo $html->link('Edit', array('controller'=>'artists','action'=>'createartist','id'=>$artist['Artist']['id']));?></td>
-			<td><?php echo $html->link('Delete', array('controller'=>'artists','action'=>'deleteartists','id'=>$artist['Artist']['id']));?></td>
+                        <td ><?php echo $this->Form->input("Info. ", array('type'=>'checkbox','id'=>$artist['Artist']['id'], 'value' => $artist['Artist']['id'], 'hiddenField' => false)); ?></td>
 		</tr>
 		<?php
 		}
 		?>
-	</table>
-	<br class="clr" />
-	<div class="paging">
+                  
+                <tr><td colspan="5">&nbsp;</td></tr>
+                <tr>
+			<td class="left" colspan="5"><div class="paging">
 	      <?php echo $paginator->prev('<< '.__('previous', true), array(), null, array('class'=>'disabled'));?>
 	| 	<?php echo $paginator->numbers();?>
-	      <?php echo $paginator->next(__('next', true).' >>', array(), null, array('class'=>'disabled'));?>
-	</div>
+	      <?php echo $paginator->next(__('next', true).' >>', array(), null, array('class'=>'disabled'));?>   
+	<?php echo $this->Form->hidden('selectedOpt'); ?>
+                </div>
+                          <?php if(count($artists)) { ?>  
+                            
+                            <span style="float:right;"> <table >
+                     <tr>
+                         <td><?php echo $this->Form->button('Remove Selected', array('name' => 'remove_selected','label'=>'Remove Selected','onclick' => 'return m_delete(1)')); ?></td>
+                         <td><?php echo $this->Form->button('Remove All', array('name' => 'remove_all','label'=>'Remove All','onclick' => 'return m_delete(2)')); ?></td>
+                     </tr>
+                
+		</table></span>
+                    <?php } ?>           
+                            
+              </td>
+		
+    </tr>
+   <?php }else{ ?> 
+         <tr><td colspan="5" align="center">No Records available.</td></tr>
+   <?php } ?>
+
+</table>
+	
+	
+	
+        
+	
+        
+        
+        
 </fieldset>
+<?php echo $this->Form->end(); ?>
 <?php echo $session->flash();?>
-</form>
 
