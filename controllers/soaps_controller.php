37d7f982ac0fb4baecf6004c238b7bbf7eacf537
Nayan More <nayan.more@infobeans.com>
2012-12-05 16:09:58 +0530

    Push Notification: registerDevice web service updated

--- controllers/soaps_controller.php
+++ controllers/soaps_controller.php
@@ -1373,10 +1373,10 @@ STR;
     
     $arr_param_values['device_id'] = $arr_param[0];
     $arr_param_values['registration_id'] = $arr_param[1];
-    $arr_param_values['patron_id'] = $arr_param[2];
-    $arr_param_values['library_id'] = $arr_param[3];
-    $arr_param_values['user_language'] = $arr_param[4];
-    $arr_param_values['system_type'] = $arr_param[6];
+    $arr_param_values['patron_id'] = $userID;
+    $arr_param_values['library_id'] = $libID;
+    $arr_param_values['user_language'] = $arr_param[2];
+    $arr_param_values['system_type'] = $arr_param[4];
     
     foreach($arr_param_values as $key => $val) {
     
@@ -1386,33 +1386,8 @@ STR;
       }
     }
     
-    $Library = $this->Library->find('first',array(
-      'fields' => array('Library.library_authentication_method'),
-      'conditions' => array('Library.id' => $libID),
-      'recursive' => -1,
-    )
-    );
     
-    if('user_account' == $Library['Library']['library_authentication_method']) {
-      
-      $user = $this->User->find('first',array(
-        'fields' => array('id', 'email'),
-        'conditions' => array(
-          'email' =>  $userID,
-          'user_status' => 'active'
-        ),
-      ));
-      
-      if('' != trim($user['User']['id'])) {
-        $arr_param_values['patron_id'] = $user['User']['id'];
-      } else {
-        $msg = 'Invalid User : '.$userID;
-        return $this->createsSuccessResponseObject(false, $msg);
-      }
-    
-    }
-    
-    $data = $this->DeviceMaster->find('first', array('conditions' => array('patron_id' => $arr_param_values['patron_id'], 'library_id' => $libID)));
+    $data = $this->DeviceMaster->find('first', array('conditions' => array('patron_id' => $userID, 'library_id' => $libID)));
     
     if('' != trim($data['DeviceMaster']['id'])) {
       
