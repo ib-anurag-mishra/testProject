f1f8dd2943103da06a50aafbbc85b710be88bf02
Taranjeet Singh <taran2010jeet@gmail.com>
2013-01-10 20:07:52 +0530

    updated for album sample

--- controllers/artists_controller.php
+++ controllers/artists_controller.php
@@ -728,8 +728,8 @@ Class ArtistsController extends AppController
 							array('and' =>
 								array(
 									array('Song.ReferenceID' => $album['Album']['ProdID']),
-									array('Song.provider_type = Sample_Files.provider_type'),
-									array('Song.provider_type = Full_Files.provider_type'),
+									array('Sample_Files.provider_type' => $provider),
+									array('Full_Files.provider_type' => $provider),
 									array('Song.provider_type = Country.provider_type'),
 									array('Song.DownloadStatus' => 1),
 								//	array('Song.TrackBundleCount' => 0),
