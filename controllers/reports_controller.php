07d9c13b93d02f0eed3979bd6d1d20df0d97cf57
Taranjeet Singh <taran2010jeet@gmail.com>
2013-01-11 17:08:28 +0530

    updated for reports issue

--- controllers/reports_controller.php
+++ controllers/reports_controller.php
@@ -1060,6 +1060,7 @@ Class ReportsController extends AppController
 		$this->set('downloadResult', $downloadResult);
 	}
 	function admin_consortium(){
+		ini_set('memory_limit','512M');
 		set_time_limit(0);
 		if((!$this->Session->read('Auth.User.type_id')) && ($this->Session->read('Auth.User.type_id') != 1))
 		{
