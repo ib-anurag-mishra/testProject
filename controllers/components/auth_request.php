3ad802c528511e1cf50bcc0a72c46c1e698b39dd
Taranjeet Singh <taran2010jeet@gmail.com>
2013-02-01 19:02:09 +0530

    updated after test

--- controllers/components/auth_request.php
+++ controllers/components/auth_request.php
@@ -37,7 +37,6 @@ Class AuthRequestComponent extends Object
 		// make the connection
 		$result = curl_exec($ch);
 		curl_close($ch);
-        echo $result; die;
    		$result =& new Xml($result);
 		$result = Set::reverse($result);
 		return $result;
