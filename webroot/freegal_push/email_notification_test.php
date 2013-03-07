8e882f926498728f2d57f7ba4ea163eabebddefa
kiran pyati <kiran.pyati@infobeans.com>
2013-03-07 15:54:47 +0530

    mobile notification code is removed from email notification added condition if the count is greater than 0 then only send the notification

--- webroot/freegal_push/email_notification_test.php
+++ webroot/freegal_push/email_notification_test.php
@@ -5,48 +5,27 @@
  */
 
 include_once("config/dbconnect.php");
-include_once("api/andriod.php");
-include_once("api/ios.php");
 include_once("includes/class.phpmailer.php");
 
 class notification extends database
 {
-  public $dayOfWeek;
   public $device;
   public $timezoneString;
   public $subscriberData;
   
   function __construct() 
-  {
-    //echo "<pre>";
-    // Get database connection
-    
-    //$arr = array("APA91bHD2r2ZEdDEpNF3AA3uOjKRcpdvAmefWBgACKz_p1Ta4gdPQhNW-L-_Vjm51AcK7w2BmsuRkFdLnPApv5K2uilwwDphQZntPGmOo9WWVz2RWmjr98KNwVdErimwhxWS6k4uXf0pW0RrrdW18jxAbAKLpzHOjQ");
-    //echo sendGCM($arr, "if you get this please let me know");
-    //echo sendIphoneNotification("299732b23d01ab6736084897b93671735264471bab7c28fada17c11b661e6cd2", $count=1, $isDev=0, "if you get this please let me know");
-    //exit;
-      
+  {   
     $this->database();
     
     $timezoneArray = $this->getAllTimeZone();
     
-    //print_r($timezoneArray);
-   
     if(count($timezoneArray) > 0)
-    {
-      // Get the all the device IDS
-      $this->device = $this->getDeviceIds();
-      
+    {      
       // get all the email addresses who have suscribed for email notification.
       $this->subscriberData = $this->getsubscribedEmailIds();
       
       // Send email notification
-      $this->sendEmaiNotification();
-    
-    exit;
-
-      // Send email notification.
-      $response = $this->sendNotification();
+      $this->sendEmailNotification();
     }
     else
     {
@@ -58,45 +37,40 @@ class notification extends database
   /*
    * Send Email notification to all email addresses who have subscribed.
    */
-  function sendEmaiNotification()
+  function sendEmailNotification()
   {
-      $subscriberData = $this->subscriberData; 
-      
-      for($i=0;$i<count($subscriberData);$i++)
+    $subscriberData = $this->subscriberData; 
+
+    for($i=0;$i<count($subscriberData);$i++)
+    {
+      $remainingDownloads = 0;
+      $remainingDownloads = $subscriberData[$i]['library_user_download_limit'] - $subscriberData[$i]['cnt'];
+
+      if($remainingDownloads > 0)
       {
-        $remainingDownloads = 0;
-        $remainingDownloads = $subscriberData[$i]['library_user_download_limit'] - $subscriberData[$i]['cnt'];
-        $newDownloads = $subscriberData[$i]['library_user_download_limit'];
-        
-        // Prepare the message
-        $subscriberData[$i]['user_language'] = "en";
-        $language = strtolower($subscriberData[$i]['user_language']);
-        
-        if($this->dayOfWeek == 1)
-        {
-          $emailContent = str_replace("#downloads#", $newDownloads, $this->mondayEmailContent($language));
-        }
-        else if($this->dayOfWeek == 4)
-        {
-          $emailContent = str_replace("#downloads#", $remainingDownloads, $this->thursdayEmailContent($language));
-        }
-        
-        $link = "http://www.freegaltest.com/users/unsubscribe/".base64_encode($subscriberData[$i]['email_id']);
-        
-        $unsubscribeLink = " <a href='".$link."'>".$link."</a>";
+          // Prepare the message
+          $subscriberData[$i]['user_language'] = "en";
+          $language = strtolower($subscriberData[$i]['user_language']);
 
-	$emailContent = str_replace("#link#", $unsubscribeLink, $emailContent);
-        
-        if($subscriberData[$i]['email_id'] != "")
-        {
-            //Send email
-            $response = $this->sendEmail($subscriberData[$i]['email_id'], $emailContent);
+          $emailContent = str_replace("#downloads#", $remainingDownloads, $this->emailContent($language));
 
-            // Log the message
-            $subscriberData[$i]['system_type'] = "email";
-            $this->createLog($response, $subscriberData[$i]);
-        }
+          $link = "http://www.freegaltest.com/users/unsubscribe/".base64_encode($subscriberData[$i]['email_id']);
+
+          $unsubscribeLink = " <a href='".$link."'>".$link."</a>";
+
+          $emailContent = str_replace("#link#", $unsubscribeLink, $emailContent);
+
+          if($subscriberData[$i]['email_id'] != "")
+          {
+              //Send email
+              $response = $this->sendEmail($subscriberData[$i]['email_id'], $emailContent);
+
+              // Log the message
+              $subscriberData[$i]['system_type'] = "email";
+              $this->createLog($response, $subscriberData[$i]);
+          }
       }
+    }
       
       // return string response 
       return $response;
@@ -128,38 +102,8 @@ class notification extends database
             return "Message sent!";
         }
     }
-  
-  function mondayEmailContent($language)
-  {
-      if($language == 'es')
-      {
-        $message = "Vi havas #downloads# novaj downloads";
-      }
-      else if($language == 'it')
-      {
-        $message = "Hai #downloads# nuovi download";
-      }
-      else if($language == 'fr')
-      {
-        $message = "Vous avez #downloads# nouveaux téléchargements";
-      }
-      else
-      {
-	$message = "Hello,<br />
-<br />
-        Just a friendly reminder that you have #downloads# Freegal Music downloads remaining between now and Sunday night. Please visit your library website and log on to Freegal Music to retrieve your downloads.<br />
-<br />
-        To unsubscribe, press here<br />
-	#link#<br />
-<br />	
-	Regards<br />
-	Freegalmusic Team";       
-      }
-      
-      return $message;
-  }
-  
-  function thursdayEmailContent($language)
+   
+  function emailContent($language)
   {
       if($language == 'es')
       {
@@ -195,170 +139,17 @@ class notification extends database
       return $message;
   }
   
-  // Get the list of all the patron and their device token with number of remaining downloads.
-  function getDeviceIds()
-  {
-    // Get the list of all libraries comes under given timezone. Then get the list of parton and from patron and their device ids.
-    // For each parton get the number of remaining downloads
-    if($this->dayOfWeek == 4)
-    {
-	$sql = "SELECT dm.patron_id, l.id, l.library_name, dm.registration_id, dm.device_id,
-        (select COUNT(d.id) from latest_downloads AS d where dm.patron_id=d.patron_id and dm.library_id=d.library_id AND d.created BETWEEN '".CURRENT_WEEK_START_DATE."' AND '".CURRENT_WEEK_END_DATE."') AS cnt,
-        dm.system_type, l.library_user_download_limit, l.library_user_download_limit, dm.user_language, lt.libraries_timezone FROM device_masters dm
-        LEFT JOIN libraries AS l ON l.id=dm.library_id
-        LEFT JOIN libraries_timezone AS lt ON l.id=lt.library_id
-        WHERE lt.libraries_timezone ".$this->timezoneString."
-        GROUP BY dm.patron_id";
-      $res = $this->db->query($sql);
-    }
-    else if($this->dayOfWeek == 1)
-    {
-      $sql = "SELECT dm.patron_id, l.id, l.library_name, dm.registration_id, dm.device_id,COUNT(d.id) AS cnt, dm.system_type, l.library_user_download_limit, l.library_user_download_limit, dm.user_language, lt.libraries_timezone FROM device_masters dm
-            LEFT JOIN latest_downloads AS d ON dm.patron_id=d.patron_id and dm.library_id=d.library_id
-            LEFT JOIN libraries AS l ON l.id=dm.library_id
-            LEFT JOIN libraries_timezone AS lt ON l.id=lt.library_id
-            WHERE lt.libraries_timezone ".$this->timezoneString." 
-            GROUP BY d.patron_id";
-      $res = $this->db->query($sql);
-    }
-    
-    //echo $sql;
-
-    if(is_object($res))
-    {
-      $i=0;
-      while($row = $res->fetch_assoc())
-      {
-        $result[$i] = $row;
-        $i++;
-      }
-    }
-    return $result;
-  }
-
-  function sendNotification()
-  {
-    //print_r($this->device);    
-    $device = $this->device;
-    
-    for($i=0;$i<count($device);$i++)
-    {    
-      $ids = array($device[$i]['registration_id']);
-      
-      $remainingDownloads = 0;
-      $remainingDownloads = $device[$i]['library_user_download_limit'] - $device[$i]['cnt'];
-      $newDownloads = $device[$i]['library_user_download_limit'];
-      
-      // Prepare the message
-      $language = strtolower($device[$i]['user_language']);
-
-      if($this->dayOfWeek == 1)
-      {
-        if($language == 'es')
-        {
-          $message = "Vi havas $newDownloads novaj downloads";
-        }
-        else if($language == 'it')
-        {
-          $message = "Hai $newDownloads nuovi download";
-        }
-        else if($language == 'fr')
-        {
-          $message = "Vous avez $newDownloads nouveaux téléchargements";
-        }
-        else
-        {
-          $message = "You have $newDownloads new downloads";
-        }
-      }
-      else if($this->dayOfWeek == 4)
-      {
-        if($language == 'es')
-        {
-          $message = "Ne forgesu vi havas $remainingDownloads elŝutoj forlasis";
-        }
-        else if($language == 'it')
-        {
-          $message = "Non dimenticare che hai $remainingDownloads downloads sinistra";
-        }
-        else if($language == 'fr')
-        {
-          $message = "N'oubliez pas que vous avez $remainingDownloads téléchargements gauche";
-        }
-        else
-        {
-          $message = "Don't forget you have $remainingDownloads downloads left";
-        }
-      }
-      else 
-      {
-        $message = "";
-      }
-      
-      //echo "<br>".$message;
-      if($message != "")
-      {
-        if($device[$i]['system_type'] == 1)
-        {
-          // Send notification to Andriod device. 
-          //$response = sendGCM($ids, $message);
-
-          // Log the message
-          $this->createLog($response, $device[$i]);
-        }
-        else
-        {
-          //Send notification to IOS device. to ".$device[$i]['registration_id'];
-          //$response = sendIphoneNotification($device[$i]['registration_id'], $count=0, $isDev=0, $message);
-
-          // Log the message
-          $this->createLog($response, $device[$i]);
-        }
-      }
-    }
-    
-    // return string response 
-    return $response;
-  }
-
   // Create log
   function createLog($response, $device_details="")
   {
-    if($device_details['system_type'] == "email")
-    {    
-	$file = 'log/email_log.txt';
-    }
-    else
-    {
-	$file = 'log/log.txt';
-    }
+    $file = 'log/email_log.txt';
 
     date_default_timezone_set('America/Chicago');
 	
-    if($device_details)
-    {    
-        if($device_details['system_type'] == 1)
-        {
-          $system_type = "Andriod";
-          $request_details = $system_type."|".$device_details['patron_id']."|".$device_details['library_name']."|".$device_details['user_language']."|".$device_details['device_id'];
-        }
-        else if($device_details['system_type'] == 2)
-        {
-          $system_type = "IPhone";
-          $request_details = $system_type."|".$device_details['patron_id']."|".$device_details['library_name']."|".$device_details['user_language']."|".$device_details['device_id'];
-        }
-        else if($device_details['system_type'] == "email")
-        {
-          $system_type = "Email";
-          $request_details = $system_type."|".$device_details['patron_id']."|".$device_details['library_name']."|".$device_details['user_language']."|".$device_details['email_id'];
-        }       
+    $request_details = $device_details['patron_id']."|".$device_details['library_name']."|".$device_details['user_language']."|".$device_details['email_id'];
 
-        echo $content = "\n".date("d-m-y H:i:s")." request - ".$request_details." response - ".$response;
-    }
-    else
-    {
-        echo $content = "\n".date("d-m-y H:i:s")." response - ".$response;
-    }
+    echo $content = "\n".date("d-m-y H:i:s")." request - ".$request_details." response - ".$response;
+
     // Write the contents to the file, 
     // using the FILE_APPEND flag to append the content to the end of the file
     // and the LOCK_EX flag to prevent anyone else writing to the file at the same time
@@ -380,19 +171,13 @@ class notification extends database
       $time = date('h');
       $meridiem = date('a');
       //if($time >= 01 && $time <= 12 && ($meridiem == 'pm' || $meridiem == 'am'))
-      if(($week == 1 || $week == 2 || $week == 5))
-      {
-        $this->dayOfWeek = $week;
-        $this->dayOfWeek = 4;
-        
+      if(($week == 1 || $week == 2 || $week == 4))
+      {        
         $result[$i] = $row;
         $i++;
       }      
     }
 
-    //echo "<br>-------------<br>";
-    //print_r($result);
-    
     // Prepare timezone string to use in sql query.
     $this->timezoneString .= "IN (";
     for($i=0;$i<count($result); $i++)
@@ -413,46 +198,23 @@ class notification extends database
   
   function getsubscribedEmailIds()
   {
-    //$this->dayOfWeek = 4;
-    //if($this->dayOfWeek == 4)
-    //{
-	$sql = "SELECT ns.patron_id, l.id, l.library_name, ns.email_id,
-        (select COUNT(d.id) from latest_downloads AS d where ns.patron_id=d.patron_id and ns.library_id=d.library_id AND d.created BETWEEN '2012-02-18 00:00:00' AND '".CURRENT_WEEK_END_DATE."') AS cnt,
-        l.library_user_download_limit, l.library_user_download_limit, lt.libraries_timezone 
-        FROM notification_subscriptions ns
-        LEFT JOIN libraries AS l ON l.id=ns.library_id
-        LEFT JOIN libraries_timezone AS lt ON l.id=lt.library_id
-        WHERE lt.libraries_timezone IN ('America/New_York') 
-        GROUP BY ns.patron_id";
-        $res = $this->db->query($sql);
-    /*}
-    else if($this->dayOfWeek == 1)
-    {
-        $sql = "SELECT ns.patron_id, ns.email_id, 
-            COUNT(d.id) AS cnt, 
-            l.library_user_download_limit, l.library_user_download_limit, l.id, l.library_name,
-            lt.libraries_timezone 
-            FROM notification_subscriptions ns
-            LEFT JOIN latest_downloads AS d ON ns.patron_id=d.patron_id and ns.library_id=d.library_id
-            LEFT JOIN libraries AS l ON l.id=ns.library_id
-            LEFT JOIN libraries_timezone AS lt ON l.id=lt.library_id
-            WHERE lt.libraries_timezone ".$this->timezoneString." 
-            GROUP BY d.patron_id";
-        $res = $this->db->query($sql);
-    }*/
-    
-    //echo $sql;
+    $sql = "SELECT ns.patron_id, l.id, l.library_name, ns.email_id,
+    (select COUNT(d.id) from latest_downloads AS d where ns.patron_id=d.patron_id and ns.library_id=d.library_id AND d.created BETWEEN '".CURRENT_WEEK_START_DATE."' AND '".CURRENT_WEEK_END_DATE."') AS cnt,
+    l.library_user_download_limit, l.library_user_download_limit, lt.libraries_timezone 
+    FROM notification_subscriptions ns
+    LEFT JOIN libraries AS l ON l.id=ns.library_id
+    LEFT JOIN libraries_timezone AS lt ON l.id=lt.library_id
+    WHERE lt.libraries_timezone ".$this->timezoneString." 
+    GROUP BY ns.patron_id";
+    $res = $this->db->query($sql);    
 
     if(is_object($res))
     {
       $i=0;
       while($row = $res->fetch_assoc())
       {
-	if($row['cnt'] > 0)
-	{
-          $result[$i] = $row;
-          $i++;
-	}
+        $result[$i] = $row;
+        $i++;	
       }
     }
     
