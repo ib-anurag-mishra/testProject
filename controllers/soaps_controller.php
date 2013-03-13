f94a19c42c64653241d5887e96ecd0cbc2b7e88c
Nayan More <nayan.more@infobeans.com>
2013-03-13 19:10:31 +0530

    Response XML for referral_url committed

--- controllers/soaps_controller.php
+++ controllers/soaps_controller.php
@@ -3402,7 +3402,7 @@ STR;
         $resp = $resp['Posts']['message'];
 
       
-        if( 'ezproxy' == $library_authentication_method ){
+        
         
           $checkValidXml = null;
           $checkValidXml = simplexml_load_string($resp);
@@ -3438,7 +3438,7 @@ STR;
               return $this->createsAuthenticationResponseDataObject(false, $response_msg);
             } 	  
           }  
-        }
+ 
 
 
         $resp = trim(strip_tags($resp));
