5f5500db0bcfd4a8672d3c18880f271c23702ef1
Nayan More <nayan.more@infobeans.com>
2013-04-23 15:10:04 +0530

    added app_view.php file

---
+++ app_view.php
@@ -0,0 +1,13 @@
+<?php
+class AppView extends View { 
+
+  function getTextEncode($text) {
+    
+    $text = iconv(mb_detect_encoding($text), "WINDOWS-1252//IGNORE", $text);
+    return iconv(mb_detect_encoding($text), "UTF-8//IGNORE", $text);
+  }
+
+    
+    
+}
+?>
