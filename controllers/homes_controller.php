9a48da847753de6ec712d91666f7ae2cc152e6ce
Narendra Nagesh <nagesh4group@gmail.com>
2013-06-18 17:58:45 +0530

    checking the national top 100

--- controllers/homes_controller.php
+++ controllers/homes_controller.php
@@ -154,7 +154,7 @@ STR;
 		$nationalTopDownload = Cache::read("national".$territory);
                 
                 
-                print_r($nationalTopDownload);
+               // print_r($nationalTopDownload);
                 
 /*		$this->Download->recursive = -1;
 		foreach($nationalTopDownload as $key => $value){
