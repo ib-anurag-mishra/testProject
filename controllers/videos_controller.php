62108447c9390444dd7477ad161f864666b9c2a9
Taranjeet Singh <taran2010jeet@gmail.com>
2013-06-21 19:28:15 +0530

    updated

--- controllers/videos_controller.php
+++ controllers/videos_controller.php
@@ -2,10 +2,21 @@
 
 class VideosController extends AppController {
   
-    var $uses = array();
+    var $uses = array('Album');
     var $layout = 'home';
     
     function index(){
+        if($featuredVideos = Cache::read("featured_videos".$this->Session->read('terrotory')) === false){
+            $featuredVideosSql = "SELECT `FeaturedVideo`.`id`,`FeaturedVideo`.`ProdID`,`Video`.`Image_FileID`, `Video`.`VideoTitle`, `Video`.`ArtistText`, `File`.`CdnPath`, `File`.`SourceURL`, `File`.`SaveAsName` FROM featured_videos as FeaturedVideo LEFT JOIN video as Video on FeaturedVideo.ProdID = Video.ProdID LEFT JOIN File as File on File.FileID = Video.Image_FileID WHERE `FeaturedVideo`.`territory` = '".$this->Session->read('territory')."'";
+            $featuredVideos = $this->Album->query($featuredVideosSql);
+            if(!empty($featuredVideos)){
+                Cache::write("featured_videos".$this->Session->read('territory'), $featuredVideos);
+            }
+        }
+        
+        $featuredVideos = Cache::read("featured_videos".$this->Session->read('territory'));
+        
+        $this->set('featuredVideos',$featuredVideos);
         
     }
 }
