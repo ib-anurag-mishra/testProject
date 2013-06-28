9ce19a6ba0dee42b1bb8f720c2a1dc105814150d
kushal <kushal.pogul@infobeans.com>
2013-06-28 16:03:59 +0530

    us top 10 authorization removed

--- controllers/homes_controller.php
+++ controllers/homes_controller.php
@@ -46,7 +46,7 @@ class HomesController extends AppController
           }
           else                                          //  Before Login
           {
-                $this->Auth->allow('display','aboutus', 'index'); 
+                $this->Auth->allow('display','aboutus', 'index', 'us_top_10', 'my_lib_top_10'); 
           }
                
                 
