76ee5651641eb2bfa5a5926fae31c3ef8b78295f
Taranjeet Singh <taran2010jeet@gmail.com>
2013-07-01 19:27:36 +0530

    added for downloads page

---
+++ views/helpers/mvideo.php
@@ -0,0 +1,34 @@
+<?php
+/*
+	 File Name : song.php
+	 File Description : helper file for getting songs detail
+	 Author : m68interactive
+ */
+class MvideoHelper extends AppHelper {
+    var $uses = array('Song');
+    
+    function getDownloadData($id, $provider) {
+        $songInstance = ClassRegistry::init('Video');
+        $details = $videoInstance->find('all', array(
+		'conditions'=>array('Video.ProdID' => $id, 'Video.provider_type' => $provider),
+		'fields' => array(
+			'Video.ProdID',
+			'Video.ProductID',
+			'Video.Title',
+			'Video.SongTitle',
+			'Video.Artist',
+			'Video.ISRC'
+		),
+		'contain' => array(										
+			'Full_Files' => array(
+				'fields' => array(
+					'Full_Files.CdnPath',
+					'Full_Files.SaveAsName'
+					),                             
+			)
+	)));
+        return  $details;
+    }
+}
+
+?>
