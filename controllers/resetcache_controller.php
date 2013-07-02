b8c4a48637807dd20a5492bf0025d1957201b19d
Nayan More <nayan.more@infobeans.com>
2013-07-02 15:22:24 +0530

    committed function sets music videos list in cache for each territory for App

--- controllers/resetcache_controller.php
+++ controllers/resetcache_controller.php
@@ -11,7 +11,7 @@ class ResetcacheController extends AppController
   var $name = 'Resetcache';
   var $helpers = array( 'Html','Ajax','Javascript','Form', 'Library', 'Page', 'Wishlist','Song', 'Language');
   var $components = array('RequestHandler','ValidatePatron','Downloads','PasswordHelper','Email', 'SuggestionSong','Cookie', 'CdnUpload');
-  var $uses = array('User','Featuredartist','Artist','Library','Download','Genre','Currentpatron','Page','Wishlist','Album','Song','Language', 'Searchrecord');
+  var $uses = array('User','Featuredartist','Artist','Library','Download','Genre','Currentpatron','Page','Wishlist','Album','Song','Language', 'Searchrecord', 'Video', 'Territory');
   private $filename = '../webroot/uploads/allCache.txt'; 
   private $filenamedate = '../webroot/uploads/allCacheDate.txt'; 
 
@@ -514,6 +514,62 @@ class ResetcacheController extends AppController
     exit;
   }
   
+  /**
+  * @function setAppMyMusicVideoList
+  * this function sets music videos list in cache for each territory for App
+  * @param nil
+  **/
+  function setAppMyMusicVideoList() {
+
+    set_time_limit(0);
+
+    $territories = $this->Territory->find("all");
+
+    for($mm=0;$mm<count($territories);$mm++)
+    {
+        $territoryNames[$mm] = $territories[$mm]['Territory']['Territory'];
+    }
+    $siteConfigSQL = "SELECT * from siteconfigs WHERE soption = 'multiple_countries'";
+    $siteConfigData = $this->Album->query($siteConfigSQL);
+    $multiple_countries = (($siteConfigData[0]['siteconfigs']['svalue']==1)?true:false);
+    for($i=0;$i<count($territoryNames);$i++){
+      $territory = $territoryNames[$i];
+      if(0 == $multiple_countries){
+        $countryPrefix = '';
+        $this->Country->setTablePrefix('');
+      } else {
+        $countryPrefix = strtolower($territory)."_";
+        $this->Country->setTablePrefix($countryPrefix);
+      }
+
+    $str_query = 'SELECT v.ProdID, v.ReferenceID, v.Title, v.VideoTitle, v.ArtistText, v.Artist, v.Advisory, v.ISRC, v.Composer,
+                v.FullLength_Duration, v.DownloadStatus, c.SalesDate, gr.Genre, ff.CdnPath AS VideoCdnPath, ff.SaveAsName AS VideoSaveAsName,
+                imgf.CdnPath AS ImgCdnPath, imgf.SourceURL AS ImgSourceURL, prd.pid, COUNT(vd.id) AS cnt
+                FROM video AS v
+                INNER JOIN '.$countryPrefix.'countries AS c ON v.ProdID = c.ProdID AND v.provider_type = c.provider_type
+                INNER JOIN Genre AS gr ON gr.ProdID = v.ProdID AND gr.provider_type = v.provider_type
+                INNER JOIN File AS ff ON v.FullLength_FileID = ff.FileID
+                INNER JOIN File AS imgf ON v.Image_FileID = imgf.FileID
+                INNER JOIN PRODUCT AS prd ON prd.ProdID = v.ProdID AND prd.provider_type = v.provider_type
+                LEFT JOIN videodownloads AS vd ON vd.ProdID = v.ProdID AND vd.provider_type = v.provider_type
+                WHERE c.Territory = "'.$territory.'" AND v.DownloadStatus = "1" GROUP BY v.ProdID
+                ORDER BY cnt DESC';
+    $arr_video = $this->Video->query($str_query);
+
+    Cache::write("AppMyMusicVideosList_".$territory, $arr_video);
+    
+    echo '<br />=====================AppMyMusicVideosList_'.$territory.'==========================================<br />';
+    echo '<pre>';
+    echo $str_query; echo '<br />';
+    print_r(Cache::read("AppMyMusicVideosList_".$territory));
+    echo '</pre>';
+    echo '<br />==================================================================================================<br />';
+
+
+    }
+    exit;
+  }
+
   
 }
 ?>
