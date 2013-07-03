9808c467b0e7104f4c4097bd89d7321db695c2b0
kushal <kushal.pogul@infobeans.com>
2013-07-03 19:47:25 +0530

    changed query

--- controllers/homes_controller.php
+++ controllers/homes_controller.php
@@ -609,7 +609,7 @@ STR;
                             PRODUCT ON (PRODUCT.ProdID = Song.ProdID) INNER JOIN Albums ON (Song.ReferenceID=Albums.ProdID) INNER JOIN File ON (Albums.FileID = File.FileID) 
                     WHERE
                             ( (Song.DownloadStatus = '1') AND  (Song.provider_type = Genre.provider_type) AND (PRODUCT.provider_type = Song.provider_type)) AND (Country.Territory = '$territory') AND Country.SalesDate != '' AND Country.SalesDate > NOW() AND 1 = 1
-                    GROUP BY Song.ReferenceID
+                    GROUP BY Song.ProdID
                     ORDER BY Country.SalesDate ASC
                     LIMIT 20
 	  	
@@ -4151,7 +4151,7 @@ STR;
                             PRODUCT ON (PRODUCT.ProdID = Song.ProdID) INNER JOIN Albums ON (Song.ReferenceID=Albums.ProdID) INNER JOIN File ON (Albums.FileID = File.FileID) 
                     WHERE
                             ( (Song.DownloadStatus = '1') AND  (Song.provider_type = Genre.provider_type) AND (PRODUCT.provider_type = Song.provider_type)) AND (Country.Territory = '$territory') AND Country.SalesDate != '' AND Country.SalesDate > NOW() AND 1 = 1
-                    GROUP BY Song.ReferenceID
+                    GROUP BY Song.ProdID
                     ORDER BY Country.SalesDate ASC
                     LIMIT 100
 	  	
