cedd02cc5f5f87431cf3453fe013a724a45a6410
kushal <kushal.pogul@infobeans.com>
2013-07-08 14:21:58 +0530

    new release query modified

--- controllers/homes_controller.php
+++ controllers/homes_controller.php
@@ -4202,14 +4202,13 @@ STR;
                                     LEFT JOIN
                             PRODUCT ON (PRODUCT.ProdID = Song.ProdID) INNER JOIN Albums ON (Song.ReferenceID=Albums.ProdID) INNER JOIN File ON (Albums.FileID = File.FileID) 
                     WHERE
-                            ( (Song.DownloadStatus = '1') AND  (Song.provider_type = Genre.provider_type) AND (PRODUCT.provider_type = Song.provider_type)) AND (Country.Territory = '$territory') AND Country.SalesDate != '' AND Country.SalesDate <= NOW() AND 1 = 1
-                    GROUP BY Song.ProdID
+                            ( (Song.DownloadStatus = '1') AND  (Song.provider_type = Genre.provider_type) AND (PRODUCT.provider_type = Song.provider_type)) AND (Country.Territory = '$territory') AND Country.SalesDate != '' AND Country.SalesDate <= NOW() AND 1 = 1                    
                     ORDER BY Country.SalesDate DESC
                     LIMIT 100
 	  	
 	  
 STR;
-                        //echo $sql_coming_soon; die;
+                       // echo $sql_coming_soon; die;
 			$coming_soon_rs = $this->Album->query($sql_coming_soon);
 //                        echo "<pre>";
 //                        print_r($coming_soon_rs);
