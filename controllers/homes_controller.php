57a7a66fa14d715600295c3bb2e8694ca84d7003
Narendra Nagesh <nagesh4group@gmail.com>
2013-07-09 16:38:08 +0530

    updated to files

--- controllers/homes_controller.php
+++ controllers/homes_controller.php
@@ -363,7 +363,7 @@ STR;
                                                                     'Files.SourceURL'
                                                     ),
                                             )
-                                    ), 'order' => array('Country.SalesDate' => 'desc')
+                                    ), 'order' => array('Country.SalesDate' => 'desc','limit'=>20)
                             )
                     );
             } else {
