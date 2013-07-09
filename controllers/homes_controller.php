46a82920a7c865430b39e16ec58b16e49568802c
Narendra Nagesh <nagesh4group@gmail.com>
2013-07-09 17:45:28 +0530

    checking the error message

--- controllers/homes_controller.php
+++ controllers/homes_controller.php
@@ -3239,8 +3239,8 @@ STR;
     function addToWishlist(){
        
         
-        if( $this->Session->read('library') && $this->Session->read('patron') 
-                && isset($_REQUEST['prodId']) && isset($_REQUEST['provider']) ){
+        if( !$this->Session->read('library') && !$this->Session->read('patron') 
+                && !isset($_REQUEST['prodId']) && !isset($_REQUEST['provider']) ){
             
             $libraryId = $this->Session->read('library');
             $patronId = $this->Session->read('patron');      
