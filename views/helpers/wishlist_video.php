ce6385533cbf6d433e7f6091d84effc41990e50a
Narendra Nagesh <nagesh4group@gmail.com>
2013-07-10 17:45:38 +0530

    updated the pages

--- views/helpers/wishlist_video.php
+++ views/helpers/wishlist_video.php
@@ -9,6 +9,7 @@ class WishlistVideoHelper extends AppHelper {
     var $uses = array('WishlistVideo');
     
     function getWishlistVideoData($id) {
+        echo 'prodid=>'.$id;
         $wishlistInstance = ClassRegistry::init('WishlistVideo');
         $libraryId = $this->Session->read('library');
         $patronId = $this->Session->read('patron');  
