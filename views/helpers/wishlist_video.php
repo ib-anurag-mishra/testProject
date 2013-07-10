80f1c3eb56c6d68aab2a54aea915bc0f2e2437c6
Narendra Nagesh <nagesh4group@gmail.com>
2013-07-10 17:36:00 +0530

    updated the files

--- views/helpers/wishlist_video.php
+++ views/helpers/wishlist_video.php
@@ -12,11 +12,13 @@ class WishlistVideoHelper extends AppHelper {
         $wishlistInstance = ClassRegistry::init('WishlistVideo');
         $libraryId = $this->Session->read('library');
         $patronId = $this->Session->read('patron');  
-        $wishlistDetails = $wishlistInstance->find('all', array('conditions' => array('library_id2' => $libraryId,'patron_id' => $patronId, 'ProdID' => $id)));
+        $wishlistDetails = $wishlistInstance->find('all', array('conditions' => array('library_id' => $libraryId,'patron_id' => $patronId, 'ProdID' => $id)));
         if(count($wishlistDetails) != 0) {
+            echo 147;
             return "Added to Wishlist";
         }
         else {
+            echo 478;
             return "Add to wishlist";
         }
     }
