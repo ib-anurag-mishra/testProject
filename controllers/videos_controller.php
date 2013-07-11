fd310be8f2e7db7f8d2255b2c4cbca54fdd405ef
Narendra Nagesh <nagesh4group@gmail.com>
2013-07-11 20:04:26 +0530

    updated the pages

--- controllers/videos_controller.php
+++ controllers/videos_controller.php
@@ -282,8 +282,7 @@ class VideosController extends AppController {
 
             //writes in log
             $this->log($log_data, $log_name);
-echo $return;
-die;
+
             $this->Library->setDataSource('default');
             if (is_numeric($return)) {
                 header("Content-Type: " . $contenttype);
