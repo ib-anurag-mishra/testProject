ebe7ad8307e7b472838ceac98eba03102155ca50
Narendra Nagesh <nagesh4group@gmail.com>
2013-07-24 11:58:15 +0530

    updated the pages

--- controllers/genres_controller.php
+++ controllers/genres_controller.php
@@ -232,7 +232,7 @@ Class GenresController extends AppController
 		}
 		$this->Genre->Behaviors->attach('Containable');
 		$this->Genre->recursive = 2;
-		//if (($genre = Cache::read("genre".$country)) === false) {
+		if (($genre = Cache::read("genre".$country)) === false) {
 			$genreAll = $this->Genre->find('all',array(
 						'conditions' =>
 							array('and' =>
@@ -253,12 +253,9 @@ Class GenresController extends AppController
 						),'group' => 'Genre.Genre'
 					));
 			 Cache::write("genre".$country, $genreAll);
-		//}
+		}
 		$genreAll = Cache::read("genre".$country);
-                
                 print_r($genreAll);
-                die;
-                
 		$this->set('genresAll', $genreAll);
 		$patId = $this->Session->read('patron');
 		$libId = $this->Session->read('library');
