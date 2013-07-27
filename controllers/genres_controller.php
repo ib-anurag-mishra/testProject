5c303b377ed7477675d4d6d528d31b283a466b53
Narendra Nagesh <nagesh4group@gmail.com>
2013-07-27 18:51:18 +0530

    updated the pages

--- controllers/genres_controller.php
+++ controllers/genres_controller.php
@@ -503,7 +503,7 @@ Class GenresController extends AppController
             $genre = mysql_escape_string($genre);
             
             if($genre != 'All'){
-                echo 47;
+               
                 $this->Song->unbindModel(array('hasOne' => array('Participant')));
                 $this->Song->unbindModel(array('hasOne' => array('Country')));
                 $this->Song->unbindModel(array('belongsTo' => array('Sample_Files','Full_Files')));
@@ -511,7 +511,7 @@ Class GenresController extends AppController
                 $gcondition = array("Song.provider_type = Genre.provider_type", "Genre.Genre = '$genre'","find_in_set('\"$country\"',Song.Territory) > 0",'Song.DownloadStatus' => 1,"Song.Sample_FileID != ''","TRIM(Song.ArtistText) != ''","Song.ArtistText IS NOT NULL","Song.FullLength_FIleID != ''",$condition,'1 = 1 GROUP BY Song.ArtistText');
                 $this->paginate = array(
                     'conditions' => $gcondition,
-                    'fields' => array('DISTINCT Song.ArtistText1'),
+                    'fields' => array('DISTINCT Song.ArtistText'),
                         'contain' => array(
                                 'Genre' => array(
                                         'fields' => array(
@@ -523,7 +523,7 @@ Class GenresController extends AppController
                     'limit' => '60', 'cache' => 'yes','check' => 2
                     );
             } else {   
-                echo 147;
+               
                 $this->Song->unbindModel(array('hasOne' => array('Participant')));
                 $this->Song->unbindModel(array('hasOne' => array('Country')));
                 $this->Song->unbindModel(array('hasOne' => array('Genre')));
@@ -532,7 +532,7 @@ Class GenresController extends AppController
                 $gcondition = array("find_in_set('\"$country\"',Song.Territory) > 0",'Song.DownloadStatus' => 1,"Song.Sample_FileID != ''","Song.FullLength_FIleID != ''","TRIM(Song.ArtistText) != ''","Song.ArtistText IS NOT NULL",$condition,'1 = 1 GROUP BY Song.ArtistText');
                 $this->paginate = array(
                     'conditions' => $gcondition,
-                    'fields' => array('DISTINCT Song.ArtistText1'),
+                    'fields' => array('DISTINCT Song.ArtistText'),
                     'extra' => array('chk' => 1),
                     'order' => 'TRIM(Song.ArtistText) ASC',
                     'limit' => '60',
