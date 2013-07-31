d6b120a5a0fa33f2e61ca33d637881a916f317c6
kushal <kushal.pogul@infobeans.com>
2013-07-31 12:43:54 +0530

    featured artist print query

--- controllers/homes_controller.php
+++ controllers/homes_controller.php
@@ -310,8 +310,8 @@ STR;
         $ids_provider_type = '';
         //featured artist slideshow code start
         
-       // if(1){
-        if (($artists = Cache::read("featured".$country)) === false) {
+        if(1){
+       // if (($artists = Cache::read("featured".$country)) === false) {
            
             
             //get all featured artist and make array
@@ -372,6 +372,9 @@ STR;
                                     ), 'order' => array('Country.SalesDate' => 'DESC'), 'limit'=>20
                             )
                     );
+                    
+                    echo "Query: ".$this->Album->lastQuery();
+                    
                     
             } else {
                     $featured = array();
