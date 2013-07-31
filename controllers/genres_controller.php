933e44ebbe69a8ba9c3882cb7df20b7b94c38549
Narendra Nagesh <nagesh4group@gmail.com>
2013-07-31 20:33:37 +0530

    updated the pages

--- controllers/genres_controller.php
+++ controllers/genres_controller.php
@@ -388,7 +388,7 @@ Class GenresController extends AppController
                     $gcondition = array("Song.provider_type = Genre.provider_type", "Genre.Genre = '$genre'","find_in_set('\"$country\"',Song.Territory) > 0",'Song.DownloadStatus' => 1,"Song.Sample_FileID != ''","TRIM(Song.ArtistText) != ''","Song.ArtistText IS NOT NULL","Song.FullLength_FIleID != ''",$condition,'1 = 1 GROUP BY Song.ArtistText');
                     $this->paginate = array(
                                     'conditions' => $gcondition,
-                                    'fields' => array('DISTINCT Song.ArtistText'),
+                                    'fields' => array('DISTINCT Song.ArtistText1'),
                                         'contain' => array(
                                                 'Genre' => array(
                                                         'fields' => array(
@@ -397,7 +397,7 @@ Class GenresController extends AppController
                                         ),
                                         'extra' => array('chk' => 1),
                                     'order' => 'TRIM(Song.ArtistText) ASC',
-                                    'limit' => '60', 'cache' => 'yes','check' => 2
+                                    'limit' => '60', 'cache' => 'no','check' => 2
                                     );
                 } else {
                     $this->Song->unbindModel(array('hasOne' => array('Participant')));
@@ -410,7 +410,7 @@ Class GenresController extends AppController
 
                     $this->paginate = array(
                             'conditions' => $gcondition,
-                            'fields' => array('DISTINCT Song.ArtistText1'),
+                            'fields' => array('DISTINCT Song.ArtistText'),
                             'extra' => array('chk' => 1),
                             'order' => 'TRIM(Song.ArtistText) ASC',
                             'limit' => '60',
