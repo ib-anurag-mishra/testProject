b0ad7d008922eb176307e2bbfd3f341841c38e29
mohan <mohanrocku@gmail.com>
2013-07-31 12:54:06 +0530

    updated after testing

--- controllers/homes_controller.php
+++ controllers/homes_controller.php
@@ -4484,8 +4484,7 @@ STR;
              
                 
              
-             //if (($coming_soon = Cache::read("new_releases_albums".$territory)) === false)    // Show from DB
-             if(1)
+             if (($coming_soon = Cache::read("new_releases_albums".$territory)) === false)    // Show from DB
              {            
                                 $this->Song->recursive = 2;
                                 $countryPrefix = $this->Session->read('multiple_countries');                                
