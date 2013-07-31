9756a58bc766ff8b541fe06cd7bb9ce2817c8a45
Narendra Nagesh <nagesh4group@gmail.com>
2013-07-31 20:49:20 +0530

    updated the pages

--- controllers/genres_controller.php
+++ controllers/genres_controller.php
@@ -381,6 +381,7 @@ Class GenresController extends AppController
 		$genre = mysql_escape_string($genre);
                 
                 if($genre != 'All'){
+                    echo 14;
                     $this->Song->unbindModel(array('hasOne' => array('Participant')));
                     $this->Song->unbindModel(array('hasOne' => array('Country')));
                     $this->Song->unbindModel(array('belongsTo' => array('Sample_Files','Full_Files')));
@@ -388,7 +389,7 @@ Class GenresController extends AppController
                     $gcondition = array("Song.provider_type = Genre.provider_type", "Genre.Genre = '$genre'","find_in_set('\"$country\"',Song.Territory) > 0",'Song.DownloadStatus' => 1,"Song.Sample_FileID != ''","TRIM(Song.ArtistText) != ''","Song.ArtistText IS NOT NULL","Song.FullLength_FIleID != ''",$condition,'1 = 1 GROUP BY Song.ArtistText');
                     $this->paginate = array(
                                     'conditions' => $gcondition,
-                                    'fields' => array('DISTINCT Song.ArtistText'),
+                                    'fields' => array('DISTINCT Song.ArtistText1'),
                                         'contain' => array(
                                                 'Genre' => array(
                                                         'fields' => array(
@@ -400,6 +401,7 @@ Class GenresController extends AppController
                                     'limit' => '60', 'cache' => 'no','check' => 2
                                     );
                 } else {
+                    echo 15;
                     $this->Song->unbindModel(array('hasOne' => array('Participant')));
                     $this->Song->unbindModel(array('hasOne' => array('Country')));
                     $this->Song->unbindModel(array('hasOne' => array('Genre')));
@@ -410,7 +412,7 @@ Class GenresController extends AppController
 
                     $this->paginate = array(
                             'conditions' => $gcondition,
-                            'fields' => array('DISTINCT Song.ArtistText'),
+                            'fields' => array('DISTINCT Song.ArtistText1'),
                             'extra' => array('chk' => 1),
                             'order' => 'TRIM(Song.ArtistText) ASC',
                             'limit' => '60',
