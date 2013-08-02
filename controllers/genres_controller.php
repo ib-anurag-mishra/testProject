be10c8268e3b08f46b1212c0f40a4205b4c7d549
Narendra Nagesh <nagesh4group@gmail.com>
2013-08-02 13:07:19 +0530

    updated the pages

--- controllers/genres_controller.php
+++ controllers/genres_controller.php
@@ -473,7 +473,8 @@ Class GenresController extends AppController
             $genre = base64_decode($Genre);
             $genre = mysql_escape_string($genre);
             
-            if($genre != 'All'){               
+            if($genre != 'All'){   
+                echo 145;
 
                 $this->Song->unbindModel(array('hasOne' => array('Participant')));
                 $this->Song->unbindModel(array('hasOne' => array('Country')));
@@ -483,7 +484,7 @@ Class GenresController extends AppController
                 $gcondition = array("Song.provider_type = Genre.provider_type", "Genre.Genre = '$genre'","find_in_set('\"$country\"',Song.Territory) > 0",'Song.DownloadStatus' => 1,"Song.Sample_FileID != ''","TRIM(Song.ArtistText) != ''","Song.ArtistText IS NOT NULL","Song.FullLength_FIleID != ''",$condition,'1 = 1 GROUP BY Song.ArtistText');
                 $this->paginate = array(
                                 'conditions' => $gcondition,
-                                'fields' => array('DISTINCT Song.ArtistText'),
+                                'fields' => array('DISTINCT Song.ArtistText1'),
                                     'contain' => array(
                                             'Genre' => array(
                                                     'fields' => array(
