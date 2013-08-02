8d031102866fd005223789c2c383e971c44f5e42
alokbhatt <alok.infouser@gmail.com>
2013-08-02 19:21:51 +0530

    Updated

--- webroot/cron/report1.php
+++ webroot/cron/report1.php
@@ -55,8 +55,8 @@ if(($currentDate == $weekFirstDay) || ($currentDate == $monthFirstDate))
             //$reports_dir = 'C:\xampp\htdocs\m68\Freegalmusic\app\webroot\cron\sony_reports1';
             //$reports_dir = 'C:\xampp\htdocs\m68\Freegalmusic\app\webroot\cron\sony_reports_12Q1';
             //$reports_dir = 'C:\xampp\htdocs\m68\Freegalmusic\app\webroot\cron\sony_reports_Oct_2012';
-//            $reports_dir = 'D:\projects\Freegalmusic\app\webroot\cron\sfv_reports';
-            $reports_dir = SONY_REPORTFILES;
+            $reports_dir = 'D:\projects\Freegalmusic\app\webroot\cron\sfv_reports';
+//            $reports_dir = SONY_REPORTFILES;
 
             if(!file_exists($reports_dir))
             {
@@ -297,9 +297,9 @@ if(($currentDate == $weekFirstDay) || ($currentDate == $monthFirstDate))
                                 else
                                 {
                                     $sales .= "1.30#*#"; // WPU
-                                    $sales .= ("1.30" * $line['TrkCount']) . "#*#"; // Wholesale Value (WPU * Quantity)
+                                    $sales .= (number_format(("1.30" * $line['TrkCount']), 2, '.', '')) . "#*#"; // Wholesale Value (WPU * Quantity)
                                     $sales .= "1.30#*#"; // Net Invoice Price (same as WPU)
-                                    $sales .= ("1.30" * $line['TrkCount']) . "#*#"; // Net Invoice Value (same as Wholesale Value)
+                                    $sales .= (number_format(("1.30" * $line['TrkCount']), 2, '.', '')) . "#*#"; // Net Invoice Value (same as Wholesale Value)
                                     $sales .= ("1.99" * $line['TrkCount']) . "#*#"; // Retail Value
                                 }
 
@@ -660,9 +660,9 @@ if(($currentDate == $weekFirstDay) || ($currentDate == $monthFirstDate))
                                 else
                                 {
                                     $sales .= "1.30#*#"; // WPU
-                                    $sales .= ("1.30" * $line['TrkCount']) . "#*#"; // Wholesale Value (WPU * Quantity)
+                                    $sales .= (number_format(("1.30" * $line['TrkCount']), 2, '.', '')) . "#*#"; // Wholesale Value (WPU * Quantity)
                                     $sales .= "1.30#*#"; // Net Invoice Price (same as WPU)
-                                    $sales .= ("1.30" * $line['TrkCount']) . "#*#"; // Net Invoice Value (same as Wholesale Value)
+                                    $sales .= (number_format(("1.30" * $line['TrkCount']), 2, '.', '')) . "#*#"; // Net Invoice Value (same as Wholesale Value)
                                     $sales .= ("1.99" * $line['TrkCount']) . "#*#"; // Retail Value
                                 }
 
