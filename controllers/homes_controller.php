2095b442928676b9932c303221b621ad02af6aa7
Nayan More <nayan.more@infobeans.com>
2013-08-03 16:42:36 +0530

    comin soon songs query optimized

--- controllers/homes_controller.php
+++ controllers/homes_controller.php
@@ -558,58 +558,42 @@ STR;
         /*
         *  Code For Coming Soon --- START
         */ 
-         $territory = $this->Session->read('territory');
+        $territory = $this->Session->read('territory');
 
-         /*if (($coming_soon = Cache::read("coming_soon_songs".$territory)) === false)    // Show from DB
-         {               
-            $this->Song->recursive = 2;
-            $countryPrefix = $this->Session->read('multiple_countries');                                
-            $sql_coming_soon =<<<STR
-               SELECT 
-        Song.ProdID,
-        Song.ReferenceID,
-        Song.Title,
-        Song.ArtistText,
-        Song.DownloadStatus,
-        Song.SongTitle,
-        Song.Artist,
-        Song.Advisory,
-        Song.Sample_Duration,
-        Song.FullLength_Duration,
-        Song.provider_type,
-        Genre.Genre,
-        Country.Territory,
-        Country.SalesDate,
-        Sample_Files.CdnPath,
-        Sample_Files.SaveAsName,
-        Full_Files.CdnPath,
-        Full_Files.SaveAsName,
-        File.CdnPath,
-        File.SourceURL,
-        File.SaveAsName,
-        Sample_Files.FileID,
-        PRODUCT.pid
-    FROM
-        Songs AS Song
-                LEFT JOIN
-        File AS Sample_Files ON (Song.Sample_FileID = Sample_Files.FileID)
-                LEFT JOIN
-        File AS Full_Files ON (Song.FullLength_FileID = Full_Files.FileID)
-                LEFT JOIN
-        Genre AS Genre ON (Genre.ProdID = Song.ProdID) AND  (Song.provider_type = Genre.provider_type)
-                LEFT JOIN
-        {$countryPrefix}countries AS Country ON (Country.ProdID = Song.ProdID) AND (Country.Territory = '$territory') AND (Song.provider_type = Country.provider_type) AND (Country.SalesDate != '') AND (Country.SalesDate > NOW())
-                LEFT JOIN
-        PRODUCT ON (PRODUCT.ProdID = Song.ProdID) AND (PRODUCT.provider_type = Song.provider_type)
-                INNER JOIN 
-        Albums ON (Song.ReferenceID=Albums.ProdID) 
-                INNER JOIN 
-        File ON (Albums.FileID = File.FileID) 
-    WHERE
+        if (($coming_soon = Cache::read("coming_soon_songs".$territory)) === false) {     
+          // Show from DB          
+          $this->Song->recursive = 2;
+          $countryPrefix = $this->Session->read('multiple_countries');                                
+          $sql_coming_soon =<<<STR
+            SELECT 
+              Song.ProdID,
+              Song.ReferenceID,
+              Song.Title,
+              Song.ArtistText,
+              Song.DownloadStatus,
+              Song.SongTitle,
+              Song.Artist,
+              Song.Advisory,
+              Song.Sample_Duration,
+              Song.FullLength_Duration,
+              Song.provider_type,
+              Genre.Genre,
+              Country.Territory,
+              Country.SalesDate,
+              File.CdnPath,
+              File.SourceURL,
+              File.SaveAsName
+            FROM
+            Songs AS Song
+              LEFT JOIN Genre AS Genre ON (Genre.ProdID = Song.ProdID) AND  (Song.provider_type = Genre.provider_type)
+              LEFT JOIN {$countryPrefix}countries AS Country ON (Country.ProdID = Song.ProdID) AND (Country.Territory = '$territory') AND (Song.provider_type = Country.provider_type) AND (Country.SalesDate != '') AND (Country.SalesDate > NOW())
+              INNER JOIN Albums ON (Song.ReferenceID=Albums.ProdID) 
+              INNER JOIN File ON (Albums.FileID = File.FileID) 
+            WHERE
             ( (Song.DownloadStatus = '1')  )   AND 1 = 1
-    GROUP BY Song.ReferenceID
-    ORDER BY Country.SalesDate ASC
-    LIMIT 20
+            GROUP BY Song.ReferenceID
+            ORDER BY Country.SalesDate ASC
+            LIMIT 5
 STR;
 
                     $coming_soon_rs = $this->Album->query($sql_coming_soon);
@@ -629,7 +613,7 @@ STR;
                 }
                 
                 $this->set('coming_soon_rs', $coming_soon_rs); 
-                */
+                
                 // Videos
                 if (($coming_soon = Cache::read("coming_soon_videos".$territory)) === false)    // Show from DB
                 {
