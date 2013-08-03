af0ec67bf791922124c04c11bb727798f919ebb8
Narendra Nagesh <nagesh4group@gmail.com>
2013-08-03 16:56:01 +0530

    updated the pages

--- controllers/genres_controller.php
+++ controllers/genres_controller.php
@@ -394,7 +394,7 @@ Class GenresController extends AppController
                     $gcondition = array("Song.provider_type = Genre.provider_type", "Genre.Genre = '$genre'","find_in_set('\"$country\"',Song.Territory) > 0",'Song.DownloadStatus' => 1,"Song.Sample_FileID != ''","TRIM(Song.ArtistText) != ''","Song.ArtistText IS NOT NULL","Song.FullLength_FIleID != ''",$condition,'1 = 1 GROUP BY Song.ArtistText');
                     $this->paginate = array(
                                     'conditions' => $gcondition,
-                                    'fields' => array('DISTINCT Song.ArtistText1'),
+                                    'fields' => array('DISTINCT Song.ArtistText'),
                                         'contain' => array(
                                                 'Genre' => array(
                                                         'fields' => array(
