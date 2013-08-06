ad3f206ff52000a4abb4efe30ee37ff74bf5becf
kiran pyati <kiran.pyati@infobeans.com>
2013-08-06 16:03:18 +0530

    fixing download issue

--- controllers/components/downloads.php
+++ controllers/components/downloads.php
@@ -75,13 +75,13 @@ Class DownloadsComponent extends Object
             if($this->checkSongExists($prodId, $providerType)){                
                 if($this->checkAllowedCountry($prodId, $providerType, $isMobileDownload, $mobileTerritory)){                    
                     if($this->checkLibraryDownload($libId)){
-                        //if($this->checkPatronDownload($uid,$libId)){
+                        if($this->checkPatronDownload($uid,$libId)){
                             $this->log($channel." : allowed download request for ".$prodId." - ".$providerType." - ".$libId." from User:".$uid." IP:".$ip,'download');
                             return array(true,'', 1);
-                        //} else {
-                            //$this->log($channel." : Rejected download request for ".$prodId." ".$providerType." from User:".$uid." IP:".$ip." as the patron download limit has been reached");
-                            //return array(false,'The patron has reached the download limit', 2);
-                        //}
+                        } else {
+                            $this->log($channel." : Rejected download request for ".$prodId." ".$providerType." from User:".$uid." IP:".$ip." as the patron download limit has been reached");
+                            return array(false,'The patron has reached the download limit', 2);
+                        }
                     } else {
                         $this->log($channel." : Rejected download request for ".$prodId." - ".$providerType." - ".$libId." from User:".$uid." IP:".$ip." as the library download limit has been reached",'download');
                         return array(false,'The library has reached the download limit.', 3);
