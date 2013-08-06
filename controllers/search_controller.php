5b0f57a89de22b20f4e66692bf1b531364c63a6e
Taranjeet Singh <taran2010jeet@gmail.com>
2013-08-06 14:50:06 +0530

    updated for wishlist add video

--- controllers/search_controller.php
+++ controllers/search_controller.php
@@ -8,7 +8,7 @@
 class SearchController extends AppController {
 
     var $name = 'Search';
-    var $helpers = array('Html', 'Ajax', 'Javascript', 'Form', 'Library', 'Page', 'Wishlist', 'Song', 'Language', 'Album', 'Session');
+    var $helpers = array('Html', 'Ajax', 'Javascript', 'Form', 'Library', 'Page', 'Wishlist', 'Song', 'Language', 'Album', 'Session','WishlistVideo');
     var $components = array('Auth', 'Acl', 'RequestHandler', 'ValidatePatron', 'Downloads', 'PasswordHelper', 'Email', 'SuggestionSong', 'Cookie', 'Solr', 'Session');
     var $uses = array('Home', 'User', 'Featuredartist', 'Artist', 'Library', 'Download', 'Genre', 'Currentpatron', 'Page', 'Wishlist', 'Album', 'Song', 'Language', 'Searchrecord');
 
