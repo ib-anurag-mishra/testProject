549fff30d68a0739f74d91f371644624be6d0348
kushal <kushal.pogul@infobeans.com>
2013-08-09 18:09:28 +0530

    album image path

---
+++ webroot/cron/album_image_path.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * 
+ *
+ * @author Rob Richmond
+ * @version $Id$
+ * @copyright Maycreate Idea Group, 19 January, 2010
+ * This cron script is intended to run every midnight to update the Album Image Path
+ * Contract Start Date by adding One Year.
+ * @package update_library_status_cron
+ **/
+
+error_reporting(E_ALL);
+set_time_limit(0);
+include 'functions.php';
+
+$memcache = new Memcache;
+$memcache->addServer('10.178.4.51', 11211);
+
+
+$query = "SELECT  Album.ProdID, Album.FileID,  FileInfo.SourceURL, FileInfo.CdnPath 
+from Albums as Album LEFT JOIN File AS FileInfo on Album.FileID=FileInfo.FileID where FileInfo.SourceURL!='' LIMIT 0,2";
+
+$result = mysql_query($query) or die('Query failed: ' . mysql_error());
+
+
+while ($line = mysql_fetch_array($result, MYSQL_ASSOC)) 
+{ 
+        //echo "<pre>"; print_r($line);    
+
+        $album_img =  shell_exec('perl files/tokengen ' . $AlbumData['CdnPath']."/".$AlbumData['SourceURL']);
+        $album_img =  "http://music.libraryideas.com/".$album_img;         
+
+        
+        echo "<BR>ProdID: ".$AlbumData['ProdID'].memcache_delete($memcache,"album_image_path" .$AlbumData['ProdID']);
+        memcache_set("album_image_path" .$AlbumData['ProdID'],$album_img,false,86400);		
+
+}
+
+echo "Done with Updation";
+
+
+
+
+?>
