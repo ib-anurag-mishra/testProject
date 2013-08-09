32fe143d1ddada69810d832d19215ca28f2a036c
kushal <kushal.pogul@infobeans.com>
2013-08-09 18:07:33 +0530

    album image path cron

--- webroot/cron/dbconnect.php
+++ webroot/cron/dbconnect.php
@@ -4,6 +4,13 @@ File Name : dbconnect.php
 File Description : This file is used to establish connection with the data base
 @author : m68interactive 
 **/
-$conn = mysql_connect(DBHOST,DBUSER,DBPASS);
-mysql_select_db(DBNAME, $conn);
+//$conn = mysql_connect(DBHOST,DBUSER,DBPASS);
+//mysql_select_db(DBNAME, $conn);
+
+//$conn = mysql_connect("192.168.2.178","infobeans","infobeans");
+//mysql_select_db("fmusic", $conn);
+
+$conn = mysql_connect("192.168.100.115","freegal_prod","}e47^B1EO9hD");
+mysql_select_db("freegal", $conn);
+
 ?>
