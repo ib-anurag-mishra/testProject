fac6e0e814fdc1670c4f65828b63848c1c9a2000
alokbhatt <alok.infouser@gmail.com>
2013-08-12 12:45:12 +0530

    updated for most popular

--- webroot/js/site.js
+++ webroot/js/site.js
@@ -999,6 +999,7 @@ $(function() {
 	
 
 	$('.site-nav .most-popular a').on('mouseenter',function(e){
+            alert(1);
 		e.preventDefault(); 
               /*  console.log('entered'); */
 		$('.most-popular-sub-nav').addClass('active');
@@ -1011,6 +1012,7 @@ $(function() {
 	
 	
 	$('.site-nav .regular').on('mouseenter',function(e){
+            alert(2);
              $('.most-popular-sub-nav').removeClass('active');
 	});
 	
