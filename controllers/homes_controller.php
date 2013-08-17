9c6030f9fb05296b9f4a524d610130d4cc45bee4
Taranjeet Singh <taran2010jeet@gmail.com>
2013-08-17 16:50:57 +0530

    updated

--- controllers/homes_controller.php
+++ controllers/homes_controller.php
@@ -48,7 +48,7 @@ class HomesController extends AppController
         $this->Cookie->name = 'baker_id';
         $this->Cookie->time = 3600; // or '1 hour'
         $this->Cookie->path = '/';
-        $this->Cookie->domain = 'www.freegalmusic.com';
+        $this->Cookie->domain = 'freegalmusic.com';
         //$this->Cookie->key = 'qSI232qs*&sXOw!';
         
     }
