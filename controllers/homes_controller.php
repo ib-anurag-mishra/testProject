d802e0a976ed1ff126015490e8386dc0c48bf4c7
kushal <kushal.pogul@infobeans.com>
2013-08-23 12:21:54 +0530

    debug tems

--- controllers/homes_controller.php
+++ controllers/homes_controller.php
@@ -2318,7 +2318,10 @@ STR;
 		$libid = $_REQUEST['libid'];
 		$patronid = $_REQUEST['patronid'];
 		$patronid = $_REQUEST['patronid'];
-		$currentPatron = $this->Currentpatron->find('all',array('conditions' => array('libid' => $libid,'patronid' => "'".$patronid."'")));                
+		$currentPatron = $this->Currentpatron->find('all',array('conditions' => array('libid' => $libid,'patronid' => "'".$patronid."'")));             
+                echo "<br>libid: ".$libid;
+                echo "<br>patronid: ".$patronid;                
+                echo "<pre>"; print_r($currentPatron); 
                 
 		if(count($currentPatron) > 0){
 			$updateArr = array();
