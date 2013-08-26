f7bf8529ca1c75aa7618e8de03b2801f11e79257
Mangesh Pagnis <mangesh.pagnis@infobeans.com>
2013-08-26 14:55:10 +0530

    change for links for album and artist on download and wishlist page change in cache link

--- webroot/cron/cache.php
+++ webroot/cron/cache.php
@@ -1,16 +1,16 @@
 <?php
     error_reporting(E_ALL);
-	set_time_limit(0);
-	include 'functions.php';
-        //$memcache = new Memcache;
-	//$memcache->addServer('10.176.4.199', 11211);
-	$url = "http://www.freegaldev.com/cache/cacheGenre";
-	print $url;
-	$ch=curl_init();
-	curl_setopt($ch, CURLOPT_URL, $url);
-	curl_setopt($ch, CURLOPT_SSLVERSION, 3);
-	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER,1);
-	$result = curl_exec($ch);echo $result;
-	curl_close($ch);exit;	
+    set_time_limit(0);
+    include 'functions.php';
+    $memcache = new Memcache;
+    $memcache->addServer('10.178.4.51', 11211);
+    $url = "http://10.181.59.64/cache/cacheGenre";
+    print $url;
+    $ch=curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_SSLVERSION, 3);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER,1);
+    $result = curl_exec($ch);echo $result;
+    curl_close($ch);exit; 
 ?>
