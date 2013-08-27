15a4ecf854e35867afff72a211e7cb51d8ab2d6d
Narendra Nagesh <nagesh4group@gmail.com>
2013-08-27 11:55:38 +0530

    updated the pages

--- controllers/homes_controller.php
+++ controllers/homes_controller.php
@@ -1261,7 +1261,7 @@ STR;
 
                  $nationalTopVideoDownload = array();
                  $countryPrefix = $this->Session->read('multiple_countries');                 
-               echo  $sql_us_10_v =<<<STR
+                 $sql_us_10_v =<<<STR
                  SELECT 
                         Video.ProdID,
                         Video.ReferenceID,
