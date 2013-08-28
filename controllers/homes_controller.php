c05898ff5f808367fdebe9fd83d8a0493911b97e
Narendra Nagesh <nagesh4group@gmail.com>
2013-08-28 12:47:30 +0530

    updated the pages

--- controllers/homes_controller.php
+++ controllers/homes_controller.php
@@ -4351,7 +4351,7 @@ STR;
             //get Advisory condition
             $advisory_status = $this->getLibraryExplicitStatus($libraryId);
             //////////////////////////////////Videos/////////////////////////////////////////////////////////            
-            //Cache::delete("new_releases_videos".$territory); 
+            Cache::delete("new_releases_videos".$territory); 
             if (($coming_soon = Cache::read("new_releases_videos".$territory)) === false)    // Show from DB
             //if(1)
             {               
@@ -4385,7 +4385,7 @@ LEFT JOIN File AS Full_Files ON (Video.FullLength_FileID = Full_Files.FileID)
 LEFT JOIN Genre AS Genre ON (Genre.ProdID = Video.ProdID)
 LEFT JOIN {$countryPrefix}countries AS Country ON (Country.ProdID = Video.ProdID) AND (Video.provider_type = Country.provider_type)
 LEFT JOIN File AS Image_Files ON (Video.Image_FileID = Image_Files.FileID) 
-WHERE ((Video.DownloadStatus = '1')) AND (Country.Territory = '$territory') AND (Country.SalesDate != '') $advisory_status AND (Country.SalesDate <= NOW()) 
+WHERE ((Video.DownloadStatus = '1')) AND (Country.Territory = '$territory') AND (Country.SalesDate != '')  AND (Country.SalesDate <= NOW()) 
 GROUP BY Video.ProdID 
 ORDER BY Country.SalesDate DESC 
 LIMIT 100 
@@ -4410,7 +4410,7 @@ STR;
         $this->set('new_releases_videos', $coming_soon_videos);           
         
         //////////////////////////////////Albums/////////////////////////////////////////////////////////
-               
+          Cache::delete("new_releases_albums".$territory);     
         if (($coming_soon = Cache::read("new_releases_albums".$territory)) === false)    // Show from DB
         //if(1)
         {            
