2e860d2efd216a3f28af88d50ee11c3fd91932a0
Narendra Nagesh <nagesh4group@gmail.com>
2013-08-29 18:08:33 +0530

    updated the video pages cache

--- controllers/videos_controller.php
+++ controllers/videos_controller.php
@@ -47,7 +47,7 @@ class VideosController extends AppController {
             $this->set('patronDownload',$patronDownload);
         }        
 
-        Cache::delete("featured_videos".$territory);
+       // Cache::delete("featured_videos".$territory);
 	if ($featuredVideos = Cache::read("featured_videos" . $territory) === false) {
    		$featuredVideosSql = "SELECT `FeaturedVideo`.`id`,`FeaturedVideo`.`ProdID`,`Video`.`Image_FileID`, `Video`.`VideoTitle`, `Video`.`ArtistText`, `Video`.`provider_type`, Video.Advisory, `File`.`CdnPath`, `File`.`SourceURL`, `File`.`SaveAsName`,`Country`.`SalesDate` FROM featured_videos as FeaturedVideo LEFT JOIN video as Video on FeaturedVideo.ProdID = Video.ProdID and FeaturedVideo.provider_type = Video.provider_type LEFT JOIN File as File on File.FileID = Video.Image_FileID LEFT JOIN {$prefix}countries as Country on (`Video`.`ProdID`=`Country`.`ProdID` AND `Video`.`provider_type`=`Country`.`provider_type`) WHERE `FeaturedVideo`.`territory` = '" . $territory . "' AND `Country`.`SalesDate` <= NOW() ";
 		
@@ -64,7 +64,7 @@ class VideosController extends AppController {
         }
 
         
- 	Cache::delete("top_download_videos".$territory);
+ //	Cache::delete("top_download_videos".$territory);
 	if ($topDownloads = Cache::read("top_download_videos" . $territory) === false) {
             $topDownloadSQL = "SELECT Videodownloads.ProdID, Video.ProdID, Video.provider_type, Video.VideoTitle, Video.ArtistText, Video.Advisory, File.CdnPath, File.SourceURL, COUNT(DISTINCT(Videodownloads.id)) AS COUNT, `Country`.`SalesDate` FROM videodownloads as Videodownloads LEFT JOIN video as Video ON (Videodownloads.ProdID = Video.ProdID AND Videodownloads.provider_type = Video.provider_type) LEFT JOIN File as File ON (Video.Image_FileID = File.FileID) LEFT JOIN {$prefix}countries as Country on (`Video`.`ProdID`=`Country`.`ProdID` AND `Video`.`provider_type`=`Country`.`provider_type`) WHERE `Country`.`SalesDate` <= NOW()  GROUP BY Videodownloads.ProdID ORDER BY COUNT DESC limit 100";
             $topDownloads = $this->Album->query($topDownloadSQL);
@@ -493,7 +493,7 @@ STR;
         
         
         //  Video Details   //
-        
+        Cache::delete("musicVideoDetails" . $this->params['pass'][0]);
         if(isset($this->params['pass'][0]))
         {
             if ($VideosData = Cache::read("musicVideoDetails" . $this->params['pass'][0]) === false) {
@@ -547,7 +547,7 @@ STR;
             if(count($VideosData)>0)
             {    
                 
-                //Cache::delete("musicVideoMoreDetails_" .$territory.'_'.$VideosData[0]['Video']['ArtistText']);
+                Cache::delete("musicVideoMoreDetails_" .$territory.'_'.$VideosData[0]['Video']['ArtistText']);
                 if ($MoreVideosData = Cache::read("musicVideoMoreDetails_" .$territory.'_'.$VideosData[0]['Video']['ArtistText']) === false) {
                    $MoreVideosSql  =
                     "SELECT Video.ProdID, Video.ReferenceID,Video.Advisory, Video.VideoTitle, Video.ArtistText, Video.FullLength_Duration, Video.CreatedOn, Video.Image_FileID, Video.provider_type, Sample_Files.CdnPath,
@@ -594,7 +594,7 @@ STR;
             
                 $this->set('MoreVideosData',$MoreVideosData);
 
-                
+                Cache::delete("top_videos_genre_" . $territory.'_'.$VideosData[0]['Video']['Genre']);
                 //Top Genre Videos By Artist 
                 //if ($topDownloads = Cache::read("top_videos_genre" . $territory) === false)
                               
