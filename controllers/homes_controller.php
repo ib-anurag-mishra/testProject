7079b72f416437d98766125b4f5c39aad1efd6a9
kushal <kushal.pogul@infobeans.com>
2013-09-03 19:55:10 +0530

    auto scroll

--- controllers/homes_controller.php
+++ controllers/homes_controller.php
@@ -4669,6 +4669,8 @@ STR;
                 LIMIT $startLimit, $endLimit
                   
 STR;
+                echo "Query: ".$sql_national_100_v;
+                
                 //execute the query          
                 $nationalTopVideoDownload = $this->Album->query($sql_national_100_v);
                 
