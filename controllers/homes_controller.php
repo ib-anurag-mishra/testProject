86417ba11fe0060bf13ec41c644fc996d717e85c
Narendra Nagesh <nagesh4group@gmail.com>
2013-09-04 19:29:14 +0530

    updated the pages

--- controllers/homes_controller.php
+++ controllers/homes_controller.php
@@ -4711,7 +4711,7 @@ STR;
     */
     function userStreaming() {
         
-        Configure::write('debug', 0);
+        //Configure::write('debug', 0);
         
         //set layout false
         $this->layout = false;
