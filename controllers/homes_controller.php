acf930c4f89f0dbb1c4528bdc3248b93a1f38aed
kushal <kushal.pogul@infobeans.com>
2013-09-05 10:51:42 +0530

    auto scroll

--- controllers/homes_controller.php
+++ controllers/homes_controller.php
@@ -42,7 +42,7 @@ class HomesController extends AppController
           }
           else                                          //  Before Login
           {
-                $this->Auth->allow('display','aboutus', 'index', 'us_top_10','chooser','forgot_password', 'new_releases', 'language',  'checkPatron', 'approvePatron','my_lib_top_10','userStreaming');
+                $this->Auth->allow('display','aboutus', 'index', 'us_top_10','chooser','forgot_password', 'new_releases', 'language',  'checkPatron', 'approvePatron','my_lib_top_10','userStreaming', 'ajax_view_national_pagination');
           }
                 
         $this->Cookie->name = 'baker_id';
