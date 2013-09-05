a130467c22af9bbd96042f656b5367ab1541ba30
kushal <kushal.pogul@infobeans.com>
2013-09-05 14:21:22 +0530

    auto scroll

--- controllers/homes_controller.php
+++ controllers/homes_controller.php
@@ -206,8 +206,8 @@ STR;
   
 	//Cache::delete("nationalvideos".$territory);             
         // National Top Videos list and Downloads functionality code 
-        if (($national = Cache::read("nationalvideos".$territory."Page1")) === false) {
-       //         if(1) {
+       // if (($national = Cache::read("nationalvideos".$territory."Page1")) === false) {
+                if(1) {
             
                   
                 $country = $territory;
