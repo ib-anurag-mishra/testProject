cc983b38643cb361dfe1bee328d8e4a098144426
kushal <kushal.pogul@infobeans.com>
2013-09-09 14:13:53 +0530

    modify script

--- webroot/cron/loginautomation.php
+++ webroot/cron/loginautomation.php
@@ -2,7 +2,7 @@
 error_reporting(E_ALL);
 set_time_limit(0);
 include 'functions.php';
-$sql = 'SELECT * FROM `credentials` WHERE library_id NOT IN(20,22,37,49,44,68,69,79,147,170,419,497,477,81,82,85,97,116,155,163,244,267,327,328,168,169,108,109,210,216,222,224,228,232,234,235,242,253,262,282,283,284,351,378,385,408,425,426,427,428,392,429,456,473,465,481) Limit 50';
+$sql = 'SELECT * FROM `credentials` WHERE library_id NOT IN(20,22,37,49,44,68,69,79,147,170,419,497,477,81,82,85,97,116,155,163,244,267,327,328,168,169,108,109,210,216,222,224,228,232,234,235,242,253,262,282,283,284,351,378,385,408,425,426,427,428,392,429,456,473,465,481) Limit 25';
 //$sql = 'SELECT * FROM `credentials` WHERE library_id NOT IN (109,425,426,79)';
 $result = mysql_query($sql);
 while ($line = mysql_fetch_array($result, MYSQL_ASSOC)) {
@@ -167,16 +167,31 @@ foreach($credential as $k=>$v){
 			$result = curl_exec($ch);
 		}
 		echo $libraryArr[0]['id']."\n\n";
-		if(strpos($result,"successful") != false){
-		//	echo $libraryArr[0]['library_name']. " is working";
-		//	$working[$v['library_id']][$libraryArr[0]['library_name']] = 'working';
+		
+                if(strpos($result,"successful") != false){
+			echo $libraryArr[0]['library_name']. " is working";
+			//$working[$v['library_id']][$libraryArr[0]['library_name']] = 'working';
+                        $login .= $libraryArr[0]['library_name']." is working\n\n\n\n";
 		}
 		else{
-			if($libraryArr[0]['library_name']){
+			/*if($libraryArr[0]['library_name']){
 				$login .= $libraryArr[0]['library_name']." is not working\n\n\n\n";
 			//	$notWorking[$v['library_id']][$libraryArr[0]['library_name']] = "not working";
 				echo $libraryArr[0]['library_name']." is not working\n\n\n\n";
-			}
+			}*/
+                    
+                        if($libraryArr[0]['library_name']){
+					$arr = xml2array($result);
+				//	print_r($arr['posts']['message']);exit;
+					if(($libraryArr[0]['library_authentication_method'] != 'mdlogin_reference') && ($libraryArr[0]['library_authentication_method'] != 'mndlogin_reference')){
+					$message = $arr['posts']['message'];
+}
+					$login .= $libraryArr[0]['library_name']." is not working\n\n\n\n";
+				//	$notWorking[$v['library_id']][$libraryArr[0]['library_name']] = "not working";
+					echo $libraryArr[0]['library_name']." is not working<BR>".$message;
+				}
+                    
+                    
 		}
 	}
 }
@@ -184,4 +199,131 @@ foreach($credential as $k=>$v){
 //echo mail('tech@m68interactive.com',"Library Login Test",$login,'From:no-reply@freegalmusic.com');exit;
 echo mail('kushal.pogul@infobeans.com',"Library Login Test",$login,'From:no-reply@freegalmusic.com');exit;
 //print "<pre>";print_r($working);print_r($notWorking);exit;
+
+
+
+function xml2array($contents, $get_attributes=1, $priority = 'tag') {
+    if(!$contents) return array();
+
+    if(!function_exists('xml_parser_create')) {
+        //print "'xml_parser_create()' function not found!";
+        return array();
+    }
+
+    //Get the XML parser of PHP - PHP must have this module for the parser to work
+    $parser = xml_parser_create('');
+    xml_parser_set_option($parser, XML_OPTION_TARGET_ENCODING, "UTF-8"); # http://minutillo.com/steve/weblog/2004/6/17/php-xml-and-character-encodings-a-tale-of-sadness-rage-and-data-loss
+    xml_parser_set_option($parser, XML_OPTION_CASE_FOLDING, 0);
+    xml_parser_set_option($parser, XML_OPTION_SKIP_WHITE, 1);
+    xml_parse_into_struct($parser, trim($contents), $xml_values);
+    xml_parser_free($parser);
+
+    if(!$xml_values) return;//Hmm...
+
+    //Initializations
+    $xml_array = array();
+    $parents = array();
+    $opened_tags = array();
+    $arr = array();
+
+    $current = &$xml_array; //Refference
+
+    //Go through the tags.
+    $repeated_tag_index = array();//Multiple tags with same name will be turned into an array
+    foreach($xml_values as $data) {
+        unset($attributes,$value);//Remove existing values, or there will be trouble
+
+        //This command will extract these variables into the foreach scope
+        // tag(string), type(string), level(int), attributes(array).
+        extract($data);//We could use the array by itself, but this cooler.
+
+        $result = array();
+        $attributes_data = array();
+        
+        if(isset($value)) {
+            if($priority == 'tag') $result = $value;
+            else $result['value'] = $value; //Put the value in a assoc array if we are in the 'Attribute' mode
+        }
+
+        //Set the attributes too.
+        if(isset($attributes) and $get_attributes) {
+            foreach($attributes as $attr => $val) {
+                if($priority == 'tag') $attributes_data[$attr] = $val;
+                else $result['attr'][$attr] = $val; //Set all the attributes in a array called 'attr'
+            }
+        }
+
+        //See tag status and do the needed.
+        if($type == "open") {//The starting of the tag '<tag>'
+            $parent[$level-1] = &$current;
+            if(!is_array($current) or (!in_array($tag, array_keys($current)))) { //Insert New tag
+                $current[$tag] = $result;
+                if($attributes_data) $current[$tag. '_attr'] = $attributes_data;
+                $repeated_tag_index[$tag.'_'.$level] = 1;
+
+                $current = &$current[$tag];
+
+            } else { //There was another element with the same tag name
+
+                if(isset($current[$tag][0])) {//If there is a 0th element it is already an array
+                    $current[$tag][$repeated_tag_index[$tag.'_'.$level]] = $result;
+                    $repeated_tag_index[$tag.'_'.$level]++;
+                } else {//This section will make the value an array if multiple tags with the same name appear together
+                    $current[$tag] = array($current[$tag],$result);//This will combine the existing item and the new item together to make an array
+                    $repeated_tag_index[$tag.'_'.$level] = 2;
+                    
+                    if(isset($current[$tag.'_attr'])) { //The attribute of the last(0th) tag must be moved as well
+                        $current[$tag]['0_attr'] = $current[$tag.'_attr'];
+                        unset($current[$tag.'_attr']);
+                    }
+
+                }
+                $last_item_index = $repeated_tag_index[$tag.'_'.$level]-1;
+                $current = &$current[$tag][$last_item_index];
+            }
+
+        } elseif($type == "complete") { //Tags that ends in 1 line '<tag />'
+            //See if the key is already taken.
+            if(!isset($current[$tag])) { //New Key
+                $current[$tag] = $result;
+                $repeated_tag_index[$tag.'_'.$level] = 1;
+                if($priority == 'tag' and $attributes_data) $current[$tag. '_attr'] = $attributes_data;
+
+            } else { //If taken, put all things inside a list(array)
+                if(isset($current[$tag][0]) and is_array($current[$tag])) {//If it is already an array...
+
+                    // ...push the new element into that array.
+                    $current[$tag][$repeated_tag_index[$tag.'_'.$level]] = $result;
+                    
+                    if($priority == 'tag' and $get_attributes and $attributes_data) {
+                        $current[$tag][$repeated_tag_index[$tag.'_'.$level] . '_attr'] = $attributes_data;
+                    }
+                    $repeated_tag_index[$tag.'_'.$level]++;
+
+                } else { //If it is not an array...
+                    $current[$tag] = array($current[$tag],$result); //...Make it an array using using the existing value and the new value
+                    $repeated_tag_index[$tag.'_'.$level] = 1;
+                    if($priority == 'tag' and $get_attributes) {
+                        if(isset($current[$tag.'_attr'])) { //The attribute of the last(0th) tag must be moved as well
+                            
+                            $current[$tag]['0_attr'] = $current[$tag.'_attr'];
+                            unset($current[$tag.'_attr']);
+                        }
+                        
+                        if($attributes_data) {
+                            $current[$tag][$repeated_tag_index[$tag.'_'.$level] . '_attr'] = $attributes_data;
+                        }
+                    }
+                    $repeated_tag_index[$tag.'_'.$level]++; //0 and 1 index is already taken
+                }
+            }
+
+        } elseif($type == 'close') { //End of tag '</tag>'
+            $current = &$parent[$level-1];
+        }
+    }
+    
+    return($xml_array);
+}
+
 ?>
