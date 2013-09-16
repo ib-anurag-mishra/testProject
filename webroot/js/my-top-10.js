11e80e69b80479e3404df0c2bf5472a9fa4c245b
Mangesh Pagnis <mangesh.pagnis@infobeans.com>
2013-09-16 18:54:23 +0530

    changes for the my top 10 page for ajaxy issue

--- webroot/js/my-top-10.js
+++ webroot/js/my-top-10.js
@@ -1,27 +1,27 @@
 $(document).ready(function(){
 	
-	$('.songs-scrollable .song-container').on('mouseenter',function(){
+	$(document).on('mouseenter','.songs-scrollable .song-container',function(){
 		$(this).find('.add-to-playlist-button').css({opacity:1});
 		$(this).find('.top-10-download-now-button').css({opacity:1});
 		$(this).find('.preview').css({opacity:1});
 		
 	});
 	
-	$('.songs-scrollable .song-container').on('mouseleave',function(){
+	$(document).on('mouseleave','.songs-scrollable .song-container',function(){
 		$(this).find('.add-to-playlist-button').css({opacity:0});
 		$(this).find('.top-10-download-now-button').css({opacity:0});
 		$(this).find('.preview').css({opacity:0});
 		
 	});
 	
-	$('.videos-scrollable .video-container').on('mouseenter',function(){
+	$(document).on('mouseenter','.videos-scrollable .video-container',function(){
 		$(this).find('.add-to-playlist-button').css({opacity:1});
 		$(this).find('.top-10-download-now-button').css({opacity:1});
 		
 		
 	});
 	
-	$('.videos-scrollable .video-container').on('mouseleave',function(){
+	$(document).on('mouseleave','.videos-scrollable .video-container',function(){
 		$(this).find('.add-to-playlist-button').css({opacity:0});
 		$(this).find('.top-10-download-now-button').css({opacity:0});
 		
