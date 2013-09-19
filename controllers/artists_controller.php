0193c7ff8a0aa79a4b5bdc5b9ef15a9d4292aee2
Mangesh Pagnis <mangesh.pagnis@infobeans.com>
2013-09-19 20:59:01 +0530

    printing the query

--- controllers/artists_controller.php
+++ controllers/artists_controller.php
@@ -1521,9 +1521,9 @@ STR;
 		$this->Song->Behaviors->attach('Containable');
                 $this->Country->setTablePrefix($_REQUEST['Territory']);
 		foreach($allAlbum as $k => $v){
-			$recordCount = $this->Song->find('all', array('fields' => array('DISTINCT Song.ProdID'),'conditions' => array('Song.ReferenceID' => $v['Album']['ProdID'],'Song.DownloadStatus' => 1,'TrackBundleCount' => 0,'Country.Territory' => $_REQUEST['Territory']), 'contain' => array('Country' => array('fields' => array('Country.Territory'))), 'recursive' => 0,'limit' => 1));
-                        //echo "<br>Query2: ".$this->Song->lastQuery();die;
-                        if(count($recordCount) > 0){
+			$recordCount = $this->Song->find('count', array('fields' => array('DISTINCT Song.ProdID'),'conditions' => array('Song.ReferenceID' => $v['Album']['ProdID'],'Song.DownloadStatus' => 1,'TrackBundleCount' => 0,'Country.Territory' => $_REQUEST['Territory']), 'contain' => array('Country' => array('fields' => array('Country.Territory'))), 'recursive' => 0,'limit' => 1));
+                        echo "<br>Query2: ".$this->Song->lastQuery();die;
+                        if($recordCount > 0){
 				$val = $val.$v['Album']['ProdID'].",";
 				$result[$v['Album']['ProdID'] . '-'. $v['Album']['provider_type']] = $v['Album']['AlbumTitle'];
 			}
