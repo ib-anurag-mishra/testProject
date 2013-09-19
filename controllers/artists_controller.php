aefa01485868e7e24514a44c143b6662bcb2a4fe
Mangesh Pagnis <mangesh.pagnis@infobeans.com>
2013-09-19 21:04:36 +0530

    printing the values

--- controllers/artists_controller.php
+++ controllers/artists_controller.php
@@ -1522,12 +1522,16 @@ STR;
                 $this->Country->setTablePrefix($_REQUEST['Territory']);
 		foreach($allAlbum as $k => $v){
 			$recordCount = $this->Song->find('count', array('fields' => array('DISTINCT Song.ProdID'),'conditions' => array('Song.ReferenceID' => $v['Album']['ProdID'],'Song.DownloadStatus' => 1,'TrackBundleCount' => 0,'Country.Territory' => $_REQUEST['Territory']), 'contain' => array('Country' => array('fields' => array('Country.Territory'))), 'recursive' => 0,'limit' => 1));
-                        echo "<br>Query2: ".$this->Song->lastQuery();die;
+                        //echo "<br>Query2: ".$this->Song->lastQuery();die;
                         if($recordCount > 0){
 				$val = $val.$v['Album']['ProdID'].",";
 				$result[$v['Album']['ProdID'] . '-'. $v['Album']['provider_type']] = $v['Album']['AlbumTitle'];
 			}
 		}
+                echo "<pre>";
+                print_r($allAlbum . "<br>=====================");
+                print_r($result);
+                echo "</pre>";die;
 		$data = "<option value=''>SELECT</option>";
 		foreach($result as $k=>$v){
 			$data = $data."<option value='".$k."'>".$v."</option>";
