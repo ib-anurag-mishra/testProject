edb778135e663176e4aa42d63a2847ac564b99c5
kushal <kushal.pogul@infobeans.com>
2013-09-20 16:47:20 +0530

    report modifications

--- controllers/reports_controller.php
+++ controllers/reports_controller.php
@@ -957,6 +957,10 @@ Class ReportsController extends AppController
 					$this->set('libraries_download', $this->Library->find('all', array('fields' => array('Library.library_name','Library.library_unlimited','Library.library_available_downloads'),'conditions' => array('Library.id = '.$library_id,'Library.library_territory= "'.$territory.'"'),'order' => 'Library.library_name ASC', 'recursive' => -1)));
 				}
 			}
+                        
+                        echo "<br>Date Range: ".$this->data['Report']['reports_daterange'];
+                        echo "<pre<"; print_r($this->data);
+                        
             if($this->Report->validates()) {
                 if($this->data['Report']['reports_daterange'] == 'day') {
                   $date_arr = explode("/", $this->data['Report']['date']);
