a481d174898808efe21fe5583cf1991483d422cb
Taranjeet Singh <taran2010jeet@gmail.com>
2013-09-20 14:20:01 +0530

    updated for solr

--- controllers/services_controller.php
+++ controllers/services_controller.php
@@ -3,7 +3,7 @@ class ServicesController extends AppController {
     var $name = 'Services';
     var $autoLayout = false;
     var $uses = array('Library', 'Song', 'Country', 'Genre', 'Files', 'Album','Currentpatron', 'Download','Variable','Url','Language','Consortium');
-	var $components = array('RequestHandler');
+	var $components = array('Solr', 'RequestHandler');
 	var $helpers = array('Xml'); // helpers used	
 	
     function search() {
@@ -193,7 +193,198 @@ class ServicesController extends AppController {
 		}
 	}
 	
-	function genre(){
+    function genre(){
+		if($this->params['pass'][3] == ''){
+		$consortium = $this->Consortium->find('all',array(
+                                                'conditions' => 
+												array('consortium_key' => $this->params['pass'][0])
+												)
+                                            );
+		if(count($consortium) > 0){
+			$this->Library->recursive = -1;
+			$existingLibraries = $this->Library->find('all',array(
+													'conditions' => 
+													array('library_apikey' => $consortium[0]['Consortium']['consortium_name'],
+														  'id' => $this->params['pass'][1],
+														  'library_status' => 'active')
+													)
+												);
+			if(count($existingLibraries) == 0){
+				$result = array('status' => 0 , 'message' => 'Access Denied');
+				$this->set('result', $result);
+				return;
+			}
+			else{				
+				$country = $existingLibraries['0']['Library']['library_territory'];
+				$this->Genre->Behaviors->attach('Containable');
+				$this->Genre->recursive = 2;
+				if (($genre = Cache::read("genre".$country)) === false) {
+						$genreAll = $this->Genre->find('all',array(
+								'conditions' =>
+									array('and' =>
+										array(
+											array('Country.Territory' => $country)
+										)
+									),
+								'fields' => array(
+										'Genre.Genre'
+										),
+								'contain' => array(
+									'Country' => array(
+											'fields' => array(
+													'Country.Territory'								
+												)
+											),
+								),'group' => 'Genre.Genre'
+						));
+					Cache::write("genre".$country, $genreAll);
+				}
+				$genreAll = Cache::read("genre".$country);
+				foreach($genreAll as $k=>$v){
+					$result[$k]['Genre'] = $v['Genre']['Genre'];
+				}
+				if(count($result) > 0){
+					$result = $result;
+				}
+				else{
+					$result = array('message' => 'No Records');
+				}		
+				$this->set('result', $result);
+			}
+		}
+		else{
+			$result = array('status' => 0 , 'message' => 'Access Denied');
+			$this->set('result', $result);
+			return;		
+		}
+	}
+	else
+	{
+		$consortium = $this->Consortium->find('all',array(
+                                                'conditions' => 
+												array('consortium_key' => $this->params['pass'][0])
+												)
+                                            );
+		if(count($consortium) > 0){
+			$this->Library->recursive = -1;
+			$existingLibraries = $this->Library->find('all',array(
+													'conditions' => 
+													array('library_apikey' => $consortium[0]['Consortium']['consortium_name'],
+														  'id' => $this->params['pass'][1],
+														  'library_status' => 'active')
+													)
+												);
+			if(count($existingLibraries) == 0){
+				$result = array('status' => 0 , 'message' => 'Access Denied');
+				$this->set('result', $result);
+				return;
+			}
+			else{			
+				$country = $existingLibraries['0']['Library']['library_territory'];
+				if($existingLibraries['0']['Library']['library_block_explicit_content'] == '1'){
+					$condSolr = "Advisory:F AND Territory:".addslashes($country);
+				}
+				else {
+					$condSolr = "Territory:".addslashes($country);
+				}
+				if($this->params['pass'][4] != ''){
+					$genre = $this->params['pass'][4];
+				}else{
+					$genre = $this->params['pass'][3];
+				}
+	//			print $genre;exit;
+				$searchString =  base64_decode($genre);	
+				$searchString = str_replace("^", " ", $searchString);					
+				$searchString = str_replace("$", " ", $searchString);
+				$sphinxCheckCondition = "AND";
+				if($genre != '') {
+					$solrGenreSearch = 'Genre:'.addslashes($searchString).'" '.$sphinxCheckCondition.' ';	
+				}
+				else {
+					$solrGenreSearch = '';
+				}			
+				$solrTempCondition = $solrGenreSearch;
+				$solrFinalCondition = substr($sphinxTempCondition, 0, -2);
+				$solrFinalCondition = $solrFinalCondition.' AND DownloadStatus:1 AND '.$condSolr;
+				if ($condSolr == "") {
+					$solrFinalCondition = substr($solrFinalCondition, 0, -2);
+				}
+			
+				// App::import('vendor', 'sphinxapi', array('file' => 'sphinxapi.php'));
+				if (isset($this->passedArgs['sort'])){
+					$solrSort = $this->passedArgs['sort'];
+				} else {
+					$solrSort = "";
+				}
+				if (isset($this->passedArgs['direction'])){
+					$solrDirection = $this->passedArgs['direction'];
+				} else {
+					$solrDirection = "";
+				}
+				
+				/*$this->paginate = array('Song' => array(
+							'sphinx' => 'yes', 'sphinxcheck' => $sphinxFinalCondition, 'sphinxsort' => $sphinxSort, 'sphinxdirection' => $sphinxDirection, 'webservice' => 1
+						));
+				
+				$searchResults = $this->paginate('Song');*/
+				$reference = '';
+				foreach($searchResults as $k=>$v){
+					$result[$k]['Song']['ProdID'] = $v['Song']['ProdID'];
+					$result[$k]['Song']['ProductID'] = $v['Song']['ProductID'];
+					$result[$k]['Song']['ReferenceID'] = $v['Song']['ReferenceID'];
+					$result[$k]['Song']['Title'] = $v['Song']['Title'];
+					$result[$k]['Song']['SongTitle'] = $v['Song']['SongTitle'];
+					$result[$k]['Song']['ArtistText'] = $v['Song']['ArtistText'];
+					$result[$k]['Song']['provider_type'] = $v['Song']['provider_type'];
+					$result[$k]['Song']['Artist'] = $v['Song']['Artist'];
+					$result[$k]['Song']['Advisory'] = $v['Song']['Advisory'];
+					$result[$k]['Song']['Composer'] = str_replace('"','',$v['Song']['Composer']);
+					$result[$k]['Song']['Genre'] = str_replace('"','',$v['Song']['Genre']);
+					if(isset($this->params['pass'][4])){
+						$result[$k]['Song']['freegal_url'] = "https://".$_SERVER['HTTP_HOST']."/services/login/".$this->params['pass'][0]."/".$this->params['pass'][1]."/".$this->params['pass'][2]."/".$this->params['pass'][3]."/".$v['Song']['ReferenceID']."/".base64_encode($v['Song']['ArtistText'])."/".base64_encode($v['Song']['provider_type']);
+					}
+					else{
+						$result[$k]['Song']['freegal_url'] = "https://".$_SERVER['HTTP_HOST']."/services/login/".$this->params['pass'][0]."/".$this->params['pass'][1]."/".$this->params['pass'][2]."/".$v['Song']['ReferenceID']."/".base64_encode($v['Song']['ArtistText'])."/".base64_encode($v['Song']['provider_type']);					
+					}
+					if($reference != $v['Song']['ReferenceID']){ 
+						$albumData = $this->Album->find('all', array(
+							'conditions'=>array('Album.ProdID' => $v['Song']['ReferenceID']),
+							'fields' => array(
+								'Album.ProdID',
+							),
+							'contain' => array(										
+								'Files' => array(
+									'fields' => array(
+										'Files.CdnPath',
+										'Files.SaveAsName',
+										'Files.SourceURL',
+										),                             
+								)
+						)));
+						$reference = $v['Song']['ReferenceID'];
+						$albumArtWork = Configure::read('App.Music_Path').shell_exec('perl files/tokengen ' . $albumData[0]['Files']['CdnPath']."/".$albumData[0]['Files']['SourceURL']);
+					//	print_r($result);exit;
+					}
+					$result[$k]['Song']['Album_Artwork'] = $albumArtWork;				
+				}
+				if(count($result) > 0){
+					$result = $result;
+				}
+				else{
+					$result = array('message' => 'No Records');
+				}
+				$this->set('result', $result);
+			}
+		}
+		else{
+			$result = array('status' => 0 , 'message' => 'Access Denied');
+			$this->set('result', $result);
+			return;		
+		}	
+	}
+	}
+    
+	function genre_old(){
 		if($this->params['pass'][3] == ''){
 		$consortium = $this->Consortium->find('all',array(
                                                 'conditions' => 
