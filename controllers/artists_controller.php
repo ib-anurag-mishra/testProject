7647231e8650a7b29ac12735def0e385423474ca
Mangesh Pagnis <mangesh.pagnis@infobeans.com>
2013-09-20 12:18:50 +0530

    changes done to get the drop down

--- controllers/artists_controller.php
+++ controllers/artists_controller.php
@@ -1521,17 +1521,14 @@ STR;
 		$this->Song->Behaviors->attach('Containable');
                 $this->Country->setTablePrefix($_REQUEST['Territory']);
 		foreach($allAlbum as $k => $v){
+                        $recordCount = 0;
 			$recordCount = $this->Song->find('all', array('fields' => array('DISTINCT Song.ProdID'),'conditions' => array('Song.ReferenceID' => $v['Album']['ProdID'],'Song.DownloadStatus' => 1,'TrackBundleCount' => 0,'Country.Territory' => $_REQUEST['Territory']), 'contain' => array('Country' => array('fields' => array('Country.Territory'))), 'recursive' => 0,'limit' => 1));
-                        print_r($recordCount);
-                        echo "<br>Query2: ".$this->Song->lastQuery();
-                        echo count($recordCount);
                         if(count($recordCount) > 0){
 				$val = $val.$v['Album']['ProdID'].",";
 				$result[$v['Album']['ProdID'] . '-'. $v['Album']['provider_type']] = $v['Album']['AlbumTitle'];
 			}
 		}
-                print_r($result);die;
-		$data = "<option value=''>SELECT</option>";
+                $data = "<option value=''>SELECT</option>";
 		foreach($result as $k=>$v){
 			$data = $data."<option value='".$k."'>".$v."</option>";
 		}
