a16ac800d52fb1da9c2bf9e21ba54ff0648ba7ae
Mangesh Pagnis <mangesh.pagnis@infobeans.com>
2013-09-23 19:06:52 +0530

    adding the helper file

---
+++ views/helpers/videodownload.php
@@ -0,0 +1,11 @@
+<?php
+/*
+	 File Name : videodownload.php
+	 File Description : helper file for getting download details
+	 Author : m68interactive
+ */
+class VideodownloadHelper extends AppHelper {
+    var $uses = array('Videodownload');
+}
+
+?>
