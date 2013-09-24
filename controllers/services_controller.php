405545f29efeefbaae2e40a53e8dc505b0099d96
Taranjeet Singh <taran2010jeet@gmail.com>
2013-09-24 20:42:14 +0530

    updated for testing

--- controllers/services_controller.php
+++ controllers/services_controller.php
@@ -159,6 +159,7 @@ class ServicesController extends AppController {
                 if ($response->getHttpStatus() == 200) {
                     echo "here";
                     //print_r($response);
+                    print_r($response->response);
                     echo $response->response->numFound;
                      die;
                     if ($response->response->numFound > 0) {
