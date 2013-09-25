13dff8c8fb4af9ae0cf64859605b3164d760c65b
Mangesh Pagnis <mangesh.pagnis@infobeans.com>
2013-09-25 16:03:53 +0530

    for not allowing empty albums to be in the list

--- controllers/artists_controller.php
+++ controllers/artists_controller.php
@@ -1515,12 +1515,13 @@ STR;
 		$this->Song->Behaviors->attach('Containable');
                 $this->Country->setTablePrefix($_REQUEST['Territory']);
 		foreach($allAlbum as $k => $v){
-                        $recordCount = 0;
-			$recordCount = $this->Song->find('all', array('fields' => array('DISTINCT Song.ProdID'),'conditions' => array('Song.ReferenceID' => $v['Album']['ProdID'],'Song.DownloadStatus' => 1,'TrackBundleCount' => 0,'Country.Territory' => $_REQUEST['Territory']), 'contain' => array('Country' => array('fields' => array('Country.Territory'))), 'recursive' => 0,'limit' => 1));
-                        if(count($recordCount) > 0){
-				$val = $val.$v['Album']['ProdID'].",";
-				$result[$v['Album']['ProdID'] . '-'. $v['Album']['provider_type']] = $v['Album']['AlbumTitle'];
-			}
+                        $recordCount = $this->Song->find('all', array('fields' => array('DISTINCT Song.ProdID'),'conditions' => array('Song.ReferenceID' => $v['Album']['ProdID'],'Song.DownloadStatus' => 1,'TrackBundleCount' => 0,'Country.Territory' => $_REQUEST['Territory']), 'contain' => array('Country' => array('fields' => array('Country.Territory'))), 'recursive' => 0,'limit' => 1));
+                        if(!empty($recordCount)){
+                            if(count($recordCount) > 0){
+                                $val = $val.$v['Album']['ProdID'].",";
+                                $result[$v['Album']['ProdID'] . '-'. $v['Album']['provider_type']] = $v['Album']['AlbumTitle'];
+                            }
+                        }
 		}
                 $data = "<option value=''>SELECT</option>";
 		foreach($result as $k=>$v){
