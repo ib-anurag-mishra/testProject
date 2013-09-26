49a3417e5358264de472042232685c0ebaf06637
Mangesh Pagnis <mangesh.pagnis@infobeans.com>
2013-09-26 12:49:00 +0530

    changes to the featured artist drop down to get album changed the way prefix is attached to the query

--- controllers/artists_controller.php
+++ controllers/artists_controller.php
@@ -1502,14 +1502,15 @@ STR;
 		$allAlbum = $this->Album->find('all', array('fields' => array('Album.ProdID','Album.AlbumTitle','Album.provider_type'),'conditions' => array('Album.ArtistText = ' => urldecode($_REQUEST['artist'])), 'recursive' => -1));
                 $val = '';
 		$this->Song->Behaviors->attach('Containable');
-                $this->Country->setTablePrefix($_REQUEST['Territory']);
+                $countryPrefix = strtolower($_REQUEST['Territory'])."_";
+                $this->Country->setTablePrefix($countryPrefix);
 		foreach($allAlbum as $k => $v){
-			$recordCount = $this->Song->find('all', array('fields' => array('DISTINCT Song.ProdID'),'conditions' => array('Song.ReferenceID' => $v['Album']['ProdID'],'Song.DownloadStatus' => 1,'TrackBundleCount' => 0,'Country.Territory' => $_REQUEST['Territory']), 'contain' => array('Country' => array('fields' => array('Country.Territory'))), 'recursive' => 0,'limit' => 1));
-                        if(count($recordCount) > 0){
-				$val = $val.$v['Album']['ProdID'].",";
-				$result[$v['Album']['ProdID'] . '-'. $v['Album']['provider_type']] = $v['Album']['AlbumTitle'];
-			}
-		}
+                    $recordCount = $this->Song->find('all', array('fields' => array('DISTINCT Song.ProdID'),'conditions' => array('Song.ReferenceID' => $v['Album']['ProdID'],'Song.DownloadStatus' => 1,'TrackBundleCount' => 0,'Country.Territory' => $_REQUEST['Territory']), 'contain' => array('Country' => array('fields' => array('Country.Territory'))), 'recursive' => 0,'limit' => 1));
+                    if(count($recordCount) > 0){
+                        $val = $val.$v['Album']['ProdID'].",";
+                        $result[$v['Album']['ProdID'] . '-'. $v['Album']['provider_type']] = $v['Album']['AlbumTitle'];
+                    }
+                }
 		$data = "<option value=''>SELECT</option>";
 		foreach($result as $k=>$v){
 			$data = $data."<option value='".$k."'>".$v."</option>";
