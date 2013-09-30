f51c7ba3571cac92985affc20a654a2a8719651b
kushal <kushal.pogul@infobeans.com>
2013-09-30 15:05:57 +0530

    encoding

--- views/dataencode.php
+++ views/dataencode.php
@@ -4,8 +4,7 @@ class dataencodeView extends View {
   function getTextEncode($text) {
 
     $text = @iconv(mb_detect_encoding($text), "WINDOWS-1252//IGNORE", $text);
-    //$text = @iconv(mb_detect_encoding($text), "ISO-8859-1//IGNORE", $text);
-    return @iconv(mb_detect_encoding($text), "ISO-8859-1//IGNORE", $text);
+    return @iconv(mb_detect_encoding($text), "UTF-16//IGNORE", $text);
   }
 
 
