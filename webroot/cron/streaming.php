9f9614ebe4545e4121eda31ad637c16fbfd78325
Narendra Nagesh <nagesh4group@gmail.com>
2013-10-04 18:28:34 +0530

    updated the pages

--- webroot/cron/streaming.php
+++ webroot/cron/streaming.php
@@ -114,11 +114,14 @@ class Streaming {
           mysql_select_db($this->STAGE_FREEGAL_DB, $this->freegalDBConnectionObj) 
             or die('Could not select freegal database.');
           
+          /*
            //connect to orchard database
           $this->orchardDBConnectionObj = mysql_connect($this->STAGE_DB_HOST, $this->STAGE_DB_USER, $this->STAGE_DB_PASS, true)
             or die('Could not connect to mysql server for freegal db of live.' );
           mysql_select_db($this->STAGE_ORCHARD_DB, $this->orchardDBConnectionObj) 
-            or die('Could not select orchard database.');         
+            or die('Could not select orchard database.');  
+           * 
+           */       
      }
           
   }
