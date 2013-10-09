6897e3fc85ed86228fd567e0da4ee7726b9f70da
Narendra <nagesh4group@gmail.com>
2013-10-09 22:51:43 +0530

    updated the pages

--- webroot/cron/set.php
+++ webroot/cron/set.php
@@ -14,11 +14,11 @@ $Streaming = new Streaming();
 //Instance
 $Streaming->Instance   = 1;
 //LIMIT of SELECT query (Songs,Files)
-$Streaming->ChunkSize  = 10;
+$Streaming->ChunkSize  = 1000;
 //1st value in LIMIT in SELECT query (Songs,Files) : Start point - ProdID
 $Streaming->LimitIndex = 0;
 //2nd value in LIMIT in SELECT query (Songs,Files) : Totals rows count
-$Streaming->LimitCount = 100;
+$Streaming->LimitCount = 100000;
 //if script will run in live or test environment
 $Streaming->LIVE       = 0;
 
