f86abdda89b75ed8c2f55085102f456fbe8891d6
Narendra Nagesh <nagesh4group@gmail.com>
2013-10-15 17:00:22 +0530

    updated the pages

--- controllers/genres_controller.php
+++ controllers/genres_controller.php
@@ -424,7 +424,7 @@ Class GenresController extends AppController
                                                                 )),
                                         ),
                                         'extra' => array('chk' => 1),                                    
-                                    'limit' => '60', 'cache' => 'yes','check' => 2
+                                    'limit' => '60', 'cache' => 'no','check' => 2
                                     );
                 } else {
                    
