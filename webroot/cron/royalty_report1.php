bcc2e15350d1d2e3b08f77208e025d658f3cc761
Taranjeet Singh <taran2010jeet@gmail.com>
2013-10-17 20:10:32 +0530

    updated

--- webroot/cron/royalty_report1.php
+++ webroot/cron/royalty_report1.php
@@ -27,6 +27,7 @@ $unit_sales_rate = null;
 foreach($arr_dates AS $key => $value) {
     foreach($libraryType as $libTypeKey=>$libTypeValue){
 		if($libraryType == 'ALC'){
+			echo "here";
 			$unit_sales_rate = 0.25;
 		} else {
 			$unit_sales_rate = 0;
