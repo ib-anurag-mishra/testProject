4c87de3befeffbf4a9680514ca608d570e907b7a
kiranpyati <kiran.pyati@infobeans.com>
2013-10-18 13:03:00 +0530

    [UPDATED] - TOP 10 LIB LOGIN BUTTON ISSUE

--- webroot/js/site.js
+++ webroot/js/site.js
@@ -1462,6 +1462,20 @@ $(function() {
         $(this).find('.preview').css({opacity: 0});
 
     });
+    
+      $(document).on('mouseenter', '.songs-scrollable .song-container', function() {
+       $(this).find('.add-to-playlist-button').css({opacity: 1});
+        $(this).find('.top-10-download-now-button').css({opacity: 1});
+        $(this).find('.preview').css({opacity: 1});
+
+    });
+    
+      $(document).on('mouseleave', '.songs-scrollable .song-container', function() {
+        $(this).find('.add-to-playlist-button').css({opacity: 0});
+        $(this).find('.top-10-download-now-button').css({opacity: 0});
+        $(this).find('.preview').css({opacity: 0});
+
+    });
 
     $('.videos-scrollable .video-container').on('mouseenter', function() {
         $(this).find('.add-to-playlist-button').css({opacity: 1});
