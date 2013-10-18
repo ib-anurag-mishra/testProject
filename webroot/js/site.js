53c9d1c12b395e46b049d5e4f1758048e78ee922
kiranpyati <kiran.pyati@infobeans.com>
2013-10-18 14:15:18 +0530

    [UPDATED] - JS ISSUE ON US TOP 10

--- webroot/js/site.js
+++ webroot/js/site.js
@@ -1487,13 +1487,13 @@ $(function() {
 
     });
     
-     $(document).on('mouseenter', '.songs-scrollable .song-container', function() {
+     $(document).on('mouseenter', '.videos-scrollable .video-container', function() {
         $(this).find('.add-to-playlist-button').css({opacity: 1});
         $(this).find('.top-10-download-now-button').css({opacity: 1});
 
     });
 
-    $(document).on('mouseleave', '.songs-scrollable .song-container', function() {
+    $(document).on('mouseleave', '.videos-scrollable .video-container', function() {
         $(this).find('.add-to-playlist-button').css({opacity: 0});
         $(this).find('.top-10-download-now-button').css({opacity: 0});
 
