e9447b7d61fc9d5019843c18b58443b6fd8b38eb
kushal <kushal.pogul@infobeans.com>
2013-10-19 14:02:46 +0530

    song error

--- controllers/homes_controller.php
+++ controllers/homes_controller.php
@@ -84,8 +84,7 @@ class HomesController extends AppController
 
 
         // National Top 100 Songs slider and Downloads functionality
-        //if (($national = Cache::read("national".$territory)) === false) {
-        if(1){
+        if (($national = Cache::read("national".$territory)) === false) {
             $nationalTopDownload = $this->Common->getNationalTop100($territory);
         }else{
             $nationalTopDownload = Cache::read("national".$territory);                
