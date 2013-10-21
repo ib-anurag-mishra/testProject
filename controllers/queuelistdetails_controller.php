cdf205077fde11a60c37ed698efb8767fd2eb8e8
mohan <mohanrocku@gmail.com>
2013-10-21 18:49:57 +0530

    updated for player

--- controllers/queuelistdetails_controller.php
+++ controllers/queuelistdetails_controller.php
@@ -17,7 +17,7 @@ class QueueListDetailsController extends AppController{
     function beforeFilter(){
            
             parent::beforeFilter();
-            $this->Auth->allow('now_streaming', 'queue_details', 'index','clearNowStreamingSession', 'ajaxQueueValidation');            
+            $this->Auth->allow('now_streaming', 'queue_details', 'index','getPlaylistData','clearNowStreamingSession', 'ajaxQueueValidation');            
     }
     
     
