64850f25a66abd6897963520e4814fbb2234507d
kushal <kushal.pogul@infobeans.com>
2013-10-23 12:22:08 +0530

    add to queue

--- controllers/queues_controller.php
+++ controllers/queues_controller.php
@@ -66,7 +66,7 @@ class QueuesController extends AppController{
     
     function addToQueue(){
         Configure::write('debug', 2);
-         $this->Streaming->getStreamingDetails();
+         $this->Streaming->getStreamingDetails($_REQUEST['songProdId'], $_REQUEST['songProviderType']); die;
         if( $this->Session->read('library') && $this->Session->read('patron') && !empty($_REQUEST['songProdId']) && !empty($_REQUEST['songProviderType'])&& !empty($_REQUEST['albumProdId'])&& !empty($_REQUEST['albumProviderType'])&& !empty($_REQUEST['queueId']) ){
             $queuesongsCount =  $this->QueueDetail->find('count',array('conditions' => array('queue_id' => $_REQUEST['queueId'],'song_prodid' => $_REQUEST['songProdId'],'song_providertype' => $_REQUEST['songProviderType'],'album_prodid' => $_REQUEST['albumProdId'],'album_providertype' => $_REQUEST['albumProviderType'])));
             if(!$queuesongsCount){
