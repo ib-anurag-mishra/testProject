9d2dda9c16c5c24fd5eed071cc0c79362fd5f1c9
kushal <kushal.pogul@infobeans.com>
2013-10-24 14:57:18 +0530

    history

--- controllers/queues_controller.php
+++ controllers/queues_controller.php
@@ -234,7 +234,8 @@ class QueuesController extends AppController{
                                                                             ),
                                                             'group' => 'StreamingHistory.ProdID, StreamingHistory.provider_type, StreamingHistory.createdOn',
                                                             'conditions' => array('StreamingHistory.library_id' => $libraryId,
-                                                                                  'StreamingHistory.patron_id' => $patronId,                                                                                  
+                                                                                  'StreamingHistory.patron_id' => $patronId, 
+                                                                                  'QueueList.queue_lists' => $patronId, 
                                                                                   'StreamingHistory.createdOn BETWEEN ? AND ?' => array(Configure::read('App.twoWeekStartDate'),
                                                                                   Configure::read('App.twoWeekEndDate'))
                                                                                  ),
