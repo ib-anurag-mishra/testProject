f58b121cc0fa1445d5e168fcdd42dc095219af07
kushal <kushal.pogul@infobeans.com>
2013-10-25 21:40:53 +0530

    player problem

--- controllers/components/common.php
+++ controllers/components/common.php
@@ -171,15 +171,10 @@ STR;
                         $data[$key]['songAlbumImage'] = $songAlbumImage;
                         
                         if($this->Session->read('library_type')==2)
-                        {
-                            echo "<br>1CdnPath: ".$value['Sample_Files']['SaveAsName'];
-                            echo "<br>1SaveAsName: ".$value['Full_Files']['SaveAsName'];
-                             echo "<br>1SaveAsName: ".$value['File']['SaveAsName'];
+                        {                            
                             
-                            $filePath = shell_exec('perl files/tokengen_streaming '. $value['File']['CdnPath']."/".$value['File']['Sample_Files']);
-                            echo "<br>$filePath: ".$filePath;
-                            echo "<br>2CdnPath: ".$value['File']['CdnPath'];
-                            echo "<br>2SaveAsName: ".$value['File']['SaveAsName'];
+                            $filePath = shell_exec('perl files/tokengen_streaming '. $value['File']['CdnPath']."/".$value['Sample_Files']['SaveAsName']);
+
                             
                             if(!empty($filePath))
                              {
