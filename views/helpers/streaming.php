5207d54172382db3b234f87ae8470082b86bf82a
kushal <kushal.pogul@infobeans.com>
2013-10-28 17:45:28 +0530

    stream time

--- views/helpers/streaming.php
+++ views/helpers/streaming.php
@@ -17,14 +17,14 @@ class StreamingHelper extends AppHelper {
     */
     function getTotalStreamTime($library_id, $patron_id){    //echo 'in'.$patron_id;
         
-        if($patron_id=='8389')
-        {     
-            //echo 'patron_id: '.$patron_id.", library_id: ".$library_id;
-            $streamingInstance = ClassRegistry::init('StreamingRecords');
-            $streamingInstance->recursive = -1;
-            $streamingDetails = $this->$streamingInstance->find('first', array('conditions' => array('patron_id' => $patron_id, 'library_id' => $library_id), 'fields' => 'consumed_time'));
-            print_r($streamingDetails);
-        }        
+//        if($patron_id=='8389')
+//        {     
+//            //echo 'patron_id: '.$patron_id.", library_id: ".$library_id;
+//            $streamingInstance = ClassRegistry::init('StreamingRecords');
+//            $streamingInstance->recursive = -1;
+//            $streamingDetails = $this->$streamingInstance->find('first', array('conditions' => array('patron_id' => $patron_id, 'library_id' => $library_id), 'fields' => 'consumed_time'));
+//            print_r($streamingDetails);
+//        }        
      
         
         return $streamingDetails;      
