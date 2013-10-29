9a9bbbc13a009553a374e2ae74f80cd67835a610
Narendra <nagesh4group@gmail.com>
2013-10-29 18:31:49 +0530

    updated the pages

--- controllers/components/queue.php
+++ controllers/components/queue.php
@@ -39,7 +39,6 @@ Class QueueComponent extends Object
     
     
     function getQueueDetails($queueID){
-        Configure::write('debug', 0);
         $queueDetailList = ClassRegistry::init('QueueDetail');
         $queueDetail = $queueDetailList->find('all',
           array(
@@ -92,7 +91,6 @@ Class QueueComponent extends Object
             'conditions' => array('QueueList.status' => 1, 'QueueDetail.queue_id' => $queueID ),                
           )
         );
-           
         return $queueDetail;
         
         
