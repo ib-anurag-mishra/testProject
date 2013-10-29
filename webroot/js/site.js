5f5f4e7fd980cf73530c7f61e5a3ff64d00061a8
daviddickson <david@m68interactive.com>
2013-10-28 23:24:23 -0400

    js edits

--- webroot/js/site.js
+++ webroot/js/site.js
@@ -1174,13 +1174,13 @@ $('document').ready(function()
     });
     
     $('.songs-scrollable .song-container').on('mouseleave',function(){
-	    
-    $(document).on('mouseleave', '.songs-scrollable .song-container', function() {
         $(this).find('.add-to-playlist-button').css({opacity: 0});
         $(this).find('.top-10-download-now-button').css({opacity: 0});
-        $(this).find('.preview').css({opacity: 0});	    
-	    
+        $(this).find('.preview').css({opacity: 0});    
+    
     });
+	    
+
     
     $('.videos-scrollable .video-container').on('mousenter',function() {
         $(this).find('.add-to-playlist-button').css({opacity: 1});
