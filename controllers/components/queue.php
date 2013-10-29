60345c5dad7f01a7b06c382c1cb0762cea40b463
Narendra <nagesh4group@gmail.com>
2013-10-29 16:17:30 +0530

    updated the pages

--- controllers/components/queue.php
+++ controllers/components/queue.php
@@ -39,11 +39,11 @@ Class QueueComponent extends Object
     
     
     function getQueueDetails($queueID){
-        Configure::write('debug', 0);
+        Configure::write('debug', 2);
         $queueDetailList = ClassRegistry::init('QueueDetail');
         $queueDetail = $queueDetailList->find('all',
           array(
-            'fields' =>  array('QueueDetail.id', 'QueueList.queue_name', 'QueueList.description', 'Songs.SongTitle','Songs.ReferenceID', 'Songs.FullLength_Duration', 'Songs.ProdID', 'Songs.provider_type', 'Songs.Title as STitle', 'Songs.ArtistText',  'Songs.Artist', 'Albums.AlbumTitle','Albums.ProdID','Albums.provider_type', 'Albums.Title as ATitle', 'Product.pid as AlbumProdID', 'AlbumFile.CdnPath as ACdnPath', 'AlbumFile.SourceURL as ASourceURL', 'SongFile.CdnPath as SCdnPath', 'SongFile.SaveAsName as SSaveAsName'),
+            'fields' =>  array('QueueDetail.id1', 'QueueList.queue_name', 'QueueList.description', 'Songs.SongTitle','Songs.ReferenceID', 'Songs.FullLength_Duration', 'Songs.ProdID', 'Songs.provider_type', 'Songs.Title as STitle', 'Songs.ArtistText',  'Songs.Artist', 'Albums.AlbumTitle','Albums.ProdID','Albums.provider_type', 'Albums.Title as ATitle', 'Product.pid as AlbumProdID', 'AlbumFile.CdnPath as ACdnPath', 'AlbumFile.SourceURL as ASourceURL', 'SongFile.CdnPath as SCdnPath', 'SongFile.SaveAsName as SSaveAsName'),
             'joins' => array(
               array(
                 'type' => 'INNER',
@@ -92,7 +92,7 @@ Class QueueComponent extends Object
             'conditions' => array('QueueList.status' => 1, 'QueueDetail.queue_id' => $queueID ),                
           )
         );
-         
+           $this->Queue->lastQuery();   
         return $queueDetail;
         
         
