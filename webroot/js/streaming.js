482e37c52d15575bd846d8b1eb30ee2622dcffd0
kushal <kushal.pogul@infobeans.com>
2013-10-30 12:13:31 +0530

    queue validation

--- webroot/js/streaming.js
+++ webroot/js/streaming.js
@@ -475,13 +475,13 @@ function flashConsole(msg) {
 	console.log(msg);
 }
 
-    function secondsToHms(d) {
-	d = Number(d);
-	var h = Math.floor(d / 3600);
-	var m = Math.floor(d % 3600 / 60);
-	var s = Math.floor(d % 3600 % 60);
-	return ((h > 0 ? h + ":" : "") + (m > 0 ? (h > 0 && m < 10 ? "0" : "") + m + ":" : "0:") + (s < 10 ? "0" : "") + s); 
-    }
+function secondsToHms(d) {
+    d = Number(d);
+    var h = Math.floor(d / 3600);
+    var m = Math.floor(d % 3600 / 60);
+    var s = Math.floor(d % 3600 % 60);
+    return ((h > 0 ? h + ":" : "") + (m > 0 ? (h > 0 && m < 10 ? "0" : "") + m + ":" : "0:") + (s < 10 ? "0" : "") + s); 
+}
 
 
 $(document).ready(function(){
