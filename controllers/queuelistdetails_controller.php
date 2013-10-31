71d7ae24c259ff40552f73f0d6d2886f85466441
Narendra <nagesh4group@gmail.com>
2013-10-31 17:15:54 +0530

    debugging

--- controllers/queuelistdetails_controller.php
+++ controllers/queuelistdetails_controller.php
@@ -148,7 +148,7 @@ class QueueListDetailsController extends AppController{
     
     function queue_details()
     {
-        Configure::write('debug', 0);
+        Configure::write('debug', 2);
         $this->layout = 'home';   
         $libId = $this->Session->read('library');
         $patId = $this->Session->read('patron');
