6017e6cb5d964efcf5bc3cbb277ede5b5d85d738
daviddickson <david@m68interactive.com>
2013-11-06 11:26:51 -0500

    debug

--- webroot/js/ajaxify-html5.js
+++ webroot/js/ajaxify-html5.js
@@ -50,7 +50,7 @@
                     isInternalLink;
 
             // Check link
-            isInternalLink = url.substring(0, rootUrl.length) === rootUrl || url.indexOf(':') === -1;
+            isInternalLink = url.substring(0, rootUrl.length) === rootUrl || url.indexOf(':') === -1 || url.indexOf('#') === 0;
 
             // Ignore or Keep
             return isInternalLink;
