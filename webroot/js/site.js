63849f1e48f48de40ca967eebac150d48377cc30
daviddickson <david@m68interactive.com>
2013-11-06 10:21:48 -0500

    git pull

--- webroot/js/site.js
+++ webroot/js/site.js
@@ -1713,7 +1713,7 @@ $('document').ready(function()
     
     $('#content').on('click','.hp-tabs',function(){
 	    
-	   console.log('clicked'); 
+	   console.log('tab clicked'); 
 	    
     });
 
