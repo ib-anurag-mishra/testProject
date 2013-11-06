be98166ec45f2eab1ee9f024aaaecc4533ebbdc5
alokbhatt <alok.infouser@gmail.com>
2013-11-06 17:04:59 +0530

    testing for report1.php

--- webroot/cron/report1.php
+++ webroot/cron/report1.php
@@ -35,8 +35,8 @@ foreach ( $period as $dt )
 {
 echo $currentDate = $dt->format( "Y-m-d" );
 echo "\n";*/
-//$currentDate = '2013-10-14';
-$currentDate = date( "Y-m-d", time());
+$currentDate = '2013-11-04';
+//$currentDate = date( "Y-m-d", time());
 echo "\n----------- Start ".$currentDate." -----------";
 
 list($year, $month, $day) = explode('-', $currentDate);
