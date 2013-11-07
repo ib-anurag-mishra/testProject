d6544ea91d91fd3b04cd7317ef849c2f40c48f52
Narendra <nagesh4group@gmail.com>
2013-11-07 19:37:36 +0530

    debugging

--- controllers/search_controller.php
+++ controllers/search_controller.php
@@ -170,7 +170,7 @@ class SearchController extends AppController {
             }
 
             $this->set('songs', $songs);
-            print_r($songs);
+            //print_r($songs);
             // Added code for all functionality
 
            // print_r($songs);
