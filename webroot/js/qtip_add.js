c465944b4d47539eba15d56e1c052e96169829e6
daviddickson <david@m68interactive.com>
2013-11-08 10:31:38 -0500

    debug

--- webroot/js/qtip_add.js
+++ webroot/js/qtip_add.js
@@ -98,7 +98,38 @@ $(document).ready(function()
 		}
 	});
 */
+
+
+	
+   $('span[title]').each(function(){
+	   $(this).qtip({
+	      position: {
+	         corner: {
+	            target: 'topRight',
+	            tooltip: 'bottomRight'
+	         }
+	      },
+	      style: {
+	         name:'cream',
+	         padding: '10px 0px',
+	         margin: 0,
+	         width: {
+	            max: 210,
+	            min: 0
+	         },
+	         border: {
+	               width: 7,
+	               radius: 5,
+	               color: '#FAF7AA'
+	         },
+	         tip: true
+	      }
+	   });	   
+	   
+	   
+   });
    
+/*   
    $('span[title]').qtip({
       position: {
          corner: {
@@ -121,7 +152,38 @@ $(document).ready(function()
          },
          tip: true
       }
-   });   
+   });
+*/
+
+$("#qtip[title]").each(function(){
+	
+   $(this).qtip({
+      position: {
+         corner: {
+	    target: 'leftBottom',
+            tooltip: 'rightTop'
+         }
+      },
+      style: {
+	 name:'cream',
+         padding: '2px 5px',
+         width: {
+            max: 500,
+            min: 0
+         },
+         border: {
+               width: 8,
+               radius: 1,
+               color: '#FAF7AA'
+         },
+         tip: true
+      }
+   });	
+	
+});
+
+/*
+      
    $("#qtip[title]").qtip({
       position: {
          corner: {
@@ -144,6 +206,37 @@ $(document).ready(function()
          tip: true
       }
    });
+*/  
+
+
+$('span .dload').each(function(){
+   $(this).qtip({
+      position: {
+         corner: {
+            target: 'topRight',
+            tooltip: 'bottomLeft'
+         }
+      },
+      style: {
+	 name:'cream',
+         padding: '5px 10px',
+         width: {
+            max: 350,
+            min: 0
+         },
+         border: {
+               width: 1,
+               radius: 8,
+               color: '#FAF7AA'
+         },
+         tip: true
+      }
+   }); 	
+	
+});
+
+/* 
+   
    $('span .dload').qtip({
       position: {
          corner: {
@@ -165,6 +258,8 @@ $(document).ready(function()
          },
          tip: true
       }
-   });   
+   }); 
+   
+*/  
  
 });
