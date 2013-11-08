eec9e3f3c3ce47673b7c258811a0ea1934adf7a1
daviddickson <david@m68interactive.com>
2013-11-08 10:21:58 -0500

    debug

--- webroot/js/qtip_add.js
+++ webroot/js/qtip_add.js
@@ -28,7 +28,43 @@ $(document).ready(function()
    });
    */
    
-
+   $(document).on('mouseover','a[title]',function(){
+		$(this).qtip({
+			position: {
+				corner: {
+					target: 'topLeft',
+					tooltip: 'bottomRight'
+				}
+			},
+			style: { 
+					
+					color:'#444',
+					fontSize:12,
+					border: {
+		
+						color: '#444'
+					},
+					
+					width: {
+						max:350,
+						min:0
+					},
+			
+					tip:{
+						corner:'bottomRight',
+						size: {
+							x:5,
+							y:5
+						}
+					}
+					
+			
+			}
+		});
+	   
+	   
+   });
+   /*
 	$('a[title]').qtip({
 		position: {
 			corner: {
@@ -61,7 +97,7 @@ $(document).ready(function()
 		
 		}
 	});
-
+	*/
    
    $('span[title]').qtip({
       position: {
