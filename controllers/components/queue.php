5f0ac2cc5c6126090c54a759d8124ad5e4e1cc71
Narendra <nagesh4group@gmail.com>
2013-11-08 19:07:35 +0530

    updated the pages

--- controllers/components/queue.php
+++ controllers/components/queue.php
@@ -44,6 +44,7 @@ Class QueueComponent extends Object
         $queueDetail = $queueDetailList->find('all',
           array(
             'fields' =>  array('QueueDetail.id', 'QueueList.queue_name', 'QueueList.description', 'Songs.SongTitle','Songs.ReferenceID','Songs.Advisory', 'Songs.FullLength_Duration', 'Songs.ProdID', 'Songs.provider_type', 'Songs.Title as STitle', 'Songs.ArtistText',  'Songs.Artist', 'Albums.AlbumTitle','Albums.ProdID','Albums.provider_type', 'Albums.Title as ATitle', 'Product.pid as AlbumProdID', 'AlbumFile.CdnPath as ACdnPath', 'AlbumFile.SourceURL as ASourceURL', 'SongFile.CdnPath as SCdnPath', 'SongFile.SaveAsName as SSaveAsName','Countries.StreamingStatus','Countries.StreamingSalesDate','Countries.DownloadStatus','Countries.SalesDate'),
+            'group' => array('Songs.ProdID','Songs.provider_type'),
             'joins' => array(
               array(
                 'type' => 'INNER',
