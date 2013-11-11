245edcab8edcb060a8bd007e3c71e791cd665ded
Taranjeet Singh <taran2010jeet@gmail.com>
2013-11-11 15:49:15 +0530

    updated

--- webroot/js/ajaxify-html5.js
+++ webroot/js/ajaxify-html5.js
@@ -76,7 +76,7 @@
         $.fn.ajaxify = function() {
             // Prepare
             var $this = $(this);
-
+			var isForm = false;
             // Ajaxify
             $this.find('a:internal:not(.no-ajaxy)').click(function(event) {
             	
@@ -103,12 +103,13 @@
             });
 			
 			// Ajaxify
-            /*$this.find('form input[type=submit]').click(function(event){
+            $this.find('form input[type=submit]').click(function(event){
 							
 				// Prepare
 				var  $this = $(this);
 				var data = $this.parents('form').serialize();
 				var method = $this.parents('form').attr('method');
+				var isForm = true;
 				if(method.toLowerCase() == 'post'){
 					var url = $this.parents('form').attr('action');
 				} else {
@@ -123,7 +124,7 @@
 				History.pushState(null,title,url);
 				event.preventDefault();
 				return false;
-			});*/
+			});
 
             // Chain
             return $this;
@@ -153,11 +154,14 @@
             // Animating to opacity to 0 still keeps the element's height intact
             // Which prevents that annoying pop bang issue when loading in new content
             $content.animate({opacity: 0}, 800);
-
-            // Ajax Request the Traditional Page
-            $.ajax({
+			
+			
+			if(isForm == true && method == 'post') {
+				// Ajax Request the Traditional Page
+				$.ajax({
                 url: url,
-				//data:data,
+				data:data,
+				method:'post',
                 success: function(data, textStatus, jqXHR) {
 
 
@@ -267,6 +271,120 @@
                     return false;
                 },
             }); // end ajax
+			} else {
+				// Ajax Request the Traditional Page
+				$.ajax({
+                url: url,
+				success: function(data, textStatus, jqXHR) {
+
+
+                    // Prepare
+                    var
+                            $data = $(documentHtml(data)),
+                            $dataBody = $data.find('.document-body:first'),
+                            $dataContent = $dataBody.find(contentSelector).filter(':first'),
+                            $menuChildren, contentHtml, $scripts;
+
+                    // Fetch the scripts
+                    $scripts = $dataContent.find('.document-script');
+                    if ($scripts.length) {
+                        $scripts.detach();
+                    }
+
+                    // Fetch the content
+                    contentHtml = $dataContent.html() || $data.html();
+                    if (!contentHtml) {
+                        document.location.href = url;
+                        return false;
+                    }
+
+                    // Update the menu
+                    /*
+                    $menuChildren = $menu.find(menuChildrenSelector);
+                    $menuChildren.filter(activeSelector).removeClass(activeClass);
+                    $menuChildren = $menuChildren.has('a[href^="' + relativeUrl + '"],a[href^="/' + relativeUrl + '"],a[href^="' + url + '"]');
+                    if ($menuChildren.length === 1) {
+                        $menuChildren.addClass(activeClass);
+                    }
+                    */
+
+                    // Update the content
+                    $content.stop(true, true);
+                    $content.html(contentHtml).ajaxify().css('opacity', 100).show(); /* you could fade in here if you'd like */
+
+                    // Update the title
+                    document.title = $data.find('.document-title:first').text();
+                    try {
+                        document.getElementsByTagName('title')[0].innerHTML = document.title.replace('<', '&lt;').replace('>', '&gt;').replace(' & ', ' &amp; ');
+                    }
+                    catch (Exception) {
+                    }
+
+                    // Add the scripts
+                    if ($scripts.length > 1) {
+                        $scripts.each(function() {
+                            var $script = $(this), scriptText = $script.text(), scriptNode = document.createElement('script');
+                            if ($script.attr('src')) {
+                                if (!$script[0].async) {
+                                    scriptNode.async = false;
+                                }
+                                scriptNode.src = $script.attr('src');
+                            }
+                            scriptNode.appendChild(document.createTextNode(scriptText));
+                            contentNode.appendChild(scriptNode);
+                        });
+                    }
+
+                    // Complete the change
+                    if ($body.ScrollTo || false) {
+                        $body.ScrollTo(scrollOptions);
+                    } /* http://balupton.com/projects/jquery-scrollto */
+
+                    $window.trigger(completedEventName);
+                    
+                    // Inform Google Analytics of the change
+                    if (typeof window._gaq !== 'undefined') {
+                        window._gaq.push(['_trackPageview', relativeUrl]);
+                    }
+
+                    // Inform ReInvigorate of a state change
+                    if (typeof window.reinvigorate !== 'undefined' && typeof window.reinvigorate.ajax_track !== 'undefined') {
+                        reinvigorate.ajax_track(url);
+                        // ^ we use the full url here as that is what reinvigorate supports
+                    }
+                    
+                    var delay = 2; // 5 second delay
+                    var now = new Date();
+                    var desiredTime = new Date().setSeconds(now.getSeconds() + delay);
+                    
+                    while (now < desiredTime) {
+                        now = new Date(); // update the current time
+                    }
+
+                      
+                    //$body.removeClass('loader');
+                    $.getScript(webroot + 'css/styles.css');
+                    $.getScript(webroot + 'css/freegal_styles.css');
+                    
+                    $.getScript(webroot + 'js/freegal.js');
+                    $.getScript(webroot + 'js/site.js');
+                    
+                    $.getScript(webroot + 'js/audioPlayer.js');
+                    $.getScript(webroot + 'js/recent-downloads.js');
+                    $.getScript(webroot + 'js/search-results.js');
+                    
+    
+                    $('.loader').fadeOut(500);
+                    
+                    $('.content').remove('.loader');
+
+                },
+                error: function(jqXHR, textStatus, errorThrown) {
+                    document.location.href = url;
+                    return false;
+                },
+            }); // end ajax
+			}
 
         }); // end onStateChange
 
