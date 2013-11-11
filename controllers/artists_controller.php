fba6f8e169d51b127ffab3284d7a075de5650b69
mohan <mohanrocku@gmail.com>
2013-11-11 18:32:34 +0530

    updated for stream url for albums

--- controllers/artists_controller.php
+++ controllers/artists_controller.php
@@ -1217,8 +1217,8 @@ Class ArtistsController extends AppController
                                 {
                                    $songPath = explode(':',$filePath);
                                    $streamUrl =  trim($songPath[1]);
-                                   $albumSongs[$key]['streamUrl'] = $streamUrl;
-                                   $albumSongs[$key]['totalseconds']  = $this->Streaming->getSeconds($value['Song']['FullLength_Duration']); 
+                                   $albumSongs[$k]['streamUrl'] = $streamUrl;
+                                   $albumSongs[$k]['totalseconds']  = $this->Streaming->getSeconds($value['Song']['FullLength_Duration']); 
                                 } 
                            }                                        
 			}
