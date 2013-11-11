c6a0d80836b06d78a2a2102fc6b300eded901e2f
Narendra <nagesh4group@gmail.com>
2013-11-11 11:21:09 +0530

    debugging

--- controllers/queuelistdetails_controller.php
+++ controllers/queuelistdetails_controller.php
@@ -163,12 +163,12 @@ class QueueListDetailsController extends AppController{
         if($this->params['pass'][1]=='1')   //  Default Queue
         {        
           
-       // if ($queue_list_array = Cache::read("defaultqueuelistdetails" . $this->params['pass'][0]) === false) {
+        if ($queue_list_array = Cache::read("defaultqueuelistdetails" . $this->params['pass'][0]) === false) {
                 $queue_list_array   =   $this->Queue->getQueueDetails($this->params['pass'][0],$territory);
                  if (!empty($queue_list_array)) {                   
                         Cache::write("defaultqueuelistdetails" . $this->params['pass'][0], $queue_list_array);
                     }
-        // }  
+         }  
 
             $queue_list_array = Cache::read("defaultqueuelistdetails" . $this->params['pass'][0]);
             // print_r($queue_list_array );die;
