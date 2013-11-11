754a137a884152e5280b2e35b4ef3daf56868686
Narendra <nagesh4group@gmail.com>
2013-11-11 12:51:33 +0530

    new release albums debugging

--- controllers/components/common.php
+++ controllers/components/common.php
@@ -1058,7 +1058,7 @@ STR;
         if ( !empty($country ) && ( $territory == "US" ) ) {           
             
             
-           echo $sql = "SELECT Song.ProdID,Song.ReferenceID,Song.provider_type
+            $sql = "SELECT Song.ProdID,Song.ReferenceID,Song.provider_type
                 FROM Songs AS Song
                 LEFT JOIN {$countryPrefix}countries AS Country ON (Country.ProdID = Song.ProdID) AND (Song.provider_type = Country.provider_type)
                 WHERE  ( (Song.DownloadStatus = '1')) AND 1 = 1 AND (Country.Territory = '$territory') AND (Country.SalesDate != '') AND (Country.SalesDate <= NOW())                    
