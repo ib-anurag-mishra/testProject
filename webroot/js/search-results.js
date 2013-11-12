1a9d1884f85872e6454a00a44f65e868b8137af8
Taranjeet Singh <taran2010jeet@gmail.com>
2013-11-12 20:54:26 +0530

    updated

--- webroot/js/search-results.js
+++ webroot/js/search-results.js
@@ -1,3 +1,145 @@
+function callSearchPageAjax(){
+    $("#submit").click(function(event){
+       ajaxSearchPage();
+    });
+} 
+
+function ajaxSearchPage(){
+       var contentSelector = '.content,article:first,.article:first,.post:first';
+       var $content = $(contentSelector).filter(':first');
+       var $body = $(document.body);
+       // Ensure Content
+        if ($content.length === 0) {
+            $content = $body;
+        }
+       
+       var q = $('#query').val();
+       var type = $('#search_type').val();
+       
+       var loading_div = "<div class='loader'>";
+            loading_div += "</div>";
+            $('.content').append(loading_div);
+         
+       // Start Fade Out
+       // Animating to opacity to 0 still keeps the element's height intact
+       // Which prevents that annoying pop bang issue when loading in new content
+       $content.animate({opacity: 0}, 800);
+            
+       
+       $.ajax({
+           url:'/search/index',
+           method:'get',
+           data:{'q':q,'type':type},
+           success:function(response){
+               $('.content').html($(response).filter('.content'));
+               // Prepare
+                    var $data = $(documentHtml(response)),
+                            $dataBody = $data.find('.document-body:first'),
+                            $dataContent = $dataBody.find(contentSelector).filter(':first'),
+                            $menuChildren, contentHtml, $scripts;
+
+                    // Fetch the scripts
+                    $scripts = $dataContent.find('.document-script');
+                    if ($scripts.length) {
+                        $scripts.detach();
+                    }
+
+                    // Fetch the content
+                    contentHtml = $dataContent.html() || $data.html();
+                    if (!contentHtml) {
+                        alert('Problem fetching data');
+                        return false;
+                    }
+
+                    // Update the menu
+                    /*
+$menuChildren = $menu.find(menuChildrenSelector);
+$menuChildren.filter(activeSelector).removeClass(activeClass);
+$menuChildren = $menuChildren.has('a[href^="' + relativeUrl + '"],a[href^="/' + relativeUrl + '"],a[href^="' + url + '"]');
+if ($menuChildren.length === 1) {
+$menuChildren.addClass(activeClass);
+}
+*/
+
+                    // Update the content
+                    $content.stop(true, true);
+                    $content.html(contentHtml).ajaxify().css('opacity', 100).show(); /* you could fade in here if you'd like */
+
+                    // Update the title
+                    document.title = $data.find('.document-title:first').text();
+                    try {
+                        document.getElementsByTagName('title')[0].innerHTML = document.title.replace('<', '&lt;').replace('>', '&gt;').replace(' & ', ' &amp; ');
+                    }
+                    catch (Exception) {
+                    }
+
+                    // Add the scripts
+                    if ($scripts.length > 1) {
+                        $scripts.each(function() {
+                            var $script = $(this), scriptText = $script.text(), scriptNode = document.createElement('script');
+                            if ($script.attr('src')) {
+                                if (!$script[0].async) {
+                                    scriptNode.async = false;
+                                }
+                                scriptNode.src = $script.attr('src');
+                            }
+                            scriptNode.appendChild(document.createTextNode(scriptText));
+                            contentNode.appendChild(scriptNode);
+                        });
+                    }
+
+                    // Complete the change
+                    if ($body.ScrollTo || false) {
+                        $body.ScrollTo(scrollOptions);
+                    } /* http://balupton.com/projects/jquery-scrollto */
+
+                    $window.trigger(completedEventName);
+                    
+                    // Inform Google Analytics of the change
+                    if (typeof window._gaq !== 'undefined') {
+                        window._gaq.push(['_trackPageview', relativeUrl]);
+                    }
+
+                    // Inform ReInvigorate of a state change
+                    if (typeof window.reinvigorate !== 'undefined' && typeof window.reinvigorate.ajax_track !== 'undefined') {
+                        reinvigorate.ajax_track(url);
+                        // ^ we use the full url here as that is what reinvigorate supports
+                    }
+                    
+                    var delay = 2; // 5 second delay
+                    var now = new Date();
+                    var desiredTime = new Date().setSeconds(now.getSeconds() + delay);
+                    
+                    while (now < desiredTime) {
+                        now = new Date(); // update the current time
+                    }
+
+                      
+                    //$body.removeClass('loader');
+                    $.getScript(webroot + 'css/styles.css');
+                    $.getScript(webroot + 'css/freegal_styles.css');
+                    
+                    $.getScript(webroot + 'js/freegal.js');
+                    $.getScript(webroot + 'js/site.js');
+                    
+                    $.getScript(webroot + 'js/audioPlayer.js');
+                    $.getScript(webroot + 'js/recent-downloads.js');
+                    $.getScript(webroot + 'js/search-results.js');
+                    
+    
+                    $('.loader').fadeOut(500);
+                    
+                    $('.content').remove('.loader');
+               callSearchPageAjax();
+           },
+           failure:function(){
+               alert('Problem fetching data');
+           }
+       });
+       return false;
+}
+
+
 $(document).ready(function() {
 
     $('#query').keypress(function(event) {
@@ -213,5 +355,7 @@ $(document).ready(function() {
 
 
     });
+    
+    callSearchPageAjax();
 
 });
