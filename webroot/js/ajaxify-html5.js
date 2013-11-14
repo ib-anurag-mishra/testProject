dc0533532101fd73134114da0c03b8673908ebb9
kiranpyati <kiran.pyati@infobeans.com>
2013-11-14 12:50:32 +0530

    [updated]- spinning spoke issue

--- webroot/js/ajaxify-html5.js
+++ webroot/js/ajaxify-html5.js
@@ -118,6 +118,10 @@
                     url = State.url,
                     relativeUrl = url.replace(rootUrl, '');
 
+            // Start Fade Out
+            // Animating to opacity to 0 still keeps the element's height intact
+            // Which prevents that annoying pop bang issue when loading in new content
+            $content.animate({opacity: 0}, 800);
 
             // Set Loading
             var loading_div = "<div class='loader'>";
@@ -228,11 +232,6 @@
                     $.getScript(webroot + 'js/recent-downloads.js');
                     $.getScript(webroot + 'js/search-results.js');
 
-                    // Start Fade Out
-                    // Animating to opacity to 0 still keeps the element's height intact
-                    // Which prevents that annoying pop bang issue when loading in new content
-                    $content.animate({opacity: 0}, 800);
-                    
                     $('.loader').fadeOut(500);
 
                     $('.content').remove('.loader');
