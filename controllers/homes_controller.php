ccefd1bb50b6c0ecdedf5e7614c7660567e82df8
Narendra <nagesh4group@gmail.com>
2013-11-15 12:49:31 +0530

    debugging

--- controllers/homes_controller.php
+++ controllers/homes_controller.php
@@ -99,7 +99,7 @@ class HomesController extends AppController
             $this->set('libraryDownload',$libraryDownload);
             $this->set('patronDownload',$patronDownload);
         }
-
+/*
 
         // National Top 100 Songs slider and Downloads functionality
         if (($national = Cache::read("national".$territory)) === false) {
@@ -134,7 +134,7 @@ class HomesController extends AppController
             $featured = Cache::read("featured".$country);
         }
         $this->set('featuredArtists', $featured);
-        
+        */
         /*
             Code OF NEWS Section --- START
         */
