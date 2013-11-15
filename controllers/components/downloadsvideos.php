3078ce6132499b9f669b77b975493d21e778e30f
Narendra <nagesh4group@gmail.com>
2013-11-15 21:46:47 +0530

    debugging

--- controllers/components/downloadsvideos.php
+++ controllers/components/downloadsvideos.php
@@ -117,7 +117,7 @@ Class DownloadsvideosComponent extends Object
         }
         $countryInstance->tablePrefix = strtolower($territory)."_";
         $country = $countryInstance->find('first', array('conditions' => array('ProdID'=>$prodId, 'provider_type'=>$providerType,'Territory'=>$territory, 'SalesDate <= NOW()')));
-       
+       print_r($country);die;
        
         if(!empty($country['Country'])){
             return true;
