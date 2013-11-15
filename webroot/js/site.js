a0657903210cbdb9e2910fa05191e03a89dc48f1
kiranpyati <kiran.pyati@infobeans.com>
2013-11-15 16:40:05 +0530

    [updated] artist view page for album-preview css

--- webroot/js/site.js
+++ webroot/js/site.js
@@ -218,7 +218,16 @@ $('document').ready(function()
     });
     
 
-
+    //album-page js
+    $('.albums-page .album-detail-container').on('mouseenter' , '.album-detail' , function(){
+        $('.album-preview').css({opacity: 1});
+    });
+    
+    $('.albums-page .album-detail-container').on('mouseleave' , '.album-detail', function(){
+        $('.album-preview').css({opacity: 0});
+    });
+    
+    
 
     $('.add-to-queue').on('mouseenter', function() {
 
