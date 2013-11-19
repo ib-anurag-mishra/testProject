9babd267ed3bef8e12964074171022e2647c2919
Narendra <nagesh4group@gmail.com>
2013-11-19 14:11:30 +0530

    debugging

--- controllers/components/downloads.php
+++ controllers/components/downloads.php
@@ -125,8 +125,8 @@ Class DownloadsComponent extends Object
             $territory = $mobileTerritory;
         }
         $countryInstance->tablePrefix = strtolower($territory)."_";
-        $country = $countryInstance->find('first', array('conditions' => array('ProdID'=>$prodId, 'provider_type'=>$providerType,'Territory'=>$territory,'DownloadStatus'=>'1', 'SalesDate <= NOW()')));
-        print_r($country);die;
+        $country = $countryInstance->find('first',array('fields'=>'ProdID','Territory','SalesDate','provider_type'),array('conditions' => array('ProdID'=>$prodId, 'provider_type'=>$providerType,'Territory'=>$territory,'DownloadStatus'=>'1', 'SalesDate <= NOW()')));
+
         if(!empty($country['Country'])){            
             return true;
         } else {            
