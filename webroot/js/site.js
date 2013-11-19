a86fd1a9a0fecbd45cc8f435ec09c4acc7a9b387
daviddickson <david@m68interactive.com>
2013-11-19 15:46:14 -0500

    debug

--- webroot/js/site.js
+++ webroot/js/site.js
@@ -344,8 +344,8 @@ $('document').ready(function()
     
    // $(document).ready($('.preview').css({opacity: 0}));
     
-    $(document).on('mouseleave', '.wishlist-popover', function() {
-    	
+    $(document).on('mouseleave', '.wishlist-popover', function(e) {
+    	console.log(e.offsetX);
     	$(this).removeClass('active');
     	
         
