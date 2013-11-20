d6d0aa979448b0dce60fc0fe42d5f5f3c0ece7ad
kushal <kushal.pogul@infobeans.com>
2013-11-20 18:54:55 +0530

    album details stream

--- controllers/artists_controller.php
+++ controllers/artists_controller.php
@@ -968,6 +968,18 @@ Class ArtistsController extends AppController
 					} else{
 						$albumSongs[$k][$key]['Song']['status'] = 'not';
 					}
+                                        
+                                        if($this->Session->read('library_type')==2)
+                                        {
+                                            $filePath = shell_exec('perl files/tokengen_streaming '. $value['Full_Files']['CdnPath']."/".$value['Full_Files']['SaveAsName']);
+                                            if(!empty($filePath))
+                                             {
+                                                $songPath = explode(':',$filePath);
+                                                $streamUrl =  trim($songPath[1]);
+                                                $albumSongs[$k][$key]['streamUrl'] = $streamUrl;
+                                                $albumSongs[$k][$key]['totalseconds']  = $this->Streaming->getSeconds($value['Song']['FullLength_Duration']); 
+                                             } 
+                                        } 
 			}
 		}
 	    $this->set('albumData', $albumData);
