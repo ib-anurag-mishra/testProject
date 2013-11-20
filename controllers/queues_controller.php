e43566a74cb6a7b0ec6a6fa5b099c990fb204095
kiranpyati <kiran.pyati@infobeans.com>
2013-11-20 18:04:41 +0530

    create queue issue message

--- controllers/queues_controller.php
+++ controllers/queues_controller.php
@@ -73,14 +73,14 @@ class QueuesController extends AppController{
                                 $this->Session ->setFlash('Queue has been Added successfully', 'modal', array( 'class' => 'queue success' ));
                                 $this->layout = 'ajax';
                                 echo "Queue has been Added successfully";
-                                $this->redirect($this->referer());						
+                                //$this->redirect($this->referer());						
                         }
                         else
                         {
                                 $this->Session ->setFlash('Error occured while adding queue', 'modal', array( 'class' => 'queue problem' ));
                                 $this->layout = 'ajax';
                                 echo "Error occured while adding queue";
-                                $this->redirect($this->referer());					
+                                //$this->redirect($this->referer());					
                         }
                         $this->QueueList->setDataSource('default');
                 }           
