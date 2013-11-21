ebc61c3177bfa5bedf8f21cc7a32152628f9dbc3
Narendra Nagesh <nagesh4group@gmail.com>
2013-11-21 13:29:59 +0530

    debugging

--- controllers/artists_controller.php
+++ controllers/artists_controller.php
@@ -1656,7 +1656,7 @@ Class ArtistsController extends AppController
                                             ), "1 = 1 GROUP BY Album.ProdID, Album.provider_type"
                                     ),
                                     'fields' => array(
-                                            'Album.ProdID',
+                                            'Album.ProdID1',
                                             'Album.Title',
                                             'Album.ArtistText',
                                             'Album.AlbumTitle',
