bcd421e0946d33e7935a63634964e3811a7d5db7
kushal <kushal.pogul@infobeans.com>
2013-11-21 15:33:31 +0530

    js data type

--- webroot/js/streaming.js
+++ webroot/js/streaming.js
@@ -485,7 +485,7 @@ function pingTimeJS() {
 	
 }
 
-function streamingValidationJS(responseDataJS) { alert("hi");
+function streamingValidationJS(responseDataJS) { alert("hi"); alert(typeof(responseDataJS));
 	
 	responseDataJS[5] = 	responseDataJS[5]*1000;
 	console.log('inside streamingValidationJS'+responseDataJS);
