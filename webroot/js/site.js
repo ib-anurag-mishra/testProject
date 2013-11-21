c35188a354fd4e27a5c52ad364f004376298929d
daviddickson <david@m68interactive.com>
2013-11-21 14:59:46 -0500

    js edit

--- webroot/js/site.js
+++ webroot/js/site.js
@@ -1596,9 +1596,9 @@ $('document').ready(function()
     });
 
     $('.now-streaming-page .playlist-scrollable,.queue-detail-page .playlist-scrollable').bind('mousewheel', function(e) {
-        $(this).scrollTop($(this).scrollTop() - e.originalEvent.wheelDeltaY);
+        //$(this).scrollTop($(this).scrollTop() - e.originalEvent.wheelDeltaY);
         //prevent page fom scrolling
-        return false;
+        //return false;
     });
 
     $(document).on('click', '.now-streaming-page .now-playing-container .add-to-wishlist-button,.queue-detail-page .now-playing-container .add-to-wishlist-button', function(e) {
