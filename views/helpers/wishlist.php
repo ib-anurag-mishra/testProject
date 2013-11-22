d4d30f610ec871228b319c4df9e273a14f7f8d0e
kiranpyati <kiran.pyati@infobeans.com>
2013-11-22 12:33:21 +0530

    wishlist issue

--- views/helpers/wishlist.php
+++ views/helpers/wishlist.php
@@ -9,10 +9,12 @@ class WishlistHelper extends AppHelper {
     var $uses = array('Wishlist');
     
     function getWishlistData($id) {
+        Configure::write('debug', 2);
         $wishlistInstance = ClassRegistry::init('Wishlist');
         $libraryId = $this->Session->read('library');
         $patronId = $this->Session->read('patron');  
         $wishlistDetails = $wishlistInstance->find('all', array('conditions' => array('library_id' => $libraryId,'patron_id' => $patronId, 'ProdID' => $id)));
+        
         if(count($wishlistDetails) != 0) {
             return "Added to Wishlist";
         }
