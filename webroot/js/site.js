ffe9496faafc117117651f09dbef0e0211f58598
daviddickson <david@m68interactive.com>
2013-11-26 16:22:58 -0500

    debug

--- webroot/js/site.js
+++ webroot/js/site.js
@@ -1,18 +1,4 @@
-$('#top-100-albums-grid .lazy').lazyload({
-    effect: 'fadeIn',
-    container: $('#top-100-albums-grid')
-});
 
-$('#top-100-songs-grid .lazy').lazyload({
-    effect: 'fadeIn',
-    container: $('#top-100-songs-grid'),
-    skip_invisible: false
-});
-
-$('#top-100-videos-grid .lazy').lazyload({
-    effect: 'fadeIn',
-    container: $('#top-100-videos-grid')
-});
 
 $('.most-downloaded-videos-grid .lazy').lazyload({
     effect: 'fadeIn',
@@ -149,7 +135,21 @@ $('document').ready(function()
      });
      */
 
+$('#top-100-albums-grid .lazy').lazyload({
+    effect: 'fadeIn',
+    container: $('#top-100-albums-grid')
+});
 
+$('#top-100-songs-grid .lazy').lazyload({
+    effect: 'fadeIn',
+    container: $('#top-100-songs-grid'),
+    skip_invisible: false
+});
+
+$('#top-100-videos-grid .lazy').lazyload({
+    effect: 'fadeIn',
+    container: $('#top-100-videos-grid')
+});
 
 
     $(document).on('click', '.announcements h4 a', function(e) {
