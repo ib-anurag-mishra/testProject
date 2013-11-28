7c7277934f381aad127b7de77c14cfa50af359ff
kushal <kushal.pogul@infobeans.com>
2013-11-28 12:02:19 +0530

    js window close

--- webroot/js/streaming.js
+++ webroot/js/streaming.js
@@ -580,6 +580,7 @@ function reportWindowClosedSongInfo (wcsobj) {
        
        if(!(songId==null || songId==undefined || songId=='' || songProviderType==null || songProviderType==undefined || songProviderType=='' || plaulistId==null || plaulistId==undefined || plaulistId=='' || songLength==null || songLength==undefined || songLength=='' || songDuration==null || songDuration==undefined || songDuration=='' || songToken==null || songToken==undefined || songToken==''))
        {
+           alert("HI");
            streamingResponse = callStreamingComponent(songId,songProviderType,plaulistId,23,songLength,songDuration,songToken);
        }
        
