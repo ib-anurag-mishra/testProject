debc78e767c505c9329b8b5d68647c330da793c9
kushal <kushal.pogul@infobeans.com>
2013-12-03 21:03:08 +0530

    queue name

--- controllers/components/queue.php
+++ controllers/components/queue.php
@@ -107,7 +107,15 @@ Class QueueComponent extends Object
                 ),
             ),
             'recursive' => -1,
-            
+            'conditions' => array('and' =>
+                array(
+                    array('QueueList.status' => 1),
+                    array('QueueDetail.queue_id' => $queueID)
+                ),
+                'or' => array(array('and' => array('Countries.StreamingStatus' => 1, 'Countries.StreamingSalesDate <=' => date('Y-m-d')))
+                    , array('and' => array('Countries.DownloadStatus' => 1))
+                )
+            )
                 )
         );
         return $queueDetail;
