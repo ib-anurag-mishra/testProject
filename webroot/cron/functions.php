3c333b84141c1122a95f1d85b73dfefb0d83afc9
alokbhatt <alok.infouser@gmail.com>
2013-12-03 16:53:42 +0530

    changes in reports functions.php for file not sending on CDN

--- webroot/cron/functions.php
+++ webroot/cron/functions.php
@@ -255,12 +255,15 @@ function sendFile($src,$dst)
 		else
 		{
 			$sftp = ssh2_sftp($con);
+                        echo $src."\n";
+                        echo $dst."\n";
+                        echo $con;
 			if(!ssh2_scp_send($con, $src, $dst, 0644)){
 				echo "error\n";
 			}
 			else
 			{
-				//echo "FILE Sucessfully sent\n";
+				echo "FILE Sucessfully sent\n";
 			}
 
 		}
