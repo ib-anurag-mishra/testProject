6b57e8460a3860545c8e928629bcc1dfc8d3147c
daviddickson <david@m68interactive.com>
2013-12-03 10:48:33 -0500

    debug

--- webroot/js/site.js
+++ webroot/js/site.js
@@ -292,7 +292,7 @@ $('document').ready(function()
 
 
     $('.playlist-options').bind('mousewheel', function(e) {
-
+		console.log('scrolling');
         $(this).scrollTop($(this).scrollTop() - e.originalEvent.wheelDeltaY);
 
         //prevent page fom scrolling
@@ -301,13 +301,7 @@ $('document').ready(function()
 
     });
     
-    $('.news .top-100 #top-100-songs-grid .song-cover-container .wishlist-popover .playlist-options').bind('mousewheel',function(e) {
-        $(this).scrollTop($(this).scrollTop() - e.originalEvent.wheelDeltaY);
 
-        //prevent page fom scrolling
-        return false;	    
-	    
-    });
 
     
 
