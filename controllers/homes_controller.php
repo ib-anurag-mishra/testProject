a1a91a2fb4688699bfd92d83edc1e7f57da39c36
Narendra Nagesh <nagesh4group@gmail.com>
2013-12-06 20:32:49 +0530

    debugging

--- controllers/homes_controller.php
+++ controllers/homes_controller.php
@@ -195,6 +195,16 @@ class HomesController extends AppController
         * Code For Coming Soon --- END
         */  
         
+        $countryPrefix = $this->Session->read('multiple_countries');
+        $countryTableName = $countryPrefix .'countries';
+        $downloadResults = Array();
+        $downloadResults =  $this->Download->find('all',array('joins'=>array(array('table' => 'Songs','alias' => 'Song','type' => 'LEFT','conditions' => array('Download.ProdID = Song.ProdID','Download.provider_type = Song.provider_type')),array('table' => $countryTableName,'alias' => 'Country','type' => 'INNER','conditions' => array('Country.ProdID = Song.ProdID','Country.provider_type = Song.provider_type')),array('table' => 'Albums','alias' => 'Album','type' => 'LEFT','conditions' => array('Song.ReferenceID = Album.ProdID','Song.provider_type = Album.provider_type')),array('table' => 'File','alias' => 'File','type' => 'LEFT','conditions' => array('Album.FileID = File.FileID')),array('table' => 'File','alias' => 'Full_Files','type' => 'LEFT','conditions' => array('Song.FullLength_FileID = Full_Files.FileID'))),'group' => 'Download.id','conditions' => array('library_id' => $libId,'patron_id' => $patId,'history < 2','created BETWEEN ? AND ?' => array(Configure::read('App.twoWeekStartDate'), Configure::read('App.twoWeekEndDate'))),'fields'=>array('Download.ProdID','Download.provider_type')));
+        $downloadVariArray = array();
+        foreach($downloadResults as $key => $downloadResult){
+            $downloadVariArray[] = $downloadResult['Download']['ProdID'].'~'.$downloadResult['Download']['provider_type'];
+        }
+        $this->Session->write('downloadVariArray', $downloadVariArray );
+        print_r($this->Session->read('downloadVariArray'));
     }
 
     //this is just for streaming component test
