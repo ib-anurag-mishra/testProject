1d259f68cf61b5d0b96d596bcad52fe94789b494
kiranpyati <kiran.pyati@infobeans.com>
2013-12-10 17:33:55 +0530

    code update

--- controllers/videos_controller.php
+++ controllers/videos_controller.php
@@ -25,8 +25,6 @@ class VideosController extends AppController
 
     function index()
     {
-        //Configure::write('debug', 2);
-
         $this->layout = 'home';
 
         $libId = $this->Session->read('library');
@@ -55,19 +53,19 @@ class VideosController extends AppController
         }
 
         // Cache::delete("featured_videos".$territory);
-        if (Cache::read("featured_videos" . $territory) === false) 
+        if (($featuredVideos = Cache::read("featured_videos" . $territory)) === false)
         {
             $featuredVideosSql = "SELECT `FeaturedVideo`.`id`,`FeaturedVideo`.`ProdID`,`Video`.`ProdID`,`Video`.`Image_FileID`, 
                                     `Video`.`VideoTitle`, `Video`.`ArtistText`, `Video`.`provider_type`, Video.Advisory, `File`.`CdnPath`, `File`.`SourceURL`, 
-                                    `File`.`SaveAsName`,`Country`.`SalesDate` FROM featured_videos as FeaturedVideo 
+                                    `File`.`SaveAsName`,`Country`.`SalesDate` 
+                                    FROM featured_videos as FeaturedVideo 
                                     LEFT JOIN video as Video on FeaturedVideo.ProdID = Video.ProdID and FeaturedVideo.provider_type = Video.provider_type 
                                     LEFT JOIN File as File on File.FileID = Video.Image_FileID 
                                     LEFT JOIN {$prefix}countries as Country on (`Video`.`ProdID`=`Country`.`ProdID` AND `Video`.`provider_type`=`Country`.`provider_type`) 
                                     WHERE `FeaturedVideo`.`territory` = '" . $territory . "' AND `Country`.`SalesDate` <= NOW() ";
 
-
             $featuredVideos = $this->Album->query($featuredVideosSql);
-           
+
             if (!empty($featuredVideos))
             {
                 foreach ($featuredVideos as $key => $featureVideo)
@@ -83,21 +81,15 @@ class VideosController extends AppController
         {
             $featuredVideos = Cache::read("featured_videos" . $territory);
         }
-
+        
         $this->set('featuredVideos', $featuredVideos);
-
-
-
+        
+        
+        
         //	Cache::delete("top_download_videos".$territory);
-        if (Cache::read("top_download_videos" . $territory) === false)            
+        if ( ($topDownloads = Cache::read("top_download_videos" . $territory)) === false)
         {
-            $topDownloadSQL = "SELECT Videodownloads.ProdID, Video.ProdID, Video.provider_type, Video.VideoTitle, Video.ArtistText, Video.Advisory, 
-                File.CdnPath, File.SourceURL, COUNT(DISTINCT(Videodownloads.id)) AS COUNT, `Country`.`SalesDate` FROM videodownloads as Videodownloads 
-                    LEFT JOIN video as Video ON (Videodownloads.ProdID = Video.ProdID AND Videodownloads.provider_type = Video.provider_type) 
-                    LEFT JOIN File as File ON (Video.Image_FileID = File.FileID) 
-                    LEFT JOIN {$prefix}countries as Country on (`Video`.`ProdID`=`Country`.`ProdID` AND `Video`.`provider_type`=`Country`.`provider_type`) 
-                    WHERE `Country`.`SalesDate` <= NOW() AND Video.DownloadStatus = '1' GROUP BY Videodownloads.ProdID ORDER BY COUNT DESC limit 100";
-
+            $topDownloadSQL = "SELECT Videodownloads.ProdID, Video.ProdID, Video.provider_type, Video.VideoTitle, Video.ArtistText, Video.Advisory, File.CdnPath, File.SourceURL, COUNT(DISTINCT(Videodownloads.id)) AS COUNT, `Country`.`SalesDate` FROM videodownloads as Videodownloads LEFT JOIN video as Video ON (Videodownloads.ProdID = Video.ProdID AND Videodownloads.provider_type = Video.provider_type) LEFT JOIN File as File ON (Video.Image_FileID = File.FileID) LEFT JOIN {$prefix}countries as Country on (`Video`.`ProdID`=`Country`.`ProdID` AND `Video`.`provider_type`=`Country`.`provider_type`) WHERE `Country`.`SalesDate` <= NOW() AND Video.DownloadStatus = '1' GROUP BY Videodownloads.ProdID ORDER BY COUNT DESC limit 100";
             $topDownloads = $this->Album->query($topDownloadSQL);
             if (!empty($topDownloads))
             {
@@ -113,10 +105,8 @@ class VideosController extends AppController
         }
         else
         {
-            $topDownloads = Cache::read("top_download_videos" . $territory);
+            $topDownloads = Cache::read("top_download_videos" . $territory) ;
         }
-
-        $this->set('topVideoDownloads', $topDownloads);
     }
 
     /**
