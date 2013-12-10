23ed69ac8df5b7b7012cb49f5e689eebee58f57f
kiranpyati <kiran.pyati@infobeans.com>
2013-12-10 17:21:56 +0530

    updated code

--- controllers/videos_controller.php
+++ controllers/videos_controller.php
@@ -55,7 +55,8 @@ class VideosController extends AppController
         }
 
         // Cache::delete("featured_videos".$territory);
-        if (Cache::read("featured_videos" . $territory) === false)       
+        //if (Cache::read("featured_videos" . $territory) === false) 
+                if(1)
         {
             $featuredVideosSql = "SELECT `FeaturedVideo`.`id`,`FeaturedVideo`.`ProdID`,`Video`.`ProdID`,`Video`.`Image_FileID`, 
                                     `Video`.`VideoTitle`, `Video`.`ArtistText`, `Video`.`provider_type`, Video.Advisory, `File`.`CdnPath`, `File`.`SourceURL`, 
@@ -89,7 +90,8 @@ class VideosController extends AppController
 
 
         //	Cache::delete("top_download_videos".$territory);
-        if (Cache::read("top_download_videos" . $territory) === false)               
+        //if (Cache::read("top_download_videos" . $territory) === false)    
+                if(1)
         {
             $topDownloadSQL = "SELECT Videodownloads.ProdID, Video.ProdID, Video.provider_type, Video.VideoTitle, Video.ArtistText, Video.Advisory, 
                 File.CdnPath, File.SourceURL, COUNT(DISTINCT(Videodownloads.id)) AS COUNT, `Country`.`SalesDate` FROM videodownloads as Videodownloads 
