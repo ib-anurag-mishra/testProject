46087d02268ec7838f0fce745e6af953c18020d6
kushal <kushal.pogul@infobeans.com>
2013-12-10 19:35:53 +0530

    debug

--- app_controller.php
+++ app_controller.php
@@ -131,7 +131,7 @@ class AppController extends Controller
                     $territoryPrefixTemp = strtolower($this->Session->read('territory'))."_";
                     $territoryTableName = $territoryPrefixTemp .'countries';
                     $downloadResults = Array();
-
+                    echo "123: ".$territoryTableName;
                     if(!$this->Session->read('downloadVariArray'))
                     {            
                             $downloadResults =  $this->Download->find('all',array('joins'=>array(array('table' => 'Songs','alias' => 'Song','type' => 'LEFT','conditions' => array('Download.ProdID = Song.ProdID','Download.provider_type = Song.provider_type')),array('table' => $territoryTableName,'alias' => 'Country','type' => 'INNER','conditions' => array('Country.ProdID = Song.ProdID','Country.provider_type = Song.provider_type')),array('table' => 'Albums','alias' => 'Album','type' => 'LEFT','conditions' => array('Song.ReferenceID = Album.ProdID','Song.provider_type = Album.provider_type')),array('table' => 'File','alias' => 'File','type' => 'LEFT','conditions' => array('Album.FileID = File.FileID')),array('table' => 'File','alias' => 'Full_Files','type' => 'LEFT','conditions' => array('Song.FullLength_FileID = Full_Files.FileID'))),'group' => 'Download.id','conditions' => array('library_id' => $libId,'patron_id' => $patId,'history < 2','created BETWEEN ? AND ?' => array(Configure::read('App.twoWeekStartDate'), Configure::read('App.twoWeekEndDate'))),'fields'=>array('Download.ProdID','Download.provider_type')));
