3b6041c32413453c81a9360a196ce93aa903578c
kiranpyati <kiran.pyati@infobeans.com>
2013-12-14 16:05:03 +0530

    debugging

--- app_controller.php
+++ app_controller.php
@@ -223,7 +223,7 @@ class AppController extends Controller
        // echo Configure::read('App.curWeekStartDate') ;
         
         //if( date('Y-m-d' , Configure::read('App.curWeekStartDate')) != date('Y-m-d' , $this->Session->read('counterStartDate')))
-        echo date('Y-m-d' , Configure::read('App.curWeekStartDate')) ;
+        echo date('Y-m-d' , strtotime(Configure::read('App.curWeekStartDate')) );
 //                if( date('Y-m-d' , Configure::read('App.curWeekStartDate')) != '2013-12-16')
 //        {
 //            $downloadCount = $this->Common->getDownloadDetails($this->Session->read('library'), $this->Session->read('patron'));
