43ae46c2a60fc9bf1606cbdf44286b872e40d82e
kiranpyati <kiran.pyati@infobeans.com>
2013-12-18 14:59:06 +0530

    announcment update

--- app_controller.php
+++ app_controller.php
@@ -112,7 +112,8 @@ class AppController extends Controller
         
         
         // add announcement in the cache
-        if (($announceInfo = Cache::read("announcementCache")) === false)
+        //if (($announceInfo = Cache::read("announcementCache")) === false)
+                if(1)
         {
             $announcment_query = "SELECT * from pages WHERE announcement = '1' and language='en' ORDER BY modified DESC LIMIT 1";
             $announcment_rs = $this->Album->query($announcment_query);
