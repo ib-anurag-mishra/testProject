89eab89a5f0b04bc5a9e324fafc78fc24084b5d4
kiranpyati <kiran.pyati@infobeans.com>
2014-01-03 14:52:18 +0530

    debugging

--- webroot/js/freegal.js
+++ webroot/js/freegal.js
@@ -2703,7 +2703,7 @@ $(document).ready(function() {
         $('.my-wishlist-shadow-container').show();
     });
 
-    $('.recent-downloads-page .recent-downloads-filter-container').on('click',
+    $('.recent-downloads-page .recent-downloads-filter-container').off('click',
             '.date-filter-button , .song-filter-button , .artist-filter-button , .artist-filter-button , .download-button',
             function() {
                 return false ;
