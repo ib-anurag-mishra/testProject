a9a50892747b64c0c9314cfc6d8ffbb0e4bda37d
Taranjeet Singh <taran2010jeet@gmail.com>
2014-01-03 18:56:38 +0530

    Updated solr component

--- controllers/components/solr.php
+++ controllers/components/solr.php
@@ -558,7 +558,7 @@ class SolrComponent extends Object {
                 ),
                 'facet.query' => $query,
                 'facet.mincount' => 1,
-                'facet.limit' => 15000
+                'facet.limit' => 5000
             );
             echo "<br />";
 echo $query;
