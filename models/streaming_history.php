c554513cbed96bfa295cfc3761da866865f441b7
alokbhatt <alok.infouser@gmail.com>
2014-01-06 19:28:16 +0530

    making changes in streaming report at admin end

--- models/streaming_history.php
+++ models/streaming_history.php
@@ -133,7 +133,7 @@ class StreamingHistory extends AppModel {
                     'conditions' => array('StreamingHistory.ProdID=countries.ProdID')
                 )
              ),
-            'fields' => array('distinct patron_id AS total_patrons'),
+            'fields' => array('distinct StreamingHistory.patron_id AS total_patrons'),
             'conditions'=>array('StreamingHistory.provider_type=countries.provider_type','createdOn BETWEEN "'.$startDate.'" and "'.$endDate.'" ',$lib_condition,'not'=>array('StreamingHistory.token_id'=>null)),
             'recursive' => -1);
             
