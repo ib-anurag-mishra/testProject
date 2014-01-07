9deee3b33fb7b05fc5891303f7d8703ff19664da
alokbhatt <alok.infouser@gmail.com>
2014-01-07 17:42:13 +0530

    making changes in streaming report at admin end

--- models/streaming_history.php
+++ models/streaming_history.php
@@ -339,7 +339,7 @@ class StreamingHistory extends AppModel {
                         'conditions' => array('StreamingHistory.ProdID=countries.ProdID')
                     )
                  ),
-                'conditions'=>array('StreamingHistory.createdOn BETWEEN "'.$startDate.'" and "'.$endDate.'" ',array('StreamingHistory.library_id'=>$lib_condition),'not'=>array('StreamingHistory.token_id'=>null),'StreamingHistory.provider_type=countries.provider_type'), 
+                'conditions'=>array('StreamingHistory.createdn BETWEEN "'.$startDate.'" and "'.$endDate.'" ',array('StreamingHistory.library_id'=>$lib_condition),'not'=>array('StreamingHistory.token_id'=>null),'StreamingHistory.provider_type=countries.provider_type'), 
                 'fields'=>array('songs.Genre','count(StreamingHistory.ProdID) as total_streamed_songs','StreamingHistory.library_id'),
                 'group' => array('songs.Genre'),
                 'recursive' => -1));
