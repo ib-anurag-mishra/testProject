d0ff5f8aa719d27e0da3d69febfb352058d4e413
daviddickson <david@m68interactive.com>
2014-01-07 16:18:16 -0500

    update

--- webroot/demo/javascripts/site.js
+++ webroot/demo/javascripts/site.js
@@ -22,46 +22,40 @@ $(document).ready(function(){
 	});
 	
 	var ulPosition;
+
 	
-	$('.album-scroll-left').on('click',function(){
+	$('.left-scroll-button').on('click',function(){
 		
-		ulPosition = parseInt($(this).parent('.album-navigation-container').siblings('.album-list-container').find('ul').css("margin-left"));
-		
+		ulPosition = parseInt($(this).siblings('.top-albums-carousel').find('ul').css("margin-left"));
 		if(ulPosition !== 0) {
 		
 			ulPosition = ulPosition + 860;
 	
 			
 			
-			$(this).parent('.album-navigation-container').siblings('.album-list-container').find('ul').animate({
+			$(this).siblings('.top-albums-carousel').find('ul').animate({
 				
 				marginLeft:ulPosition
 			});
 		}
+		
 	});
 	
 	
-	$('.album-scroll-right').on('click',function(){
-	
-		ulPosition = parseInt($(this).parent('.album-navigation-container').siblings('.album-list-container').find('ul').css("margin-left"));
+	$('.right-scroll-button').on('click',function(){
 		
-		
-		
-		
-
+		ulPosition = parseInt($(this).siblings('.top-albums-carousel').find('ul').css("margin-left"));
 		if(ulPosition !== -3440) {
 		
 			ulPosition = ulPosition - 860;
 	
-
-
-			$(this).parent('.album-navigation-container').siblings('.album-list-container').find('ul').animate({
+			
+			
+			$(this).siblings('.top-albums-carousel').find('ul').animate({
 				
 				marginLeft:ulPosition
 			});
-
 		}
-		
 		
 	});
 	
