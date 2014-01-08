ea03fb87395da4539e4008dafac01389b466bb9e
alokbhatt <alok.infouser@gmail.com>
2014-01-08 16:11:21 +0530

    creating streaming report csv

--- controllers/reports_controller.php
+++ controllers/reports_controller.php
@@ -1643,7 +1643,11 @@ Class ReportsController extends AppController {
      */
 
     function admin_streamingreport() {
-        print_r($this->param);exit;
+        echo "here";
+        print_r($this);
+        print_r($this->param['pass']);
+        print_r($this->param);
+        exit;
         if ($this->Session->read("Auth.User.type_id") == 4 && $this->Session->read("Auth.User.consortium") == '') {
             $libraryAdminID = $this->Library->find("first", array("conditions" => array('library_admin_id' => $this->Session->read("Auth.User.id")), 'fields' => array('id', 'library_name', 'library_territory'), 'recursive' => -1));
             $this->set('libraryID', $libraryAdminID["Library"]["id"]);
