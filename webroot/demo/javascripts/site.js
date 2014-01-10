0bd84b8033ffc0422da7c80ba2d5dff37dd5c615
daviddickson <david@m68interactive.com>
2014-01-10 15:19:10 -0500

    demo update

--- webroot/demo/javascripts/site.js
+++ webroot/demo/javascripts/site.js
@@ -5,10 +5,52 @@ attributes.id = "fmp_player";
 swfobject.switchOffAutoHideShow();
 swfobject.embedSWF("swf/fmp.swf?"+(Math.random()*1000000), "alt", "960", "100", "9.0.0", false, flashvars, params, attributes);
 
+(function($) {
+
+    $.fn.bindMouseWheel = function() {
+
+        return this.each(function() {
+
+            $(this).bind('mousewheel', function(e) {
+
+                $(this).scrollTop($(this).scrollTop() - e.originalEvent.wheelDeltaY);
+                //prevent page fom scrolling
+                return false;
+
+            });
+
+
+        });
+
+    };
+
+
+}(jQuery));
+
 $(document).ready(function(){
 	
+	$('.album-cover-container ul').bindMouseWheel();
 	
-
+	
+	$('.album-cover-container').on('mouseenter',function(){
+		$(this).find('.playlist-menu-icon').addClass('active');
+		
+	});
+	
+	$('.album-cover-container').on('mouseleave',function(){
+		$(this).find('.playlist-menu-icon').removeClass('active');
+		
+	});
+	
+	$('.playlist-menu-icon').on('click',function(){
+		
+		$(this).siblings('ul').toggleClass('active');
+	});
+	
+	$('.album-cover-container ul').on('mouseleave',function(){
+		$(this).removeClass('active');
+		
+	});
 	
 	$('.my-account-menu').on('click',function(){
 		
@@ -59,7 +101,8 @@ $(document).ready(function(){
 		
 	});
 	
-	$('.freegal-playlist-subcat-toggle').on('click',function(){
+	$('.freegal-playlist-subcat-toggle').on('click',function(e){
+		e.preventDefault();
 		
 		$('.freegal-playlist-subcat').toggleClass('active');
 	});
@@ -68,4 +111,15 @@ $(document).ready(function(){
 		
 		$('.my-playlist-subcat').toggleClass('active');
 	});
+	
+	$('.featured-grid-item').on('mouseenter',function(){
+		$(this).find('.featured-grid-menu').addClass('active');
+		
+	});
+	
+	$('.featured-grid-item').on('mouseleave',function(){
+		$(this).find('.featured-grid-menu').removeClass('active');
+		
+	});
+	
 });
