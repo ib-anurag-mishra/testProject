935f803b4cde8e9d834a816e650d138d713e8ba9
Narendra <nagesh4group@gmail.com>
2014-01-21 15:37:11 +0530

    adding more logs in the report1.php file to investigate the cron job time issue

--- webroot/cron/report1.php
+++ webroot/cron/report1.php
@@ -38,6 +38,7 @@ echo "\n";*/
 //$currentDate = '2013-10-14';
 $currentDate = date( "Y-m-d", time());
 echo "\n----------- Start ".$currentDate." -----------";
+echo "\n----------- Start ".date('Y-m-d H:i:s')." -----------";
 
 list($year, $month, $day) = explode('-', $currentDate);
 $weekFirstDay = date('Y-m-d', strtotime(date('Y' , strtotime($currentDate))."W".date('W' , strtotime($currentDate))."1"));
@@ -73,7 +74,7 @@ if(($currentDate == $weekFirstDay) || ($currentDate == $monthFirstDate))
 
             if($currentDate == $weekFirstDay)
             {
-                echo "\ncurrent date is week first day";
+                echo "\n".date('Y-m-d H:i:s')." current date is week first day";
                 //echo "\n---------------- \n";
                 $StartOfLastWeek = 6 + date("w",strtotime($currentDate));
 
@@ -774,6 +775,7 @@ else
 {
     echo "\nToday is not either the week first day or the month first day so the report didn't get generated.\n";
 }
-
+echo "\n----------- End ".date('Y-m-d H:i:s')." -----------";
 echo "\n----------- End -----------";
+
 ?>
