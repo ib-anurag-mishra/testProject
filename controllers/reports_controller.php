8e5fdb0661e5ae8393c930cfff67907869951f02
narendra nagesh <nagesh4group@gmail.com>
2014-01-21 20:51:03 +0530

    debugging

--- controllers/reports_controller.php
+++ controllers/reports_controller.php
@@ -1468,13 +1468,13 @@ Class ReportsController extends AppController {
         $data = '';
         if ($this->Session->read("Auth.User.type_id") == 4 && $this->Session->read("Auth.User.consortium") == '') {
             $var = $this->Library->find("list", array("conditions" => array('Library.library_admin_id' => $this->Session->read("Auth.User.id"), 'Library.library_territory' => $_REQUEST['Territory']), 'fields' => array('Library.id', 'Library.library_name'), 'order' => 'Library.library_name ASC', 'recursive' => -1));
-            echo 146;
+            
         } elseif ($this->Session->read("Auth.User.type_id") == 4 && $this->Session->read("Auth.User.consortium") != '') {
-            echo 148;
+            
                $libValue = isset($_REQUEST['lib_id'])? $_REQUEST['lib_id']:'';
             $var = $this->Library->find("list", array("conditions" => array('Library.library_apikey' => $this->Session->read("Auth.User.consortium"), 'Library.library_territory' => $_REQUEST['Territory'], 'Library.library_type' => 2), 'fields' => array('Library.id', 'Library.library_name'), 'order' => 'Library.library_name ASC', 'recursive' => -1));
         } else {
-            echo 147;
+            
             $var = $this->Library->find('list', array('conditions' => array('Library.library_territory' => $_REQUEST['Territory'], 'Library.library_type' => 2), 'fields' => array('Library.id', 'Library.library_name'), 'order' => 'Library.library_name ASC', 'recursive' => -1));
             $data = "<option value='all'>All Libraries</option>";
         }
