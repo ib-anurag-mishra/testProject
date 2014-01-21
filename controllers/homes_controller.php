7ccf583d0c5242ac92989e261664ad58aeff2b79
kiranpyati <kiran.pyati@infobeans.com>
2014-01-21 12:35:20 +0530

    error log update for add to wihslist

--- controllers/homes_controller.php
+++ controllers/homes_controller.php
@@ -2540,7 +2540,7 @@ STR;
                 }
                 else
                 {
-                    $log_data .= "  :Mysql Error :" . $this->Wishlist->validationErrors;
+                    $log_data .= "  :Mysql Error :" . serialize($this->Wishlist->validationErrors);
                 }
 
                 $this->Wishlist->setDataSource('default');
