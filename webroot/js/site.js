9c8c7ce3eeea109636020fc6fc187c9de16973df
Taranjeet Singh <taran2010jeet@gmail.com>
2014-02-03 19:02:13 +0530

    Update

--- webroot/js/site.js
+++ webroot/js/site.js
@@ -2280,6 +2280,127 @@ function getQueueList(ProdID, type, addTo) {
     return false;
 }
 
+// code to ajaxify Notification form end
+function ajaxRedirectRecentDownload() {
+//       $('#btnMyAccount').click(function(){
+    var USendNewsLetterCheck = '';
+    var UNewsletterEmail = '';
+//            var UserID='';
+    var contentSelector = '.content,article:first,.article:first,.post:first';
+    var $content = $(contentSelector).filter(':first');
+    var $body = $(document.body);
+    //  Ensure Content
+    if ($content.length === 0) {
+        $content = $body;
+    }
+    /*
+     var q = $('#search-text').val();
+     var type = $('#master-filter').val();
+     */
+    /*if($('#UserId').val()){
+     UserID=$('#UserId').val();
+     }*/
+    var loading_div = "<div class='loader'>";
+    loading_div += "</div>";
+    $('.content').append(loading_div);
+
+    // Start Fade Out
+    // Animating to opacity to 0 still keeps the element's height intact
+    // Which prevents that annoying pop bang issue when loading in new content
+    $content.animate({opacity: 0}, 800);
+
+
+    $.ajax({
+        url: 'homes/my_history',
+        method: 'post',
+        success: function(response) {
+            $('.content').html($(response).filter('.content'));
+            // Prepare
+            var $data = $(documentHtml(response)),
+                    $dataBody = $data.find('.document-body:first'),
+                    $dataContent = $dataBody.find(contentSelector).filter(':first'),
+                    $menuChildren, contentHtml, $scripts;
+
+            // Fetch the scripts
+            $scripts = $dataContent.find('.document-script');
+            if ($scripts.length) {
+                $scripts.detach();
+            }
+
+            // Fetch the content
+            contentHtml = $dataContent.html() || $data.html();
+            if (!contentHtml) {
+                alert('Problem fetching data');
+                return false;
+            }
+
+            // Update the menu
+            /*
+             $menuChildren = $menu.find(menuChildrenSelector);
+             $menuChildren.filter(activeSelector).removeClass(activeClass);
+             $menuChildren = $menuChildren.has('a[href^="' + relativeUrl + '"],a[href^="/' + relativeUrl + '"],a[href^="' + url + '"]');
+             if ($menuChildren.length === 1) {
+             $menuChildren.addClass(activeClass);
+             }
+             */
+
+            // Update the content
+            $content.stop(true, true);
+            $content.html(contentHtml).ajaxify().css('opacity', 100).show(); /* you could fade in here if you'd like */
+
+            // Update the title
+            document.title = $data.find('.document-title:first').text();
+            try {
+                document.getElementsByTagName('title')[0].innerHTML = document.title.replace('<', '&lt;').replace('>', '&gt;').replace(' & ', ' &amp; ');
+            }
+            catch (Exception) {
+            }
+
+            // Add the scripts
+            if ($scripts.length > 1) {
+                $scripts.each(function() {
+                    var $script = $(this), scriptText = $script.text(), scriptNode = document.createElement('script');
+                    if ($script.attr('src')) {
+                        if (!$script[0].async) {
+                            scriptNode.async = false;
+                        }
+                        scriptNode.src = $script.attr('src');
+                    }
+                    scriptNode.appendChild(document.createTextNode(scriptText));
+                    contentNode.appendChild(scriptNode);
+                });
+            }
+
+            // Complete the change
+            if ($body.ScrollTo || false) {
+                $body.ScrollTo(scrollOptions);
+            } /* http://balupton.com/projects/jquery-scrollto */
+
+
+            //$body.removeClass('loader');
+            $.getScript(webroot + 'css/styles.css');
+            $.getScript(webroot + 'css/freegal_styles.css');
+
+            $.getScript(webroot + 'js/freegal.js');
+            $.getScript(webroot + 'js/site.js');
+
+            $.getScript(webroot + 'js/audioPlayer.js');
+            $.getScript(webroot + 'js/recent-downloads.js');
+            $.getScript(webroot + 'js/search-results.js');
+
+
+            $('.loader').fadeOut(500);
+
+            $('.content').remove('.loader');
+        },
+        failure: function() {
+            alert('Problem fetching data');
+        }
+    });
+    return false;
+//    });
+}
+
 $('document').ready(function() {
     $('#search-text').autocomplete("/search/autocomplete",
             {
