288cc99a694ed657f9910d92bd7a58ede2e7c2f9
Taranjeet Singh <taran2010jeet@gmail.com>
2014-02-03 18:01:33 +0530

    Update

--- controllers/search_controller.php
+++ controllers/search_controller.php
@@ -966,8 +966,8 @@ class SearchController extends AppController
             {
 	        $downloadsUsed = $this->LatestDownload->find('all', array('conditions' => array('LatestDownload.ProdID' => $prodId, 'LatestDownload.provider_type' => $providerType, 'library_id' => $libId, 'patron_id' => $patId, 'history < 2', 'created BETWEEN ? AND ?' => array(Configure::read('App.twoWeekStartDate'), Configure::read('App.twoWeekEndDate')))));
                 $set = 0;
-                echo $this->LatestDownload->lastQuery();
-                print_r($downloadsUsed);
+                //echo $this->LatestDownload->lastQuery();
+                //print_r($downloadsUsed);
                 foreach ($downloadsUsed as $downloadKey => $downloadData)
                 {
                     if ($downloadData['LatestDownload']['ProdID'] == $prodId && $downloadData['LatestDownload']['provider_type'] == $providerType)
