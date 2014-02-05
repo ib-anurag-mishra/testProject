d2bb732b8ce06274166b02c1df74cbfc89a79fe7
narendra nagesh <nagesh4group@gmail.com>
2014-02-05 23:43:56 +0530

    debugging

--- controllers/homes_controller.php
+++ controllers/homes_controller.php
@@ -111,13 +111,12 @@ class HomesController extends AppController
                 $cacheFlag = $this->MemDatas->find('count',array('conditions' => array('territory'=>'US','vari_info != '=>'')));
                 if($cacheFlag > 0){        
                     $memDatasArr = $this->MemDatas->find('first',array('conditions' => array('territory'=>'US')));                
-                    $unMemDatasArr = unserialize($memDatasArr['MemDatas']['vari_info']);
+                    $unMemDatasArr = unserialize(stripslashes($memDatasArr['MemDatas']['vari_info']));
                     Cache::write("national" . $territory,$unMemDatasArr);
-                    $nationalTopDownload = $unMemDatasArr;
-                    
+                    $nationalTopDownload = $unMemDatasArr;                    
                 }else{                
                     $nationalTopDownload = $this->Common->getNationalTop100($territory);
-                    $nationalTopDownloadSer = serialize($nationalTopDownload);
+                    $nationalTopDownloadSer = serialize(addslashes($nationalTopDownload));
                     $memQuery = "update mem_datas  set vari_info='".$nationalTopDownloadSer."'  where territory='US'";
                     $this->MemDatas->query($memQuery);
                 } 
