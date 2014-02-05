f77c6189722ccebc6becf9033ada19a3e3d968a4
Ratnesh <ratnesh.gupta@infobeans.com>
2014-02-05 12:07:47 +0530

    anTION TOP 100 SONGS QUERY CHANGE

--- controllers/components/common.php
+++ controllers/components/common.php
@@ -155,7 +155,7 @@ Class CommonComponent extends Object
                         File AS Full_Files ON (Song.FullLength_FileID = Full_Files.FileID)
                                 LEFT JOIN
                         Genre AS Genre ON (Genre.ProdID = Song.ProdID) AND (Song.provider_type = Genre.provider_type) 
-                                LEFT JOIN
+                                INNER JOIN
                         {$countryPrefix}countries AS Country ON (Country.ProdID = Song.ProdID) AND (Country.Territory = '$country') AND Country.DownloadStatus = '1' AND (Song.provider_type = Country.provider_type) AND (Country.SalesDate != '') AND (Country.SalesDate < NOW()) 
                                 LEFT JOIN
                         PRODUCT ON ((PRODUCT.ProdID = Song.ProdID) AND (PRODUCT.provider_type = Song.provider_type))
