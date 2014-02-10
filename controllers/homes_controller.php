7ee9f7e10010d031ff3e4a4fce0ac49a188a2bbf
Narendra <nagesh4group@gmail.com>
2014-02-10 14:53:11 +0530

    updated download patch

--- controllers/homes_controller.php
+++ controllers/homes_controller.php
@@ -4724,7 +4724,7 @@ STR;
                 $sql = "CALL sonyproc_ioda('" . $libId . "','" . $patId . "', '" . $prodId . "', '" . $trackDetails['0']['Song']['ProductID'] . "', '" . $trackDetails['0']['Song']['ISRC'] . "', '" . addslashes($trackDetails['0']['Song']['Artist']) . "', '" . addslashes($trackDetails['0']['Song']['SongTitle']) . "', '" . $insertArr['user_login_type'] . "', '" . $insertArr['provider_type'] . "', '" . $insertArr['email'] . "', '" . addslashes($insertArr['user_agent']) . "', '" . $insertArr['ip'] . "', '" . Configure::read('App.curWeekStartDate') . "', '" . Configure::read('App.curWeekEndDate') . "',@ret)";
             }
             
-            $this->LatestDownload->setDataSource('master');
+            $this->Library->setDataSource('master');
             
             $this->Library->query($sql);
             $sql = "SELECT @ret";
