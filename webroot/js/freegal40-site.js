000c57851b043a9c83b51657b80f7c5b6cce0b9c
daviddickson <david@m68interactive.com>
2014-02-24 12:25:35 -0500

    debug mouse wheel on .ac_results

--- webroot/js/freegal40-site.js
+++ webroot/js/freegal40-site.js
@@ -1,7 +1,7 @@
 
 $(document).ready(function(){
 
-    $('.site-header').on('mouseenter','.ac_results ul',function(){
+    $('.body').on('mouseenter','.ac_results ul',function(){
         $(this).bind('mousewheel', function(e) {
 
             $(this).scrollTop($(this).scrollTop() - e.originalEvent.wheelDeltaY);
