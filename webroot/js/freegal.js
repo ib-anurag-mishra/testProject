c16d13413fcfdae00b895ca7dc65131243f0a5df
Ratnesh <ratnesh.gupta@infobeans.com>
2014-03-03 10:28:21 +0530

    debugging multi song

--- webroot/js/freegal.js
+++ webroot/js/freegal.js
@@ -1772,27 +1772,36 @@ function addToAlbumTest(queueID, addTo)
 
 function addToPlaylistNew(queueID, addTo)
 {
-    var type = $(addTo).parent().parent().parent().find('input[type="hidden"]').attr('value');
-    var ProdID = $(addTo).parent().parent().parent().find('input[type="hidden"]').attr('id');
-    
-    $.ajax({
-        type: "post",
-        data: {'prodID': ProdID, 'type': type, 'QueueID': queueID},
-        url: webroot + 'queues/queueListAlbums',
-        success: function(response)
-        {
-            //alert(response);
-            addToQueueResponse(response, type);
-        },
-        error: function(jqXHR, textStatus, errorThrown) {
-            // log the error to the console
-            console.log(
-                    "The following error occured: " +
-                    textStatus, errorThrown);
-        }
-    });
+    if ($(addTo).parent().parent().parent().parent().hasClass())
+    {
+        alert('yes');
+    }
+    else
+    {
+        var type = $(addTo).parent().parent().parent().find('input[type="hidden"]').attr('value');
+        var ProdID = $(addTo).parent().parent().parent().find('input[type="hidden"]').attr('id');
+
+        $.ajax({
+            type: "post",
+            data: {'prodID': ProdID, 'type': type, 'QueueID': queueID},
+            url: webroot + 'queues/queueListAlbums',
+            success: function(response)
+            {
+                //alert(response);
+                addToQueueResponse(response, type);
+            },
+            error: function(jqXHR, textStatus, errorThrown) {
+                // log the error to the console
+                console.log(
+                        "The following error occured: " +
+                        textStatus, errorThrown);
+            }
+        });
+    }
     return false;
 }
+
+
 function addToQueueResponse(response, type)
 {
     if (response.length === 6) {
