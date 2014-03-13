cd106946a994f9e83cd1c40df9f0eaf3317de98c
mohan <mohanrocku@gmail.com>
2014-03-13 18:09:33 +0530

    updated for adding pagination to composer albums

--- controllers/artists_controller.php
+++ controllers/artists_controller.php
@@ -2633,13 +2633,16 @@ Class ArtistsController extends AppController
      * 
      */
     
-    function composer($composer_text){
+    function composer($composer_text,$facetPage = 1){
         
         $this->layout = 'home';
-        $composer_text = base64_decode($this->params['pass'][0]);    
+        $composer_text = base64_decode($this->params['pass'][0]); 
+        if(!empty($this->params['pass'][1])){
+            $facetPage = $this->params['pass'][1];
+        }
         if(isset($composer_text)){
             $totalFacetCount = $this->Solr->getFacetSearchTotal($composer_text, 'album');
-            //$limit = 12;
+            $limit = 12;
             $albums = $this->Solr->groupSearch($composer_text, 'album', $facetPage, $limit);
             $arr_albumStream = array();
             foreach ($albums as $objKey => $objAlbum)
@@ -2648,9 +2651,18 @@ Class ArtistsController extends AppController
                         array('controller' => 'artists', 'action' => 'getAlbumSongs'), array('pass' => array(base64_encode($objAlbum->ArtistText), $objAlbum->ReferenceID, base64_encode($objAlbum->provider_type), 1))
                 );
             }
+            if (!empty($totalFacetCount))
+            {
+                $this->set('totalFacetPages', ceil($totalFacetCount / $limit));
+            }
+            else
+            {
+                $this->set('totalFacetPages', 0);
+            }            
             $this->set('albumData', $albums);
             $this->set('arr_albumStream', $arr_albumStream);
             $this->set('composertext', $composer_text);
+            $this->set('facetPage',$facetPage);
         }
     }
 
