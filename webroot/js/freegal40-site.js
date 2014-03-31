63630d9a0c5e5c51ff958fe650e47d85b9944be5
daviddickson <david@m68interactive.com>
2014-03-31 13:12:58 -0400

    debug

--- webroot/js/freegal40-site.js
+++ webroot/js/freegal40-site.js
@@ -233,9 +233,9 @@ $(document).ready(function() {
 
     });
 
-    $genre_scroll_down = $('.genre-scroll-down');
+    
 
-    $(document).on('click', $genre_scroll_down, function() {
+    $(document).on('click', '.genre-scroll-down', function() {
 
         var currentScrollTop = $genre_column.scrollTop();
         var genreListHeight = $genre_column_ul.height();
