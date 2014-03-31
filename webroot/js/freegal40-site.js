ea8fcfdb445fd08c057bd716d2ccef7112f5d142
daviddickson <david@m68interactive.com>
2014-03-31 13:11:01 -0400

    jquery optimization

--- webroot/js/freegal40-site.js
+++ webroot/js/freegal40-site.js
@@ -233,9 +233,9 @@ $(document).ready(function() {
 
     });
 
-    $genre_scroll_down = $('.genre-scroll-down')
+    var $genre_scroll_down = $('.genre-scroll-down')
 
-    $(document).on('click', $genre_scroll_downs, function() {
+    $(document).on('click', $genre_scroll_down, function() {
 
         var currentScrollTop = $genre_column.scrollTop();
         var genreListHeight = $genre_column_ul.height();
