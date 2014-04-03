89cb18f133f61b1ce03424d5ce7f64917a391588
Taranjeet Singh <taran2010jeet@gmail.com>
2014-04-03 18:28:35 +0530

    Updated

--- vendors/shells/incremental_index.php
+++ vendors/shells/incremental_index.php
@@ -71,26 +71,43 @@ class IncrementalIndexShell extends Shell {
                 $msg = 'Indexing Failed To Start: Internal Error ('.$response.')';
                 mail($this->emailList, 'Apache Solr Indexer ('.date('Y-m-d h:i').'-'.$logId.') Status', 'Status :- "'.$msg.'"');
             }
-        }
+        } else {
+            
+            $logData .= date('Y-m-d h:i:s').' > Indexing failed to start ( Response: '.$response.' )'.PHP_EOL;
+            $logData .= "\n\n";
+            $logData .= date('Y-m-d h:i:s').' > Indexing failed to start ( Response: '.$response.' )'.PHP_EOL;
+            $logData .= date('Y-m-d h:i:s').' > End Time: '.date('Y-m-d h:i:s').PHP_EOL;
+            $logData .= PHP_EOL."---------Request (".$logId.") End----------------";
+            $msg = 'Indexing Failed To Start: Valid response XML not sent ('.$response.')';
+            mail($emailList, 'Apache Solr Indexer ('.date('Y-m-d h:i').'-'.$logId.') Status', 'Status :- "'.$msg.'"');
+            writeToLog($logData);
+        }//for invalid xml response
         
         $response = $httpSocket->get($this->videosIndexUrl,$this->query);
+        
+        // object type-casted 
+        $checkValidXml = null;
+        $checkValidXml = simplexml_load_string($response);
+        
+        // executes IF for valid xml response
+        if($checkValidXml) {
         
         $response = $httpSocket->get($this->videosIndexUrl,$this->statusQuery);
         
         $status = 1;
         
         while($status) {
-            sleep( $this->sleepTime );
+            // sleep( $this->sleepTime );
             $response = $httpSocket->get($this->songsIndexUrl,$this->statusQuery);
             $status = $this->parseStatusResponse($response);
         }
     }
     
     function parseStatusResponse($response) {
-        $xmlArray = simplexml_load_string($response);
+        $objXmlResponse = simplexml_load_string($response);
         
         // type-casted to array format
-        $arrData = (array)$obj_xml_response;
+        $arrData = (array)$objXmlResponse;
 
         if('busy' == strtolower($arrData['str'][1])) {
             return 1;
