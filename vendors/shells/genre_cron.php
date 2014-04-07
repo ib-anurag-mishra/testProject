f04660ab906f20499f3f3b029bcce1791b3cb221
Narendra <nagesh4group@gmail.com>
2014-04-07 17:43:35 +0530

    update the shell related changes

---
+++ vendors/shells/genre_cron.php
@@ -0,0 +1,18 @@
+<?php
+class GenreCronShell extends Shell {
+/**
+ * this funciton set Genre page log
+ *
+ * @return void
+ * @access public
+ */
+	function main() {
+		
+             App::import('Component', 'Common');
+             $this->common = &new CommonComponent();
+             $result= $this->common->runGenreCacheFromShell(); 
+	}
+
+}
+
+?>
