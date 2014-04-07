7c088c684441b0bd25c8a72024fb03579fe715fb
daviddickson <david@m68interactive.com>
2014-04-07 15:18:19 -0400

    remove unused code, optimize jquery

--- webroot/js/site.js
+++ webroot/js/site.js
@@ -63,31 +63,36 @@ $('document').ready(function() {
 
 
     //album-page js
-    $('.albums-page .album-detail-container').on('mouseenter', '.album-cover-image', function() {
+    $album_page_album_detail_container = $('.albums-page').find('.album-detail-container');
+
+    $album_page_album_detail_container.on('mouseenter', '.album-cover-image', function() {
         $(this).find('.album-preview').css({opacity: 1});
         $(this).find('.add-to-playlist-button').css({opacity: 1});
     });
 
-    $('.albums-page .album-detail-container').on('mouseleave', '.album-cover-image', function() {
+    $album_page_album_detail_container.on('mouseleave', '.album-cover-image', function() {
         $(this).find('.album-preview').css({opacity: 0});
         $(this).find('.add-to-playlist-button').css({opacity: 0});
         $(this).find('.wishlist-popover').removeClass('active');
     });
+
+    // $('.albums-page .album-detail-container').on('mouseenter', '.album-cover-image', function() {
+    //     $(this).find('.album-preview').css({opacity: 1});
+    //     $(this).find('.add-to-playlist-button').css({opacity: 1});
+    // });
+
+    // $('.albums-page .album-detail-container').on('mouseleave', '.album-cover-image', function() {
+    //     $(this).find('.album-preview').css({opacity: 0});
+    //     $(this).find('.add-to-playlist-button').css({opacity: 0});
+    //     $(this).find('.wishlist-popover').removeClass('active');
+    // });
 
     $('.albums-page .album-detail-container .tracklist-container').on('mouseleave', '.tracklist', function() {
         $(this).find('.wishlist-popover').removeClass('active');
     });
 
 
-    //genres-page 
-    $('.genres-page .album-detail-container').on('mouseenter', '.album-detail', function() {
-        $('.album-preview').css({opacity: 1});
-    });
-
-    $('.genres-page .album-detail-container').on('mouseleave', '.album-detail', function() {
-        $('.album-preview').css({opacity: 0});
-        $(this).find('.wishlist-popover').removeClass('active');
-    });
+    
 
     $(document).on('mouseleave', '.search-page .tracklist-container .tracklist', function() {
         $(this).find('.wishlist-popover').removeClass('active');
