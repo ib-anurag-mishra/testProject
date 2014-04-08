c36511550961be418e9e7eff29bac0c9a83334fa
daviddickson <david@m68interactive.com>
2014-04-08 13:30:12 -0400

    remove unused js

--- webroot/js/site.js
+++ webroot/js/site.js
@@ -444,49 +444,7 @@ $('document').ready(function() {
 
     $('.artist-page .videos-scrollable ul').css({width: totalVSLiWidth + 5});
 
-    $(document).on('click', '.artist-page .tracklist-scrollable .tracklist .preview', function() {
 
-        if ($(this).hasClass('playing')) {
-
-            $(this).removeClass('playing');
-            $(this).parents('.row').removeClass('playing');
-            $(this).parent().removeClass('playing');
-            $(this).siblings('.date').removeClass('playing');
-            $(this).siblings('.album').removeClass('playing');
-            $(this).siblings('.artist').removeClass('playing');
-            $(this).siblings('.time').removeClass('playing');
-            $(this).siblings('.song').removeClass('playing');
-            $(this).siblings('.add-to-wishlist-button').removeClass('playing');
-            $(this).siblings('.download').removeClass('playing');
-
-
-        } else {
-
-            $('.artist-page .tracklist-scrollable .tracklist').removeClass('playing');
-            $('.artist-page .tracklist-scrollable .tracklist .date').removeClass('playing');
-            $('.artist-page .tracklist-scrollable .tracklist .preview').removeClass('playing');
-            $('.artist-page .tracklist-scrollable .tracklist .album').removeClass('playing');
-            $('.artist-page .tracklist-scrollable .tracklist .artist').removeClass('playing');
-            $('.artist-page .tracklist-scrollable .tracklist .time').removeClass('playing');
-            $('.artist-page .tracklist-scrollable .tracklist .song').removeClass('playing');
-            $('.artist-page .tracklist-scrollable .tracklist .add-to-wishlist-button').removeClass('playing');
-            $('.artist-page .tracklist-scrollable .tracklist .download').removeClass('playing');
-
-            $(this).addClass('playing');
-            $(this).parents('.tracklist').addClass('playing');
-            $(this).parent().addClass('playing');
-            $(this).siblings('.date').addClass('playing');
-            $(this).siblings('.album').addClass('playing');
-            $(this).siblings('.artist').addClass('playing');
-            $(this).siblings('.time').addClass('playing');
-            $(this).siblings('.song').addClass('playing');
-            $(this).siblings('.add-to-wishlist-button').addClass('playing');
-            $(this).siblings('.download').addClass('playing');
-
-
-        }
-
-    });
     
     $(document).on('click', '.artist-page .album-shadow-container div.paging span a', function(event) {
 
