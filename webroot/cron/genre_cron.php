00976e5aa0011ff20c9190072e68c4a8bdb0b71f
Narendra Nagesh <nagesh4group@gmail.com>
2014-04-11 13:17:46 +0530

    updating the genre cron changes for old branch

---
+++ webroot/cron/genre_cron.php
@@ -0,0 +1,16 @@
+<?php
+    error_reporting(E_ALL);
+    set_time_limit(0);
+    include 'functions.php';
+    $memcache = new Memcache;
+    $memcache->addServer('10.209.137.72', 11211);     
+    $url = "http://10.181.60.3/cache/runGenreCache";
+    print $url;
+    $ch=curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_SSLVERSION, 3);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER,1);
+    $result = curl_exec($ch);
+    curl_close($ch);exit;    
+?>
