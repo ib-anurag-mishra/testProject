1178f3759e324057616a24a932d13d843df04b06
kushalpogul <kushal.pogul@infobeans.com>
2014-04-28 15:35:34 +0530

    debug

--- webroot/cron/combine_genre.php
+++ webroot/cron/combine_genre.php
@@ -22,7 +22,6 @@ mysql_select_db("freegal", $conn);
 $lf = new logfile();
 
 // Read values from combine_genre table and store in associative array.
-
 $syngenre_query    = "SELECT genre, expected_genre from combine_genre";
 $rs_syngenre       = mysql_query($syngenre_query) or die('Query failed: ' . mysql_error());
 $total_syngenres   = mysql_num_rows($rs_syngenre);
@@ -40,8 +39,7 @@ for($count=0;$count<$total_syngenres; $count++)
 }
 
 
-// Find Total number of records in Genre Table
-
+        //Find Total number of records in Genre Table
         $count_query        = "SELECT count(*) from Genre";
         $rs_count           = mysql_query($count_query) or die('Query failed: ' . mysql_error());
         $count_data         = mysql_fetch_array($rs_count, MYSQL_ASSOC);
@@ -49,23 +47,24 @@ for($count=0;$count<$total_syngenres; $count++)
         echo "<br>Total records in Genre Table: ". $total_genres;
         $lf->write("\nTotal records in Genre Table: ". $total_genres);
 
+        
+        //Find Total Iterations to update expected_genre column in Genre Table.
         $total_iterations   =   ceil($total_genres/10000);
-        
-        
+               
         for($count=0; $count<$total_iterations; $count++)
         {
             $start_limit    =   10000*$count;
             $end_limit      =   ($start_limit+10000)-1;
             
-            //echo "<br>Start Limit: ".$start_limit.", End Limit: ".$end_limit;
-            
+            // echo "<br>Start Limit: ".$start_limit.", End Limit: ".$end_limit;
+            // Fetch 10000 records from Genre table to update expected_genre Field.
             $genre_query    = "SELECT ProdID, Genre from Genre Limit ".$start_limit.", ".$end_limit;
             $rs_genre       = mysql_query($genre_query) or die('Query failed: ' . mysql_error());
             $tot_count      = mysql_num_rows($rs_genre);
             
             while ($rowData = mysql_fetch_array($rs_genre, MYSQL_ASSOC)) 
             {
-                $value_to_be_updated    =  $combine_genre_arr[$rowData['Genre']]; 
+                $value_to_be_updated    =  $combine_genre_arr[$rowData['Genre']]; // value from expected_genre field in combine_genre table
                 //$genreUpdate_query      =  "Update Genre set expected_genre='".$value_to_be_updated."' where ProdID=".$rowData['ProdID']." and Genre='".$rowData['Genre']."'";
                 //$rs_genre               =  mysql_query($genreUpdate_query) or die('Query failed: ' . mysql_error());
                 echo "<br>Genre updated: From ". $rowData['Genre']." to ".$value_to_be_updated." having ProdID ".$rowData['ProdID'];
@@ -75,41 +74,7 @@ for($count=0;$count<$total_syngenres; $count++)
             die;
         }
         
-        
-// Read distinct genres from Genre table and do processing of array
-/*
-$genre_query    = "SELECT distinct Genre from Genre";
-$rs_genre       = mysql_query($genre_query) or die('Query failed: ' . mysql_error());
-$total_genres   = mysql_num_rows($rs_genre);
-
-echo "<br>Total Distinct Genres: ". mysql_num_rows($rs_genre);
-
-for($count=0;$count<$total_genres; $count++)
-{
-    $row_data               =   mysql_fetch_array($rs_genre, MYSQL_ASSOC);
-    //print_r($row_data);  
-    if(!empty($row_data['Genre']))
-    {
-        $count_query        = "SELECT count(*) from Genre where Genre='".$row_data['Genre']."'";
-        $rs_count           = mysql_query($count_query) or die('Query failed: ' . mysql_error());
-        $tot_dgenres        = mysql_num_rows($rs_count);
-        
-        if($tot_dgenres>10000)
-        {
-            $total_iterations = ceil($tot_dgenres/10000);
-            
-            for($count=0;$count<$total_iterations; $count++)
-            {
-                
-            }
-        }
-        else
-        {
-            
-        }
-    }
-}*/
-
-
+        echo "<br>Done With Updation";
+        $lf->write("\nDone with Updation");
 
 ?>
