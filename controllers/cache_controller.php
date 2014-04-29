6d70e3021c4bc97eeb3fd34fd3c11cb2d8629112
kiranpyati <kiran.pyati@infobeans.com>
2014-04-29 16:04:53 +0530

    Optimize new_release_albums cache object

--- controllers/cache_controller.php
+++ controllers/cache_controller.php
@@ -105,6 +105,7 @@ class CacheController extends AppController {
             $this->Common->getUsTop10Albums($territory);
             $this->Common->getUsTop10Videos($territory);
             $this->Common->getNewReleaseAlbums($territory);
+            $this->Common->getNewReleaseAlbums($territory, true);
             $this->Common->getNewReleaseVideos($territory);
             $this->Common->getFeaturedArtists($territory);
             $this->Common->getDifferentGenreData($territory);
