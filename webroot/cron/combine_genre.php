a59721faf76de7becb4ed0b23f6a7412d02bd6c6
kushalpogul <kushal.pogul@infobeans.com>
2014-05-02 11:23:58 +0530

    modify genre combine script

--- webroot/cron/combine_genre.php
+++ webroot/cron/combine_genre.php
@@ -65,8 +65,8 @@ for($count=0;$count<$total_syngenres; $count++)
             while ($rowData = mysql_fetch_array($rs_genre, MYSQL_ASSOC)) 
             {
                 $value_to_be_updated    =  $combine_genre_arr[$rowData['Genre']]; // value from expected_genre field in combine_genre table
-                //$genreUpdate_query      =  "Update Genre set expected_genre='".$value_to_be_updated."' where ProdID=".$rowData['ProdID']." and Genre='".$rowData['Genre']."'";
-                //$rs_genre               =  mysql_query($genreUpdate_query) or die('Query failed: ' . mysql_error());
+                $genreUpdate_query      =  "Update Genre set expected_genre='".$value_to_be_updated."' where ProdID=".$rowData['ProdID']." and Genre='".$rowData['Genre']."'";
+                $rs_genre               =  mysql_query($genreUpdate_query) or die('Query failed: ' . mysql_error());
                 echo "<br>Genre updated: From ". $rowData['Genre']." to ".$value_to_be_updated." having ProdID ".$rowData['ProdID'];
                 $lf->write("\nGenre updated: From ". $rowData['Genre']." to ".$value_to_be_updated." having ProdID ".$rowData['ProdID']);
             }
