0954064e67021985e22423b11179b3072522e9e4
kushalpogul <kushal.pogul@infobeans.com>
2014-05-07 17:29:59 +0530

    debug

--- controllers/components/common.php
+++ controllers/components/common.php
@@ -2973,10 +2973,17 @@ STR;
         
         if($genre_name!='')
         {
-            
+            $synGenres = '';
+            for($cnt=0; $cnt<count($combineGenreData); $cnt++)
+            {
+                if($combineGenreData[$cnt]['CombineGenre']['expected_genre']==$genre_name)       // if $genre_name (expected_genre from Genre table) matches  $combineGenreData[$cnt]['CombineGenre']['expected_genre'] (expected_genre from combine_genres table), then copy genre value from combine_genre
+                {
+                    $synGenres  .=    empty($synGenres)?$combineGenreData[$cnt]['CombineGenre']['expected_genre']:'|'.$combineGenreData[$cnt]['CombineGenre']['expected_genre'];
+                }
+            }
         }
         
-        return $territoryNames;
+        return $synGenres;
     }
 
 }
