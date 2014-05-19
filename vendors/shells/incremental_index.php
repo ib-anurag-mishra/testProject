82118f91dbf756bc770de2c4989ac9cf97ede644
kiranpyati <kiran.pyati@infobeans.com>
2014-05-19 18:10:07 +0530

    cron for delta import

--- vendors/shells/incremental_index.php
+++ vendors/shells/incremental_index.php
@@ -6,7 +6,7 @@ class IncrementalIndexShell extends Shell {
 
 	var $core1 = 'freegalmusicstage';
 	var $core2 = 'freegalmusicvideos';
-	
+
 	var $songsIndexUrl = "http://192.168.100.24:8080/solr/freegalmusicstage/dataimport";
 
 	var $videosIndexUrl = "http://192.168.100.24:8080/solr/freegalmusicvideos/dataimport";
@@ -21,164 +21,87 @@ class IncrementalIndexShell extends Shell {
 
 	function main() {
 
-		$httpSocket = new HttpSocket();
-
-		// start songs data indexing
-
 		//log
 		$logId = strtotime(date('Y-m-d h:i:s'));
 		$logData = PHP_EOL."----------Request (".$logId.") Start----------------".PHP_EOL;
 		$logData .= date('Y-m-d h:i:s').' > Start Time: '.date('Y-m-d h:i:s').PHP_EOL;
 
-		$response = $httpSocket->get($this->songsIndexUrl,$this->query);
-
-		// object type-casted
-		$checkValidXml = null;
-		$checkValidXml = simplexml_load_string($response);
-
-		// executes IF for valid xml response
-		if($checkValidXml) {
-			$response = $httpSocket->get($this->songsIndexUrl,$this->statusQuery);
-
-			// object type-casted
-			$objXmlResponse = simplexml_load_string($response);
-
-			// type-casted to array format
-			$arrData = (array)$objXmlResponse;
-			if('busy' == strtolower($arrData['str'][1])) {
-				// valid
-
-				$logData .= date('Y-m-d h:i:s').' > Incremental Indexing Started ( Response: '.$response.' )'.PHP_EOL;
-				$logData .= "\n\n";
-
-				$msg = 'Incremental Indexing Started ('.$response.')';
-
-				mail($this->emailList, 'Apache Solr Indexer ' . $this->core1 .' ('.date('Y-m-d h:i').'-'.$logId.') Status', 'Status :- "'.$msg.'"');
-
-				// sleep perodic
-				sleep ( $this->sleepTime );
-
-				$status = 1;
-
-				while($status){
-					sleep ( $this->sleepTime );
-					$response = $httpSocket->get($this->songsIndexUrl,$this->statusQuery);
-					$status   = $this->getOperationStatus($response, $logId, $this->core1);
-				}
-
-			} else {
-				$logData .= date('Y-m-d h:i:s').' > Indexing failed to start ( Response: '.$response.' )'.PHP_EOL;
-				$logData .= "\n\n";
-				$logData .= date('Y-m-d h:i:s').' > End Time: '.date('Y-m-d h:i:s').PHP_EOL;
-				$logData .= PHP_EOL."---------Request (".$logId.") End----------------";
-
-				$msg = 'Indexing Failed To Start: Internal Error ('.$response.')';
-				mail($this->emailList, 'Apache Solr Indexer ' . $this->core1 . ' ('.date('Y-m-d h:i').'-'.$logId.') Status', 'Status :- "'.$msg.'"');
-			}
-		} else {
-
-			$logData .= date('Y-m-d h:i:s').' > Indexing failed to start ( Response: '.$response.' )'.PHP_EOL;
-			$logData .= "\n\n";
-			$logData .= date('Y-m-d h:i:s').' > Indexing failed to start ( Response: '.$response.' )'.PHP_EOL;
-			$logData .= date('Y-m-d h:i:s').' > End Time: '.date('Y-m-d h:i:s').PHP_EOL;
-			$logData .= PHP_EOL."---------Request (".$logId.") End----------------";
-			$msg = 'Indexing Failed To Start: Valid response XML not sent ('.$response.')';
-			mail($this->emailList, 'Apache Solr Indexer ' . $this->core1 . ' ('.date('Y-m-d h:i').'-'.$logId.') Status', 'Status :- "'.$msg.'"');
-			writeToLog($logData);
-		}//for invalid xml response
-
-		$response = $httpSocket->get($this->videosIndexUrl, $this->query);
-
-		// object type-casted
-		$checkValidXml = null;
-		$checkValidXml = simplexml_load_string($response);
-
-		// executes IF for valid xml response
-		if($checkValidXml) {
-
-			$response = $httpSocket->get($this->videosIndexUrl, $this->statusQuery);
-
-			// object type-casted
-			$objXmlResponse = simplexml_load_string($response);
-				
-			// type-casted to array format
-			$arrData = (array)$objXmlResponse;
-			
-			if('busy' == strtolower($arrData['str'][1])) {
-				// valid
-			
-				$logData .= date('Y-m-d h:i:s').' > Incremental Indexing Started ( Response: '.$response.' )'.PHP_EOL;
-				$logData .= "\n\n";
-			
-				$msg = 'Incremental Indexing Started ('.$response.')';
-			
-				mail($this->emailList, 'Apache Solr Indexer ' . $this->core2 .' ('.date('Y-m-d h:i').'-'.$logId.') Status', 'Status :- "'.$msg.'"');
-			
-				// sleep perodic
-				sleep ( $this->sleepTime );
-			
-				$status = 1;
-			
-				while($status){
-					sleep ( $this->sleepTime );
-					$response = $httpSocket->get($this->videosIndexUrl,$this->statusQuery);
-					$status   = $this->getOperationStatus($response, $logId, $this->core2);
-				}
-			
-			} else {
-				$logData .= date('Y-m-d h:i:s').' > Indexing failed to start ( Response: '.$response.' )'.PHP_EOL;
-				$logData .= "\n\n";
-				$logData .= date('Y-m-d h:i:s').' > End Time: '.date('Y-m-d h:i:s').PHP_EOL;
-				$logData .= PHP_EOL."---------Request (".$logId.") End----------------";
-			
-				$msg = 'Indexing Failed To Start: Internal Error ('.$response.')';
-				mail($this->emailList, 'Apache Solr Indexer ' . $this->core2 . ' ('.date('Y-m-d h:i').'-'.$logId.') Status', 'Status :- "'.$msg.'"');
-			}
-		} else {
-			
-			$logData .= date('Y-m-d h:i:s').' > Indexing failed to start ( Response: '.$response.' )'.PHP_EOL;
-			$logData .= "\n\n";
-			$logData .= date('Y-m-d h:i:s').' > Indexing failed to start ( Response: '.$response.' )'.PHP_EOL;
-			$logData .= date('Y-m-d h:i:s').' > End Time: '.date('Y-m-d h:i:s').PHP_EOL;
-			$logData .= PHP_EOL."---------Request (".$logId.") End----------------";
-			$msg = 'Indexing Failed To Start: Valid response XML not sent ('.$response.')';
-			mail($this->emailList, 'Apache Solr Indexer ' . $this->core2 . ' ('.date('Y-m-d h:i').'-'.$logId.') Status', 'Status :- "'.$msg.'"');
-			writeToLog($logData);
-		}
+		$this->processing( $this->songsIndexUrl, $this->core1, $logId );
+		$this->processing( $this->videosIndexUrl, $this->core2, $logId );
 	}
 
-	function getOperationStatus($response, $logId, $core) {
-	
-		$objXmlResponse = simplexml_load_string($response);
-			
-		// type-casted to array format
-		$arrData = (array)$objXmlResponse;
+	public function processing( $url, $coreName, $logId  ) {
+
+		$httpSocket = new HttpSocket();
+
+		$response = $httpSocket->get( $url, $this->query);
+
+		// object type-casted
+		$checkValidXml = null;
+		$checkValidXml = simplexml_load_string( $response );
+
+		if( $checkValidXml ) {
+
+			$response = $httpSocket->get( $url, $this->statusQuery );
+
+			// object type-casted
+			$arrData = (array) simplexml_load_string( $response );
+
+			if( 'busy' == strtolower( trim( $arrData['str'][1] ) ) ) {
+
+				$logData .= date('Y-m-d h:i:s').' > Incremental Indexing Started ( Response: '.$response.' )'.PHP_EOL;
+				$logData .= "\n\n";
+
+				$msg = 'Incremental Indexing Started ('.$response.')';
+
+				mail( $this->emailList, 'Apache Solr Indexer ' . $coreName .' ('.date('Y-m-d h:i').'-'.$logId.') Status', 'Status :- "'.$msg.'"' );
+
+				$status = 1;
+					
+				while( $status ) {
+					sleep ( $this->sleepTime );
+					$response = $httpSocket->get( $url, $this->statusQuery );
+					$status   = $this->getOperationStatus( $response, $logId, $coreName );
+				}
+
+			} else {
+
+				$logData .= date('Y-m-d h:i:s').' > Indexing failed to start ( Response: '.$response.' )'.PHP_EOL;
+				$logData .= "\n\n";
+				$logData .= date('Y-m-d h:i:s').' > End Time: '.date('Y-m-d h:i:s').PHP_EOL;
+				$logData .= PHP_EOL."---------Request (".$logId.") End----------------";
+					
+				$msg = 'Indexing Failed To Start: Internal Error ('.$response.')';
+				mail( $this->emailList, 'Apache Solr Indexer ' . $coreName . ' ('.date('Y-m-d h:i').'-'.$logId.') Status', 'Status :- "'.$msg.'"' );
+				writeToLog($logData);
+			}
+
+		} else {
+
+			$logData .= date('Y-m-d h:i:s').' > Indexing failed to start ( Response: '.$response.' )'.PHP_EOL;
+			$logData .= "\n\n";
+			$logData .= date('Y-m-d h:i:s').' > Indexing failed to start ( Response: '.$response.' )'.PHP_EOL;
+			$logData .= date('Y-m-d h:i:s').' > End Time: '.date('Y-m-d h:i:s').PHP_EOL;
+			$logData .= PHP_EOL."---------Request (".$logId.") End----------------";
+
+			$msg = 'Indexing Failed To Start: Valid response XML not sent ('.$response.')';
+			mail($this->emailList, 'Apache Solr Indexer ' . $coreName . ' ('.date('Y-m-d h:i').'-'.$logId.') Status', 'Status :- "'.$msg.'"');
+			writeToLog($logData);
+		}
+
+	}
+
+	public function getOperationStatus($response, $logId, $coreName) {
+
+		$arrData = (array) simplexml_load_string($response);
 			
 		if('busy' == strtolower($arrData['str'][1])) {
 			return 1;
 		} else {
-			//Total Documents Processed
-			$total_documents_processed = null;
-			if(isset($arrData['lst'][2]->str[10])) {
-				$test = (array)$arrData['lst'][2]->str[10];
-				$total_documents_processed = $test[0];
-			}
-	
-			//Total Time taken to process documents
-			$total_time = null;
-			if(isset($arrData['lst'][2]->str[11])) {
-				$test = (array)$arrData['lst'][2]->str[11];
-				$total_time = $test[0];
-			}
-				
-			if( (!empty($total_documents_processed)) && (!empty($total_time)) ) {
-				$msg = 'Indexer processed '.$total_documents_processed. ' documents in '.$total_time.' hours.';
-				mail($this->emailList, 'Apache Solr Indexer ' . $core . ' ('.date('Y-m-d h:i').'-'.$logId.') Status', 'Status :- "'.$msg.'"');
-			} else {
-				$msg = 'Indexer failed to complete ('.$response.')';
-				mail($this->emailList, 'Apache Solr Indexer ' . $core . ' ('.date('Y-m-d h:i').'-'.$logId.') Status', 'Status :- "'.$msg.'"');
-			}
-				
+			
+			$msg = 'Response of '. $coreName . ' Core:' . PHP_EOL . $response ;
+			mail($this->emailList, 'Apache Solr Indexer ' . $coreName . ' ('.date('Y-m-d h:i').'-'.$logId.') Status', 'Status :- "'.$msg.'"');
+
 			return 0;
 		}
 	}
