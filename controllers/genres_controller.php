29208325766677f05067e5626948e2500eb083da
Ralph Kelley <ralphkelley@fullsail.edu>
2014-05-21 09:48:57 -0400

    Uncommenting a cache read
    A cache read was commented out. I believe this was originally done by
    Narendra for testing and was never changed back.

--- controllers/genres_controller.php
+++ controllers/genres_controller.php
@@ -267,8 +267,8 @@ Class GenresController extends AppController
 
         $this->Genre->Behaviors->attach('Containable');
         $this->Genre->recursive = 2;
-        //if (($genre = Cache::read("genre" . $country)) === false)
-        if(1) { 
+        if (($genre = Cache::read("genre" . $country)) === false)
+        //if(1) { 
             $genreAll = $this->Genre->find('all', array(
                 'conditions' =>
                 array('and' =>
