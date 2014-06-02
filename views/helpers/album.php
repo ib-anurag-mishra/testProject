e7afcff71e93a9f9f705f1a281da4c7cbd3e51c9
kiranpyati <kiran.pyati@infobeans.com>
2014-06-02 16:45:37 +0530

    fixed warning errors for album section on search page

--- views/helpers/album.php
+++ views/helpers/album.php
@@ -18,18 +18,84 @@ class AlbumHelper extends AppHelper {
     }
     
     function getImage($id, $provider = null) {
-        if($provider == null) {
+    	$songInstance = ClassRegistry::init('Album');
+        /*if($provider == null) {
         	$conditions = array('Album.ProdID' => $id);
  		}
          else {
          	$conditions = array('Album.ProdID' => $id,'Album.provider_type'=>$provider);
  		}
-        $songInstance = ClassRegistry::init('Album');
+
         $details = $songInstance->find('all', array(
            'conditions'=>$conditions,
           )
-        );       
-        return  $details;
+        );*/
+
+    	if ( $provider == null ) {
+    		$conditions = ' Album.ProdID =' . $id;
+    	} else {
+    		$conditions = ' Album.ProdID =' . $id . ' AND Album.provider_type =\'' . $provider .'\'';
+    	}
+
+    	$sql = "SELECT
+    	`Album`.`ProdID`,
+    	`Album`.`ProductID`,
+    	`Album`.`AlbumTitle`,
+    	`Album`.`Title`,
+    	`Album`.`ArtistText`,
+    	`Album`.`Artist`,
+    	`Album`.`ArtistURL`,
+    	`Album`.`Label`,
+    	`Album`.`Copyright`,
+    	`Album`.`Advisory`,
+    	`Album`.`FileID`,
+    	`Album`.`DownloadStatus`,
+    	`Album`.`TrackBundleCount`,
+    	`Album`.`UPC`,
+    	`Album`.`PublicationStatus`,
+    	`Album`.`LastUpdated`,
+    	`Album`.`StatusNotes`,
+    	`Album`.`PublicationDate`,
+    	`Album`.`provider_type`,
+    	`Files`.`FileID`,
+    	`Files`.`SourceURL`,
+    	`Files`.`HostURL`,
+    	`Files`.`SaveAsName`,
+    	`Files`.`DigitalSignature`,
+    	`Files`.`CdnPath`,
+    	`Genre`.`ProdID`,
+    	`Genre`.`Genre`,
+    	`Genre`.`expected_genre`,
+    	`Genre`.`Subgenre`,
+    	`Genre`.`GenreId`,
+    	`Genre`.`provider_type`,
+    	`Country`.`ProdID`,
+    	`Country`.`Territory`,
+    	`Country`.`SalesDate`,
+    	`Country`.`TerritoryId`,
+    	`Country`.`provider_type`
+    	FROM
+    	`Albums` AS `Album`
+    	LEFT JOIN
+    	`File` AS `Files`
+    	ON (
+    	`Album`.`FileID` = `Files`.`FileID`
+    	)
+    	LEFT JOIN
+    	`Genre` AS `Genre`
+    	ON (
+    	`Genre`.`ProdID` = `Album`.`ProdID`
+    	)
+    	LEFT JOIN
+    	`us_countries` AS `Country`
+    	ON (
+    	`Country`.`ProdID` = `Album`.`ProdID`
+    	)
+    	WHERE
+    	`Album`.`ProdID` = $id
+    	AND `Album`.`provider_type` = '$provider'";
+    	$details = $songInstance->query($sql);
+    	return  $details;
     }
 }
 
