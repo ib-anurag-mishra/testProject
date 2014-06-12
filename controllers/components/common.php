fa0ff0a292026b0f40f67cddbd0b1d4c29965185
kushalpogul <kushal.pogul@infobeans.com>
2014-06-12 15:26:30 +0530

    Genre performance changes

--- controllers/components/common.php
+++ controllers/components/common.php
@@ -143,7 +143,7 @@ Class CommonComponent extends Object
      * 
      * @return  $artistListResults array
      */
-     function getArtistText($genreValue,$territory,$artistFilter='',$pageNo=1){
+     function getArtistText($genreValue,$territory,$artistFilter='',$pageNo=1){        
         set_time_limit(0);  
         
         //check the page no. must be greater than 0
@@ -156,7 +156,7 @@ Class CommonComponent extends Object
         //set the territory value
         $territory = strtolower($territory);
         
-        //create conditions 
+        //Common conditions 
         $conditionArray[] = "(Country.DownloadStatus = 1 OR Country.StreamingStatus = 1)";
         $conditionArray[] = "Country.provider_type = Song.provider_type";
         $conditionArray[] = "Song.ArtistText!=''";
@@ -166,13 +166,13 @@ Class CommonComponent extends Object
             $synonym_list   =   $this->getGenreSynonyms($genreValue);
             $conditionOR = '';
             foreach($synonym_list as $single_synGenre){
-                $conditionOR = empty($conditionOR)? "(Song.Genre LIKE '%".$single_synGenre."%'" : $conditionOR." OR Song.Genre LIKE '%".$single_synGenre."%'";            
+                $conditionOR = empty($conditionOR)? "(Genre.Genre LIKE '%".$single_synGenre."%'" : $conditionOR." OR Genre.Genre LIKE '%".$single_synGenre."%'";            
             }            
             if(!empty($conditionOR))
             {
                 $conditionArray[] = $conditionOR.")";
             }
-        }
+        }       
         
         //make condition according to Genre value
         if ($artistFilter == 'spl'){
@@ -181,10 +181,6 @@ Class CommonComponent extends Object
         elseif ($artistFilter != '' && $artistFilter != 'All') {
             $conditionArray[] = " Song.ArtistText LIKE '".$artistFilter."%'";
         }
-      
-        
-        $endLimit =  10000;
-        $startLimit = ($pageNo * 10000) - 10000;
         
         $songInstance->unbindModel(array('hasOne' => array('Participant')));
         $songInstance->unbindModel(array('hasOne' => array('Country')));
@@ -192,34 +188,69 @@ Class CommonComponent extends Object
         $songInstance->unbindModel(array('belongsTo' => array('Sample_Files','Full_Files')));
         $songInstance->recursive = 0;
         //create query that fetch all artist according to selected Genre
-        $artistListResults = $songInstance->find('all', array(
-            'conditions' => $conditionArray,
-            'fields' => array('Song.ArtistText'),
-            'limit'=> $endLimit, 'offset'=> $startLimit,
-            'order' => array('Song.ArtistText ASC'),
-            'joins' => array(
-                array(
-                    'table' => $territory.'_countries',
-                    'alias' => 'Country',
-                    'type' => 'inner',
-                    'foreignKey' => false,
-                    'conditions'=> array('Country.ProdID = Song.ProdID')
-                ),
-                array(
-                    'table' => 'Albums',
-                    'alias' => 'Albums',
-                    'type' => 'inner',
-                    'foreignKey' => false,
-                    'conditions'=> array('Song.ReferenceID = Albums.ProdID')
+        
+        if ($genreValue != 'All') {
+            
+            $endLimit =  120;
+            $startLimit = ($pageNo * 120) - 120;
+            
+            $artistListResults = $songInstance->find('all', array(
+                'conditions' => $conditionArray,
+                'fields' => array('DISTINCT Song.ArtistText'),
+                'limit'=> $endLimit, 'offset'=> $startLimit,
+                'order' => array('Song.ArtistText ASC'),
+                'joins' => array(
+                    array(
+                        'table' => $territory.'_countries',
+                        'alias' => 'Country',
+                        'type' => 'inner',
+                        'foreignKey' => false,
+                        'conditions'=> array('Country.ProdID = Song.ProdID', 'Country.provider_type = Song.provider_type')
+                    ),
+                    array(
+                        'table' => 'Genre',
+                        'alias' => 'Genres',
+                        'type' => 'inner',
+                        'foreignKey' => false,
+                        'conditions'=> array('Genres.ProdID = Song.ProdID', 'Genres.provider_type = Song.provider_type')
+                    ),
+                    array(
+                        'table' => 'Albums',
+                        'alias' => 'Albums',
+                        'type' => 'inner',
+                        'foreignKey' => false,
+                        'conditions'=> array('Song.ReferenceID = Albums.ProdID', )
+                    )
                 )
-            )
-         ));
+             ));        
+        
+        }
+        else
+        {
+            $endLimitG =  12000;
+            $startLimitG = ($pageNoG * 12000) - 12000;
+            
+            /* Query written in below format as it is not possible to write in Cakephp Standard.
+             * Ref URL: http://stackoverflow.com/questions/8175080/cakephp-select-from-subquery-select-foo-from-select
+             * Ref URL: http://stackoverflow.com/questions/3781654/how-to-implement-a-sorting-subquery-in-the-from-section
+             */
+            
+            $artistListResults = $songInstance->query( "SELECT DISTINCT `DArtistText`.`ArtistText` 
+                                                        FROM 
+                                                        (SELECT `Song`.`ArtistText` FROM `Songs` AS `Song` 
+                                                        LEFT JOIN `us_countries` AS `Country` ON (`Country`.`ProdID` = `Song`.`ProdID` and `Country`.`provider_type` = `Song`.`provider_type`) 
+                                                        LEFT JOIN `Albums` AS `Albums` ON (`Song`.`ReferenceID` = `Albums`.`ProdID`) 
+                                                        WHERE (`Country`.`DownloadStatus` = 1 or `Country`.`StreamingStatus` =1) 
+                                                        AND `Song`.`ArtistText`!='' 
+                                                        ORDER BY `Song`.`ArtistText` ASC 
+                                                        LIMIT ".$startLimitG.", ".$endLimitG.") DArtistText");
+        }
                 
          //set artist list in the cache
          if (!empty($artistListResults)) {             
             //create cache variable name
              
-            $artistListResults   =   $this->processGenre($artistListResults);
+            //$artistListResults   =   $this->processGenre($artistListResults);
              
             $cacheVariableName = base64_encode($genreValue).$territory.strtolower($artistFilter).$pageNo;              
             Cache::write($cacheVariableName, $artistListResults,'GenreCache');    
