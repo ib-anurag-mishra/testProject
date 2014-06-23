f7640691ec234939720b5507cfae1463bc8dc9ef
Ratnesh Kumar Gupta <ratnesh.gupta@infobeans.com>
2014-06-23 15:03:53 +0530

    debugging

--- webroot/cron/royalty_report.php
+++ webroot/cron/royalty_report.php
@@ -12,6 +12,7 @@ unlink($reportsFolder . '/tmp_debug_data.txt');
 $arr_dates = array();
 //set timezone
 date_default_timezone_set('America/New_York');
+ini_set('memory_limit', '-1');
 
 //$arr_dates['month']['from_date'] = date('Y-m-01 00:00:00',strtotime('-31 days'));
 //$arr_dates['month']['to_date'] = date('Y-m-t 23:59:59',strtotime('-31 days'));
@@ -62,7 +63,22 @@ foreach ($arr_dates AS $key => $value)
                     array('RECORD_TYPE', 'DETAIL_ROWS', 'TOTAL_SOLD', 'TOTAL_FREE', 'TOTAL_PROMO', 'TOTAL_SUB', 'TOTAL_UPGRADE', 'TOTAL_REFUND')
                 ),
             );
-            $query = "SELECT clp.library_id, clp.library_contract_start_date, clp.library_contract_end_date, clp.library_unlimited, sum(lp.purchased_tracks) as total_tracks_purchase ,  sum(lp.purchased_amount) as total_amount, l.library_territory, lp.library_id, clp.id as contract_id, clp.id_library_purchases, l.library_name, l.library_user_download_limit, l.library_download_type, l.library_download_limit, l.library_current_downloads, l.library_total_downloads, l.library_available_downloads, l.library_territory, l.library_status FROM library_purchases lp INNER JOIN contract_library_purchases clp ON lp.id = clp.id_library_purchases INNER JOIN libraries l ON clp.library_id = l.id WHERE ( (clp.library_contract_start_date <= '" . $value['from_date'] . "' AND clp.library_contract_end_date >= '" . $value['to_date'] . "')  OR (clp.library_contract_start_date <= '" . $value['from_date'] . "' AND clp.library_contract_end_date BETWEEN '" . $value['from_date'] . "' AND '" . $value['to_date'] . "') OR (clp.library_contract_start_date BETWEEN '" . $value['from_date'] . "' AND '" . $value['to_date'] . "' AND clp.library_contract_end_date >= '" . $value['to_date'] . "') OR (clp.library_contract_start_date >= '" . $value['from_date'] . "' AND clp.library_contract_end_date <= '" . $value['to_date'] . "') ) AND clp.library_unlimited = '" . $libTypeValue . "' AND l.library_territory='" . $row_country['library_territory'] . "' GROUP BY concat(clp.library_contract_start_date,'-',clp.library_contract_end_date,'-',clp.library_id),clp.library_unlimited,clp.library_id ORDER BY clp.library_id;";
+            echo $query = "SELECT clp.library_id, clp.library_contract_start_date, clp.library_contract_end_date, clp.library_unlimited, "
+                    . "sum(lp.purchased_tracks) as total_tracks_purchase ,  sum(lp.purchased_amount) as total_amount, l.library_territory, "
+                    . "lp.library_id, clp.id as contract_id, clp.id_library_purchases, l.library_name, l.library_user_download_limit, "
+                    . "l.library_download_type, l.library_download_limit, l.library_current_downloads, l.library_total_downloads, "
+                    . "l.library_available_downloads, l.library_territory, l.library_status "
+                    . "FROM library_purchases lp "
+                    . "INNER JOIN contract_library_purchases clp ON lp.id = clp.id_library_purchases "
+                    . "INNER JOIN libraries l ON clp.library_id = l.id "
+                    . "WHERE ( (clp.library_contract_start_date <= '" . $value['from_date'] . "' AND clp.library_contract_end_date >= '" . $value['to_date'] . "')  "
+                    . "OR (clp.library_contract_start_date <= '" . $value['from_date'] . "' AND clp.library_contract_end_date BETWEEN '" . $value['from_date'] . "' "
+                    . "AND '" . $value['to_date'] . "') OR (clp.library_contract_start_date BETWEEN '" . $value['from_date'] . "' AND '" . $value['to_date'] . "' "
+                    . "AND clp.library_contract_end_date >= '" . $value['to_date'] . "') OR (clp.library_contract_start_date >= '" . $value['from_date'] . "' "
+                    . "AND clp.library_contract_end_date <= '" . $value['to_date'] . "') ) AND clp.library_unlimited = '" . $libTypeValue . "' "
+                    . "AND l.library_territory='" . $row_country['library_territory'] . "' "
+                    . "GROUP BY concat(clp.library_contract_start_date,'-',clp.library_contract_end_date,'-',clp.library_id),clp.library_unlimited,clp.library_id "
+                    . "ORDER BY clp.library_id;";
 
             //$query = "select Songs.*,count(Songs.ProdID) as unit_count,Albums.AlbumTitle,Albums.UPC,libraries.library_territory From downloads JOIN Songs on Songs.ProdID = downloads.ProdID AND Songs.provider_type = 'ioda'  JOIN Albums ON Albums.ProdID = Songs.ReferenceID AND Albums.provider_type = 'ioda' JOIN libraries ON downloads.library_id = libraries.id Where downloads.created >= '".$value['from_date']."' AND downloads.created <= '".$value['to_date']."' AND downloads.provider_type='ioda' AND libraries.library_territory = '$row_country['library_territory']' GROUP BY Songs.ProdID ";
 
@@ -83,26 +99,59 @@ foreach ($arr_dates AS $key => $value)
                 {
                     if (strtotime($q['library_contract_end_date']) >= strtotime($value['to_date']))
                     {
-                        $song_download_query = "select Songs.*,count(Songs.ProdID) as unit_count,Albums.AlbumTitle,Albums.UPC,libraries.library_territory, downloads.created From $fetchRecordsFromTable as downloads JOIN Songs on Songs.ProdID = downloads.ProdID AND Songs.provider_type = 'ioda'  JOIN Albums ON Albums.ProdID = Songs.ReferenceID AND Albums.provider_type = 'ioda' JOIN libraries ON downloads.library_id = libraries.id Where downloads.created >= '" . $value['from_date'] . "' AND downloads.created <= '" . $value['to_date'] . "' AND downloads.provider_type='ioda' AND libraries.library_territory = '" . $row_country['library_territory'] . "' AND library_id = " . $q['library_id'] . " GROUP BY Songs.ProdID ";
+                        echo '1';
+                        echo $song_download_query = "select Songs.*,count(Songs.ProdID) as unit_count,Albums.AlbumTitle,Albums.UPC,libraries.library_territory, downloads.created "
+                                . "From $fetchRecordsFromTable as downloads "
+                                . "JOIN Songs on Songs.ProdID = downloads.ProdID AND Songs.provider_type = 'ioda'  "
+                                . "JOIN Albums ON Albums.ProdID = Songs.ReferenceID AND Albums.provider_type = 'ioda' "
+                                . "JOIN libraries ON downloads.library_id = libraries.id "
+                                . "Where downloads.created >= '" . $value['from_date'] . "' AND downloads.created <= '" . $value['to_date'] . "' "
+                                . "AND downloads.provider_type='ioda' AND libraries.library_territory = '" . $row_country['library_territory'] . "' "
+                                . "AND library_id = " . $q['library_id'] . " GROUP BY Songs.ProdID ";
                     }
                     else
                     {
-                        //echo "2";
-                        $song_download_query = "select Songs.*,count(Songs.ProdID) as unit_count,Albums.AlbumTitle,Albums.UPC,libraries.library_territory, downloads.created From $fetchRecordsFromTable as downloads JOIN Songs on Songs.ProdID = downloads.ProdID AND Songs.provider_type = 'ioda'  JOIN Albums ON Albums.ProdID = Songs.ReferenceID AND Albums.provider_type = 'ioda' JOIN libraries ON downloads.library_id = libraries.id Where downloads.created >= '" . $value['from_date'] . "' AND downloads.created <= '" . $q['library_contract_end_date'] . " 23:59:59' AND downloads.provider_type='ioda' AND libraries.library_territory = '" . $row_country['library_territory'] . "' AND library_id = " . $q['library_id'] . " GROUP BY Songs.ProdID ";
+                        echo "2";
+                        echo $song_download_query = "select Songs.*,count(Songs.ProdID) as unit_count,"
+                                . "Albums.AlbumTitle,Albums.UPC,libraries.library_territory, downloads.created "
+                                . "From $fetchRecordsFromTable as downloads "
+                                . "JOIN Songs on Songs.ProdID = downloads.ProdID AND Songs.provider_type = 'ioda'  "
+                                . "JOIN Albums ON Albums.ProdID = Songs.ReferenceID AND Albums.provider_type = 'ioda' "
+                                . "JOIN libraries ON downloads.library_id = libraries.id Where downloads.created >= '" . $value['from_date'] . "' "
+                                . "AND downloads.created <= '" . $q['library_contract_end_date'] . " 23:59:59' AND downloads.provider_type='ioda' "
+                                . "AND libraries.library_territory = '" . $row_country['library_territory'] . "' AND library_id = " . $q['library_id'] . " "
+                                . "GROUP BY Songs.ProdID ";
                     }
                 }
                 else
                 {
                     if (strtotime($q['library_contract_end_date']) >= strtotime($value['to_date']))
                     {
-                        //echo "3";
+                        echo "3";
                         //$total_download_query = "SELECT library_id, count(*) as total_count From $fetchRecordsFromTable as downloads WHERE provider_type='ioda' and downloads.created between '" . $q['library_contract_start_date'] . " 00:00:00' and '" . $to_date . "' and library_id = " . $q['library_id'];
-                        $song_download_query = "select Songs.*,count(Songs.ProdID) as unit_count,Albums.AlbumTitle,Albums.UPC,libraries.library_territory, downloads.created From $fetchRecordsFromTable as downloads JOIN Songs on Songs.ProdID = downloads.ProdID AND Songs.provider_type = 'ioda'  JOIN Albums ON Albums.ProdID = Songs.ReferenceID AND Albums.provider_type = 'ioda' JOIN libraries ON downloads.library_id = libraries.id Where downloads.created >= '" . $q['library_contract_start_date'] . " 00:00:00' AND downloads.created <= '" . $value['to_date'] . "' AND downloads.provider_type='ioda' AND libraries.library_territory = '" . $row_country['library_territory'] . "' AND library_id = " . $q['library_id'] . " GROUP BY Songs.ProdID ";
+                        echo $song_download_query = "select Songs.*,count(Songs.ProdID) as unit_count,Albums.AlbumTitle,Albums.UPC,"
+                                . "libraries.library_territory, downloads.created "
+                                . "From $fetchRecordsFromTable as downloads "
+                                . "JOIN Songs on Songs.ProdID = downloads.ProdID AND Songs.provider_type = 'ioda'  "
+                                . "JOIN Albums ON Albums.ProdID = Songs.ReferenceID AND Albums.provider_type = 'ioda' "
+                                . "JOIN libraries ON downloads.library_id = libraries.id "
+                                . "Where downloads.created >= '" . $q['library_contract_start_date'] . " 00:00:00' "
+                                . "AND downloads.created <= '" . $value['to_date'] . "' AND downloads.provider_type='ioda' "
+                                . "AND libraries.library_territory = '" . $row_country['library_territory'] . "' "
+                                . "AND library_id = " . $q['library_id'] . " GROUP BY Songs.ProdID ";
                     }
                     else
                     {
-                        //echo "4";
-                        $song_download_query = "select Songs.*,count(Songs.ProdID) as unit_count,Albums.AlbumTitle,Albums.UPC,libraries.library_territory, downloads.created From $fetchRecordsFromTable as downloads JOIN Songs on Songs.ProdID = downloads.ProdID AND Songs.provider_type = 'ioda'  JOIN Albums ON Albums.ProdID = Songs.ReferenceID AND Albums.provider_type = 'ioda' JOIN libraries ON downloads.library_id = libraries.id Where downloads.created between '" . $q['library_contract_start_date'] . " 00:00:00' and '" . $q['library_contract_end_date'] . " 23:59:59' AND downloads.provider_type='ioda' AND libraries.library_territory = '" . $row_country['library_territory'] . "' AND library_id = " . $q['library_id'] . " GROUP BY Songs.ProdID ";
+                        echo "4";
+                        $song_download_query = "select Songs.*,count(Songs.ProdID) as unit_count,Albums.AlbumTitle,Albums.UPC,"
+                                . "libraries.library_territory, downloads.created "
+                                . "From $fetchRecordsFromTable as downloads "
+                                . "JOIN Songs on Songs.ProdID = downloads.ProdID AND Songs.provider_type = 'ioda'  "
+                                . "JOIN Albums ON Albums.ProdID = Songs.ReferenceID AND Albums.provider_type = 'ioda' "
+                                . "JOIN libraries ON downloads.library_id = libraries.id "
+                                . "Where downloads.created between '" . $q['library_contract_start_date'] . " 00:00:00' and '" . $q['library_contract_end_date'] . " 23:59:59' "
+                                . "AND downloads.provider_type='ioda' AND libraries.library_territory = '" . $row_country['library_territory'] . "' "
+                                . "AND library_id = " . $q['library_id'] . " GROUP BY Songs.ProdID ";
                     }
                 }
 
@@ -133,37 +182,37 @@ foreach ($arr_dates AS $key => $value)
                 }
             }
             
-            $version = 1;
-            $file_name = "Freegal_r_" . strtolower($row_country['library_territory']) . "_" . date('Ym', strtotime($value['from_date'])) . '_' . $libTypeKey . "_v$version" . ".txt";
-            while (1)
-            {
-                if (file_exists($reportsFolder . "/" . $file_name))
-                {
-                    $version++;
-                    $file_name = "Freegal_r_" . strtolower($row_country['library_territory']) . "_" . date('Ym', strtotime($value['from_date'])) . '_' . $libTypeKey . "_v$version" . ".txt";
-                }
-                else
-                {
-                    break;
-                }
-            }
-            $round_total_sales = round($total_sales, 2);
-            if (false === strpos($round_total_sales, '.'))
-            {
-                $round_total_sales .= '.00';
-            }
-            else
-            {
-                $strlen = strlen(end(explode('.', $round_total_sales)));
-                if (1 == $strlen)
-                {
-                    $round_total_sales .= '0';
-                }
-            }
-
-            $royalty_content[0][] = array("H", date('Ymd', strtotime($value['from_date'])), date('Ymd', strtotime($value['to_date'])), $round_total_sales, $country_curency[$row_country['library_territory']], "Y", "ET", "3.0", "$version");
-            $royalty_content[2][] = array("T", $total_records, $total_sold, 0, 0, 0, 0, 0);
-            write_file($royalty_content, $file_name, $reportsFolder . "/");
+//            $version = 1;
+//            $file_name = "Freegal_r_" . strtolower($row_country['library_territory']) . "_" . date('Ym', strtotime($value['from_date'])) . '_' . $libTypeKey . "_v$version" . ".txt";
+//            while (1)
+//            {
+//                if (file_exists($reportsFolder . "/" . $file_name))
+//                {
+//                    $version++;
+//                    $file_name = "Freegal_r_" . strtolower($row_country['library_territory']) . "_" . date('Ym', strtotime($value['from_date'])) . '_' . $libTypeKey . "_v$version" . ".txt";
+//                }
+//                else
+//                {
+//                    break;
+//                }
+//            }
+//            $round_total_sales = round($total_sales, 2);
+//            if (false === strpos($round_total_sales, '.'))
+//            {
+//                $round_total_sales .= '.00';
+//            }
+//            else
+//            {
+//                $strlen = strlen(end(explode('.', $round_total_sales)));
+//                if (1 == $strlen)
+//                {
+//                    $round_total_sales .= '0';
+//                }
+//            }
+//
+//            $royalty_content[0][] = array("H", date('Ymd', strtotime($value['from_date'])), date('Ymd', strtotime($value['to_date'])), $round_total_sales, $country_curency[$row_country['library_territory']], "Y", "ET", "3.0", "$version");
+//            $royalty_content[2][] = array("T", $total_records, $total_sold, 0, 0, 0, 0, 0);
+//            write_file($royalty_content, $file_name, $reportsFolder . "/");
         }
     }
 }
@@ -192,10 +241,10 @@ function write_file($content, $file_name, $folder)
         fclose($fh);
 
         //FOR SENDING REPORT TO IODA SERVER USING SFTP
-        if (sendReportFilesftp($folder . $file_name, $file_name, $logFileWrite, "monthly"))
-        {
-            echo "Report $file_name sent";
-        }
+//        if (sendReportFilesftp($folder . $file_name, $file_name, $logFileWrite, "monthly"))
+//        {
+//            echo "Report $file_name sent";
+//        }
         //FOR SENDING REPORT TO IODA SERVER USING FTP
 //        if (sendReportFileftp($folder . $file_name, $file_name, $logFileWrite, "monthly"))
 //        {
