fb58b5c8a8714d4fe049800b68320c13963b0e83
Ralph Kelley <ralphkelley@fullsail.edu>
2014-06-27 19:48:27 -0400

    remove debug kit

--- app_controller.php
+++ app_controller.php
@@ -2,7 +2,7 @@
 class AppController extends Controller
 {
 
-    var $components = array('Session', 'RequestHandler', 'Cookie', 'Acl', 'Common', 'DebugKit.Toolbar');
+    var $components = array('Session', 'RequestHandler', 'Cookie', 'Acl', 'Common');
     var $helpers = array('Session', 'Html', 'Ajax', 'Javascript', 'Form', 'Library', 'Download', 'Queue', 'Streaming');
     var $uses = array('Genre', 'Featuredartist', 'Newartist', 'Category', 'Album', 'Country', 'Wishlist', 'WishlistVideo', 'Download', 'Library');
     var $view = 'Dataencode';
