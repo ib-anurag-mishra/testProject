263d5c0aa515dc49607261b9ef46b57cbcfb8e88
daviddickson <david@m68interactive.com>
2014-07-07 17:03:49 -0400

    debug js

--- webroot/js/freegal40-site.js
+++ webroot/js/freegal40-site.js
@@ -1674,6 +1674,8 @@ $(document).ready(function() {
         var Provider = $(this).siblings().find('input[type="hidden"]').attr('data-provider');
         var type = $(this).siblings().find('input[type="hidden"]').attr('value');
 
+        console.log(ProdID);
+
         $.ajax({
             type: "post",
             data: {'prodID': ProdID, 'provider_type': Provider, 'type': type},
