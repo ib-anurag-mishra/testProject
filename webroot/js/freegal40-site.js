4bd11c46621424605a32646875bf8222f0ea9d59
daviddickson <david@m68interactive.com>
2014-07-08 10:54:22 -0400

    update js

--- webroot/js/freegal40-site.js
+++ webroot/js/freegal40-site.js
@@ -1679,14 +1679,14 @@ $(document).ready(function() {
 
         $(this).find('.playlist-menu-icon').css({opacity:.5});
         $(this).find('.top-10-download-now-button').css({opacity:.5});
-        // $(this).find('.album-preview').css({opacity:.5});
+        $(this).find('.album-preview').css({opacity:.5});
     });
 
     $('.my-top-100-page .songs-shadow-container .songs-scrollable ul li .song-container').on('mouseleave',function(){
 
         $(this).find('.playlist-menu-icon').css({opacity:.0});
         $(this).find('.top-10-download-now-button').css({opacity:.5});
-        // $(this).find('.album-preview').css({opacity:.0});
+        $(this).find('.album-preview').css({opacity:.0});
     });            
 
     /* */
