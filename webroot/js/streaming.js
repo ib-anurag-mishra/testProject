77531912da63d34f90b52b9c1e169069729e54e6
daviddickson <david@m68interactive.com>
2014-08-14 10:19:05 -0400

    comment out test code

--- webroot/js/streaming.js
+++ webroot/js/streaming.js
@@ -3,8 +3,8 @@ var params = {};
 var attributes = {};
 attributes.id = "fmp_player";
 swfobject.switchOffAutoHideShow();
-// swfobject.embedSWF("/swf/fmp.swf?"+(Math.random()*1000000), "alt", "960", "100", "9.0.0", false, flashvars, params, attributes);
-swfobject.embedSWF("/swf/fmp-test.swf?"+(Math.random()*1000000), "alt", "960", "100", "9.0.0", false, flashvars, params, attributes);
+swfobject.embedSWF("/swf/fmp.swf?"+(Math.random()*1000000), "alt", "960", "100", "9.0.0", false, flashvars, params, attributes);
+// swfobject.embedSWF("/swf/fmp-test.swf?"+(Math.random()*1000000), "alt", "960", "100", "9.0.0", false, flashvars, params, attributes);
 
 
 
