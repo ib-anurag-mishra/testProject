81f3b31a0bfbddd2e84429a7787414dfd03974d9
ibmohan <mohanrocku@gmail.com>
2014-08-18 11:38:02 +0530

    removed group by

--- webroot/cron/update_missing_patronids.php
+++ webroot/cron/update_missing_patronids.php
@@ -13,13 +13,13 @@
 set_time_limit(0);
 $freegalConn = mysql_connect("192.168.100.114","freegal_prod","}e47^B1EO9hD");
 mysql_select_db("freegal", $freegalConn); 
-$query = "select patron_id,library_id,created from downloads where patron_id not in (select distinct patronid from currentpatrons) group by patron_id";
+$query = "select distinct patron_id,library_id from downloads where patron_id not in (select distinct patronid from currentpatrons)";
 $result = mysql_query($query) or die('Query failed: ' . mysql_error());
 $list = array();
+$created = date("Y-m-d H:i:s");
 while ($line = mysql_fetch_array($result, MYSQL_ASSOC)) {
     $patron_id = $line['patron_id'];
     $library_id = $line['library_id'];
-    $created = $line['created'];
     $sql = "insert into currentpatrons (libid,patronid,created,modified) values($library_id,"."'".$patron_id."'"    .","."'".$created."',"."'".$created."'".")";
     $result1 = mysql_query($sql) or die('Query failed: ' . mysql_error());
     $list[] = mysql_insert_id();
