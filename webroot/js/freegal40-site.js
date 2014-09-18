0d9d080a2d294f7df4cf519000eeb6df4caa8c4e
daviddickson <david@m68interactive.com>
2014-09-18 11:00:09 -0400

    debug js

--- webroot/js/freegal40-site.js
+++ webroot/js/freegal40-site.js
@@ -1852,7 +1852,7 @@ $(document).ready(function() {
     });
 
 
-    $('.my-top-100-page .album-container .add-to-playlist-button,.my-top-100-page .song-container .add-to-playlist-button').on('mouseleave',function(e){
+    $('.my-top-100-page .album-container .playlist-menu-icon,.my-top-100-page .song-container .playlist-menu-icon').on('mouseleave',function(e){
         if (e.offsetX < 0 || e.offsetX > $(this).width() || e.offsetY < $(this).height()) {
             $(this).next('ul').removeClass('active');
 
