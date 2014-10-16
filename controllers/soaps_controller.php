17a2c24b401a14ebe2831d00e8787a4857e50266
ganesh588 <ganesh.routhu@infobeans.com>
2014-10-16 13:29:24 +0530

    formatting

--- controllers/soaps_controller.php
+++ controllers/soaps_controller.php
@@ -744,7 +744,7 @@ class SoapsController extends AppController {
    * @param int $libraryId
 	 * @return NationalTopTenType[]
    */
-	function getNationalTopTen($authenticationToken, $libraryId) {
+ function getNationalTopTen($authenticationToken, $libraryId) {
 
     if(!($this->isValidAuthenticationToken($authenticationToken))) {
       throw new SOAPFault('Soap:logout', 'Freegal needs to update. Please logout and login again.');
@@ -784,7 +784,7 @@ class SoapsController extends AppController {
           $this->Album->recursive = -1;
           $album = $this->Album->find('first',array('fields' => array('AlbumTitle'),'conditions' => array("ProdId = ".$data['Song']['ReferenceID'], "provider_type" => $data['Song']['provider_type'])));
           $obj->AlbumTitle = $this->getTextUTF($album['Album']['AlbumTitle']);
-          $fileURL = $this->Token->regularToken( $data['Sample_Files']['CdnPath']."/".$data['Sample_Files']['SaveAsName']);
+          $fileURL = $this->Token->regularToken( $data['Song']['CdnPath']."/".$data['Song']['Sample_SaveAsName']);
           $fileURL = Configure::read('App.Music_Path').$fileURL;
           
           if($this->IsDownloadable($data['Song']['ProdID'], $territory, $data['Song']['provider_type'])) {
@@ -792,7 +792,7 @@ class SoapsController extends AppController {
             $obj->FullLength_FIleURL      = 'nostring';
           } else {
             $obj->fileURL                 = (string)$fileURL;
-            $obj->FullLength_FIleURL      = Configure::read('App.Music_Path').$this->Token->regularToken( $data['Full_Files']['CdnPath']."/".$data['Full_Files']['SaveAsName']);
+            $obj->FullLength_FIleURL      = Configure::read('App.Music_Path').$this->Token->regularToken( $data['Song']['CdnPath']."/".$data['Song']['FullLength_SaveAsName']);
           }
           
           $obj->FullLength_FIleID         = (int)$data['Full_Files']['FileID'];
@@ -861,7 +861,7 @@ class SoapsController extends AppController {
           $this->Album->recursive = -1;
           $album = $this->Album->find('first',array('fields' => array('AlbumTitle'),'conditions' => array("ProdId = ".$data['Song']['ReferenceID'], "provider_type" => $data['Song']['provider_type'])));
           $obj->AlbumTitle = $this->getTextUTF($album['Album']['AlbumTitle']);
-          $fileURL = $this->Token->regularToken( $data['Sample_Files']['CdnPath']."/".$data['Sample_Files']['SaveAsName']);
+          $fileURL = $this->Token->regularToken( $data['Song']['CdnPath']."/".$data['Song']['Sample_SaveAsName']);
           $fileURL = Configure::read('App.Music_Path').$fileURL;
           
           if($this->IsDownloadable($data['Song']['ProdID'], $territory, $data['Song']['provider_type'])) {
@@ -869,10 +869,10 @@ class SoapsController extends AppController {
             $obj->FullLength_FIleURL      = 'nostring';
           } else {
             $obj->fileURL                 = (string)$fileURL;
-            $obj->FullLength_FIleURL      = Configure::read('App.Music_Path').$this->Token->regularToken( $data['Full_Files']['CdnPath']."/".$data['Full_Files']['SaveAsName']);
+            $obj->FullLength_FIleURL      = Configure::read('App.Music_Path').$this->Token->regularToken( $data['Song']['CdnPath']."/".$data['Song']['FullLength_SaveAsName']);
           }
           
-          $obj->FullLength_FIleID         = (int)$data['Full_Files']['FileID'];
+          $obj->FullLength_FIleID         = (int)'0';
           
           $obj->playButtonStatus          = $this->getPlayButtonStatus($data['Song']['ProdID'], $territory, $data['Song']['provider_type']);         
           
@@ -4732,62 +4732,64 @@ class SoapsController extends AppController {
    */
 
   private function isValidAuthenticationToken($token){
+
     $encodingKey = 'bGlicmFyeWlkZWFzMjAxNA==';
 	$char = array('"','*','_','/','+','~','!','@','#','$','%','^','&','(',')','{','}',':','<','>','?','`','-','=',';',',','.','|');
 	$val = $this->AuthenticationToken->find('first', array('conditions' => array('token' => $token)));
 
 	if($val) {
-	$lastVisited = $val['AuthenticationToken']['auth_time'];
-	
-	if((time() - $lastVisited) > 1200){
 
-	 if(empty($val['AuthenticationToken']['email']))         { $val['AuthenticationToken']['email'] = ' ';           }
-        if(empty($val['AuthenticationToken']['password']))      { $val['AuthenticationToken']['password'] = ' ';        }
-		else {
-			$val['AuthenticationToken']['password'] = $this->Common->freegalDecode($val['AuthenticationToken']['password'],$encodingKey);
-			if(preg_match('/\W/',$val['AuthenticationToken']['password'])) {
-				$decryptArr = str_split($val['AuthenticationToken']['password']);
-				foreach($decryptArr as $a) 	{
-					if(!preg_match('/\w/',$a)){
-	 					if (!in_array($a,$char)) { 
-							$val['AuthenticationToken']['password'] = $this->Common->freegalDecode($val['AuthenticationToken']['password']); 
-	 					}
-					}
-    			}
-			}	
-		}
-        if(empty($val['AuthenticationToken']['card']))          { $val['AuthenticationToken']['card'] = ' ';            }
-		else {
-			$val['AuthenticationToken']['card'] = $this->Common->freegalDecode($val['AuthenticationToken']['card'],$encodingKey);
-			if(preg_match('/\W/',$val['AuthenticationToken']['card'])) {
-				$decryptArr = str_split($val['AuthenticationToken']['card']);
-				foreach($decryptArr as $a) 	{
-					if(!preg_match('/\w/',$a)){
-	 					if (!in_array($a,$char)) { 
-							$val['AuthenticationToken']['card'] = $this->Common->freegalDecode($val['AuthenticationToken']['card']); 
-	 					}
-					}
-    			}
-			}	
-		}
-        if(empty($val['AuthenticationToken']['pin']))           { $val['AuthenticationToken']['pin'] = ' ';             }
-		else {
-			$val['AuthenticationToken']['pin'] = $this->Common->freegalDecode($val['AuthenticationToken']['pin'],$encodingKey);
-			if(preg_match('/\W/',$val['AuthenticationToken']['pin'])) {
-				$decryptArr = str_split($val['AuthenticationToken']['pin']);
-				foreach($decryptArr as $a) 	{
-					if(!preg_match('/\w/',$a)){
-	 					if (!in_array($a,$char)) { 
-							$val['AuthenticationToken']['pin'] = $this->Common->freegalDecode($val['AuthenticationToken']['pin']); 
-	 					}
-					}
-    			}
-			}	
-		}
-        if(empty($val['AuthenticationToken']['last_name']))     { $val['AuthenticationToken']['last_name'] = ' ';       }
-        if(empty($val['AuthenticationToken']['library_id']))    { $val['AuthenticationToken']['library_id'] = ' ';      }
-        if(empty($val['AuthenticationToken']['agent']))         { $val['AuthenticationToken']['agent'] = ' '; 	       }
-		if($val['AuthenticationToken']['agent'] == '?')			{ $val['AuthenticationToken']['agent'] = ' ';		   }
+		$lastVisited = $val['AuthenticationToken']['auth_time'];
+	
+		if((time() - $lastVisited) > 1200){
+
+	 		if(empty($val['AuthenticationToken']['email']))         { $val['AuthenticationToken']['email'] = ' ';           }
+        	if(empty($val['AuthenticationToken']['password']))      { $val['AuthenticationToken']['password'] = ' ';        }
+			else {
+				$val['AuthenticationToken']['password'] = $this->Common->freegalDecode($val['AuthenticationToken']['password'],$encodingKey);
+				if(preg_match('/\W/',$val['AuthenticationToken']['password'])) {
+					$decryptArr = str_split($val['AuthenticationToken']['password']);
+					foreach($decryptArr as $a) 	{
+						if(!preg_match('/\w/',$a)){
+	 						if (!in_array($a,$char)) { 
+								$val['AuthenticationToken']['password'] = $this->Common->freegalDecode($val['AuthenticationToken']['password']); 
+	 						}	
+						}
+    				}
+				}	
+			}
+        	if(empty($val['AuthenticationToken']['card']))          { $val['AuthenticationToken']['card'] = ' ';            }
+			else {
+				$val['AuthenticationToken']['card'] = $this->Common->freegalDecode($val['AuthenticationToken']['card'],$encodingKey);
+				if(preg_match('/\W/',$val['AuthenticationToken']['card'])) {
+					$decryptArr = str_split($val['AuthenticationToken']['card']);
+					foreach($decryptArr as $a) 	{
+						if(!preg_match('/\w/',$a)){
+	 						if (!in_array($a,$char)) { 
+								$val['AuthenticationToken']['card'] = $this->Common->freegalDecode($val['AuthenticationToken']['card']); 
+	 						}
+						}
+    				}
+				}	
+			}
+        	if(empty($val['AuthenticationToken']['pin']))           { $val['AuthenticationToken']['pin'] = ' ';             }
+			else {
+				$val['AuthenticationToken']['pin'] = $this->Common->freegalDecode($val['AuthenticationToken']['pin'],$encodingKey);
+				if(preg_match('/\W/',$val['AuthenticationToken']['pin'])) {
+					$decryptArr = str_split($val['AuthenticationToken']['pin']);
+					foreach($decryptArr as $a) 	{
+						if(!preg_match('/\w/',$a)){
+	 						if (!in_array($a,$char)) { 
+								$val['AuthenticationToken']['pin'] = $this->Common->freegalDecode($val['AuthenticationToken']['pin']); 
+	 						}
+						}
+    				}
+				}	
+			}
+        	if(empty($val['AuthenticationToken']['last_name']))     { $val['AuthenticationToken']['last_name'] = ' ';       }
+        	if(empty($val['AuthenticationToken']['library_id']))    { $val['AuthenticationToken']['library_id'] = ' ';      }
+        	if(empty($val['AuthenticationToken']['agent']))         { $val['AuthenticationToken']['agent'] = ' '; 	       }
+			if($val['AuthenticationToken']['agent'] == '?')			{ $val['AuthenticationToken']['agent'] = ' ';		   }
 
 	$log_name = 'block_card_app_log';
 	$log_data = PHP_EOL."----------Request (".$val['AuthenticationToken']['id'].") Start----------------".PHP_EOL;
@@ -4812,11 +4814,11 @@ class SoapsController extends AppController {
 			$this->AuthenticationToken->save($data);
 			return true;
 		}
-	}
-	else {
+	  }
+	  else {
 		return true;
-	}
-   }
+	  }
+    }
 	else{
 		return false;
 	}
