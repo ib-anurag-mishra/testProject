b70da8cfb783fb11503c1b0eedccc204f9b7f6e9
kiranpyati <kiran.pyati@infobeans.com>
2014-10-28 16:36:34 +0530

    fixed sphinx related issues

--- models/song.php
+++ models/song.php
@@ -9,6 +9,7 @@ class Song extends AppModel {
 	var $name = 'Song';
 	var $useTable = 'Songs';
 	var $primaryKey = 'ProdID';
+	var $actsAs = array('Containable', 'Sphinx');
 	var $uses = array('Featuredartist', 'Country');
 
 	var $hasOne = array(
